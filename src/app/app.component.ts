92f5a292628676c66390262be343bba4bdf2f386
Ildar Galeev <KeinAsylum@gmail.com>
2018-12-04 20:38:16 +0300

    Form control UI kit (#2)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-    selector: 'app-root',
+    selector: 'dsh-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    title = 'dashboard';
+    form: FormGroup;
+
+    constructor(private fb: FormBuilder, private router: ActivatedRoute) {
+        this.form = this.fb.group({
+            shopName: ['', Validators.required],
+            category: [''],
+            creationDate: [''],
+            document: ['One'],
+            comment: [''],
+            checked: [false],
+            radio: [''],
+            slided: [false]
+        });
+    }
 }
