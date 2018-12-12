9e65653ec1a41b6cf59329e494e95e864d9b998b
Ildar Galeev <keinasylum@gmail.com>
2018-12-12 19:19:40 +0300

    FE-716: prettier fix

--- src/app/actionbar/actionbar.module.ts
+++ src/app/actionbar/actionbar.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { UiKitModule } from '../ui-kit';
 import { ActionbarComponent } from './actionbar.component';
 
 @NgModule({
     declarations: [ActionbarComponent],
-    imports: [CommonModule, UiKitModule],
-    exports: [ActionbarComponent],
-    providers: []
+    imports: [UiKitModule],
+    exports: [ActionbarComponent]
 })
 export class ActionbarModule {}
