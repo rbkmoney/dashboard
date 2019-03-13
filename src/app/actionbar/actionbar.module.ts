054f6c8fc4a04c9a2bc9e38248c846cb8366246c
Rinat Arsaev <krickray@gmail.com>
2019-03-13 16:44:02 +0300

    add notifications icon

--- src/app/actionbar/actionbar.module.ts
+++ src/app/actionbar/actionbar.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { ActionbarComponent } from './actionbar.component';
 
 @NgModule({
     declarations: [ActionbarComponent],
-    imports: [MatIconModule],
+    imports: [MatIconModule, FlexLayoutModule],
     exports: [ActionbarComponent]
 })
 export class ActionbarModule {}
