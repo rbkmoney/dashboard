0b77dad27797d451e793e3689e4d4070568611a7
Rinat Arsaev <krickray@gmail.com>
2019-03-15 16:38:09 +0300

    calculate triangle position

--- src/app/dropdown/dropdown.component.ts
+++ src/app/dropdown/dropdown.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewChild, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input, ViewChild, OnChanges, SimpleChanges, ElementRef } from '@angular/core';
 import { Overlay, CdkOverlayOrigin, OverlayConfig, OverlayRef } from '@angular/cdk/overlay';
 import { TemplatePortalDirective } from '@angular/cdk/portal';
 import { trigger, state, style, transition, animate } from '@angular/animations';
@@ -39,33 +39,37 @@ export class DropdownComponent implements OnChanges {
     @Input() overlayOrigin: CdkOverlayOrigin;
     @ViewChild('portalTemplate') portalTemplate: TemplatePortalDirective;
     overlayRef: OverlayRef;
+    @ViewChild('dropdown') dropdown: ElementRef;
+    @ViewChild('dropdownTriangle') dropdownTriangle: ElementRef;
     isOpen = false;
     state = State;
+    triangleLeft;
 
     constructor(private overlay: Overlay) {}
 
     private createOverlay() {
+        const positionStrategy = this.overlay
+            .position()
+            .flexibleConnectedTo(this.overlayOrigin.elementRef)
+            .withPush(true)
+            .withDefaultOffsetX(0)
+            .withPositions([
+                {
+                    originX: 'center',
+                    originY: 'bottom',
+                    overlayX: 'center',
+                    overlayY: 'top',
+                    offsetY: 10
+                },
+                {
+                    originX: 'center',
+                    originY: 'top',
+                    overlayX: 'start',
+                    overlayY: 'bottom'
+                }
+            ]);
         const config = new OverlayConfig({
-            positionStrategy: this.overlay
-                .position()
-                .flexibleConnectedTo(this.overlayOrigin.elementRef)
-                .withPush(true)
-                .withDefaultOffsetX(0)
-                .withPositions([
-                    {
-                        originX: 'center',
-                        originY: 'bottom',
-                        overlayX: 'center',
-                        overlayY: 'top',
-                        offsetY: 15
-                    },
-                    {
-                        originX: 'center',
-                        originY: 'top',
-                        overlayX: 'start',
-                        overlayY: 'bottom'
-                    }
-                ]),
+            positionStrategy,
             scrollStrategy: this.overlay.scrollStrategies.reposition(),
             hasBackdrop: true,
             backdropClass: '',
@@ -74,6 +78,9 @@ export class DropdownComponent implements OnChanges {
         this.overlayRef = this.overlay.create(config);
         this.overlayRef.backdropClick().subscribe(() => this.closeStart());
         this.overlayRef.attach(this.portalTemplate);
+        positionStrategy.positionChanges.subscribe(() => {
+            this.updateTrianglePosition();
+        });
     }
 
     ngOnChanges(changes: SimpleChanges) {
@@ -85,6 +92,23 @@ export class DropdownComponent implements OnChanges {
     animationEnd(event) {
         if (!this.isOpen) {
             this.closeEnd();
+        }
+        this.updateTrianglePosition();
+    }
+
+    updateTrianglePosition() {
+        if (this.dropdown && this.dropdownTriangle && this.overlayOrigin.elementRef) {
+            const dropdownRect = this.dropdown.nativeElement.getBoundingClientRect();
+            const dropdownTriangleRect = this.dropdownTriangle.nativeElement.getBoundingClientRect();
+            const overlayOriginRect = this.overlayOrigin.elementRef.nativeElement.getBoundingClientRect();
+            console.log(dropdownTriangleRect);
+            const leftOffset =
+                overlayOriginRect.left -
+                dropdownRect.left +
+                overlayOriginRect.width / 2 -
+                dropdownTriangleRect.width / 2 -
+                4;
+            this.triangleLeft = `${leftOffset}px`;
         }
     }
 
