8c3957d30080e444ec100d54f3cf19bb4874fcd8
DenisEzhov <d.ezhov@rbkmoney.com>
2019-04-02 15:54:29 +0300

    fixes

--- src/app/charts/charts.module.ts
+++ src/app/charts/charts.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DonutChartComponent } from './donut-chart/donut-chart.component';
 import { LayoutModule } from '../layout';
+import { BarChartComponent } from './bar-chart/bar-chart.component';
 
 @NgModule({
-    declarations: [DonutChartComponent],
+    declarations: [DonutChartComponent, BarChartComponent],
     imports: [CommonModule, LayoutModule],
-    exports: [DonutChartComponent]
+    exports: [DonutChartComponent, BarChartComponent]
 })
 export class ChartsModule {}
