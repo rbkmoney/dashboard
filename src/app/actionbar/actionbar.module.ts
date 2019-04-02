307e21bed306f864264b3233c134548957d0e9e3
Rinat Arsaev <krickray@gmail.com>
2019-04-02 14:56:34 +0300

    FE-793: Create dropdown module (#6)

--- src/app/actionbar/actionbar.module.ts
+++ src/app/actionbar/actionbar.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { OverlayModule } from '@angular/cdk/overlay';
 
 import { ActionbarComponent } from './actionbar.component';
+import { DropdownModule } from '../dropdown/dropdown.module';
 
 @NgModule({
     declarations: [ActionbarComponent],
-    imports: [MatIconModule],
+    imports: [MatIconModule, FlexLayoutModule, DropdownModule, OverlayModule],
     exports: [ActionbarComponent]
 })
 export class ActionbarModule {}
---
+++ src/app/dropdown/dropdown.component.ts
@@ -0,0 +1,41 @@
+import { Component, Input, ViewChild, EventEmitter, Output, TemplateRef } from '@angular/core';
+import { Subject } from 'rxjs';
+
+import { openCloseAnimation, State } from './open-close-animation';
+
+/**
+ * overlayX: 'center' has problem with width: '100%'
+ */
+const FULL_WIDTH = '99.99%';
+
+@Component({
+    selector: 'dsh-dropdown',
+    templateUrl: 'dropdown.component.html',
+    styleUrls: ['dropdown.component.scss'],
+    animations: [openCloseAnimation],
+    exportAs: 'dshDropdown'
+})
+export class DropdownComponent {
+    @Input() width?: number | string;
+    @Input() disableClose = false;
+    @Output() backdropClick? = new EventEmitter<MouseEvent>();
+
+    @ViewChild(TemplateRef) templateRef: TemplateRef<any>;
+    state = State.closed;
+    triangleLeftOffset: string;
+    animationDone$ = new Subject();
+
+    get correctedWidth() {
+        if (this.width === '100%') {
+            return FULL_WIDTH;
+        }
+        if (!this.width || (typeof this.width === 'string' && this.width.slice(-1) === '%')) {
+            return this.width;
+        }
+        const widthPx: number = typeof this.width === 'string' ? parseFloat(this.width) : this.width;
+        if (widthPx + 1 >= document.body.getBoundingClientRect().width) {
+            return FULL_WIDTH;
+        }
+        return widthPx;
+    }
+}
