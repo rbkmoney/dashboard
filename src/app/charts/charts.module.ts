39de3003280706dd48c987ee15975f360b14336d
DenisEzhov <d.ezhov@rbkmoney.com>
2019-04-19 13:48:16 +0300

    fixes

--- src/app/charts/charts.module.ts
+++ src/app/charts/charts.module.ts
@@ -7,11 +7,12 @@ import { ChartsService } from './charts.service';
 import { DonutChartComponent } from './donut-chart/donut-chart.component';
 import { BarChartComponent } from './bar-chart/bar-chart.component';
 import { BarChartService } from './bar-chart/bar-chart.service';
+import { DonutChartService } from './donut-chart/donut-chart.service';
 
 @NgModule({
     declarations: [DonutChartComponent, BarChartComponent, BarChartComponent],
     imports: [CommonModule, LayoutModule],
     exports: [DonutChartComponent, BarChartComponent, BarChartComponent],
-    providers: [ChartsService, BarChartService]
+    providers: [ChartsService, BarChartService, DonutChartService]
 })
 export class ChartsModule {}
--- src/app/charts/donut-chart/donut-chart.component.ts
+++ src/app/charts/donut-chart/donut-chart.component.ts
@@ -1,11 +1,9 @@
-import { Component, ElementRef, Input, OnChanges, OnInit, ViewChild } from '@angular/core';
-import { Arc, arc, DefaultArcObject, Pie, pie, PieArcDatum } from 'd3-shape';
-import { select, Selection } from 'd3-selection';
-import { interpolate } from 'd3-interpolate';
-import { easeExp } from 'd3-ease';
+import { Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
+import { PieArcDatum } from 'd3-shape';
+import { Selection } from 'd3-selection';
 
 import { SegmentData } from '../models/chart-data-models';
-import { chartColors } from '../color-constants';
+import { DonutChartService } from './donut-chart.service';
 
 @Component({
     selector: 'dsh-donut-chart',
@@ -19,99 +17,18 @@ export class DonutChartComponent implements OnChanges, OnInit {
     @Input()
     data: SegmentData[];
 
-    private svg: Selection<SVGGElement, {}, null, undefined>;
-    private generatePieData: Pie<void, SegmentData>;
-    private generateArc: Arc<void, PieArcDatum<SegmentData> | DefaultArcObject>;
     private donut: Selection<any, PieArcDatum<SegmentData>, SVGGElement, {}>;
 
-    private radius: number;
-    private innerRadius = 0.96; // percent of radius
-    private padAngle = 0.05;
-    private cornerRadius = 4;
-
-    constructor() {}
+    constructor(private donutChartService: DonutChartService) {}
 
     ngOnInit() {
-        this.createChart();
-        this.createChartTransition();
+        const element = this.chartContainer.nativeElement;
+        this.donut = this.donutChartService.createChart(this.data, element);
     }
 
-    ngOnChanges(changes: any) {
+    ngOnChanges(changes: SimpleChanges) {
         if (this.donut) {
-            this.updateChart();
+            this.donutChartService.updateChart(this.data);
         }
-    }
-
-    createChart() {
-        const container = this.chartContainer.nativeElement;
-        const size = container.offsetWidth;
-        this.radius = size / 2;
-
-        select(container)
-            .select('svg')
-            .selectAll('*')
-            .remove();
-
-        this.generatePieData = pie<void, SegmentData>()
-            .sort(null)
-            .value(d => d.value);
-
-        this.svg = select(container)
-            .select('svg')
-            .attr('height', size)
-            .attr('width', size)
-            .append('g')
-            .attr('transform', `translate(${this.radius}, ${this.radius})`)
-            .attr('class', 'donutChart');
-
-        const generatedData = this.generatePieData(this.data);
-
-        this.generateArc = arc()
-            .innerRadius(this.radius * this.innerRadius)
-            .outerRadius(this.radius)
-            .padAngle(this.padAngle)
-            .cornerRadius(this.cornerRadius);
-
-        this.donut = this.svg
-            .selectAll('.arc')
-            .data(generatedData)
-            .enter()
-            .append('path')
-            .attr('id', () => 'segment')
-            .attr('fill', (d, i) => chartColors[i])
-            .attr('periodData-index', (d, i) => i)
-            .style('stroke-width', 1);
-
-        this.updateChart();
-    }
-
-    updateChart() {
-        this.donut = this.donut.data(this.generatePieData(this.data));
-        this.updateChartTransition();
-    }
-
-    private createChartTransition() {
-        const generateArc = this.generateArc;
-        return this.donut
-            .transition()
-            .ease(easeExp)
-            .duration(1500)
-            .attrTween('d', b => {
-                const i = interpolate({ startAngle: 0, endAngle: 0 }, b);
-                return t => generateArc(i(t));
-            });
-    }
-
-    private updateChartTransition() {
-        const generateArc = this.generateArc;
-        return this.donut
-            .transition()
-            .ease(easeExp)
-            .duration(750)
-            .attrTween('d', function(d) {
-                const i = interpolate(this._current, d);
-                this._current = i(0);
-                return t => generateArc(i(t));
-            });
     }
 }
