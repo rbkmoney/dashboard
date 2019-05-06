09b96c8aca9d23042ff62e5bb61005630a1f9014
Rinat Arsaev <krickray@gmail.com>
2019-05-06 20:25:46 +0300

    add index.ts

--- src/app/sections/sections.module.ts
+++ src/app/sections/sections.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { SectionsRoutingModule } from './sections-routing.module';
 import { MainModule } from './main';
 import { PartyMngModule } from './party-mgt';
-import { AnalyticsModule } from './analytics/analytics.module';
+import { AnalyticsModule } from './analytics';
 import { DetailsModule } from './details';
 import { PageNotFoundModule } from './page-not-found';
 import { TableModule } from './table';
