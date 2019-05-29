831049a5216d131211a634960b7a48420d6740db
Rinat Arsaev <krickray@gmail.com>
2019-05-29 16:58:49 +0300

    add icon

--- src/app/layout/float-panel/float-panel.module.ts
+++ src/app/layout/float-panel/float-panel.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { FloatPanelComponent } from './float-panel.component';
 import { CardModule } from '../card';
 
 @NgModule({
-    imports: [CardModule],
+    imports: [CardModule, MatIconModule, FlexLayoutModule],
     declarations: [FloatPanelComponent],
     exports: [FloatPanelComponent]
 })
