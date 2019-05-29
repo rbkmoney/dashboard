a60aabbfccea2e540075ad3482ebcc8dbca7a0b8
Rinat Arsaev <krickray@gmail.com>
2019-05-29 17:45:55 +0300

    init open

--- src/app/layout/float-panel/float-panel.component.ts
+++ src/app/layout/float-panel/float-panel.component.ts
@@ -1,18 +1,49 @@
-import { Component } from '@angular/core';
-import { Overlay } from '@angular/cdk/overlay';
-import { ComponentPortal } from '@angular/cdk/portal';
+import { Component, ContentChild, ViewContainerRef, OnDestroy } from '@angular/core';
+import { Overlay, OverlayRef, OverlayConfig } from '@angular/cdk/overlay';
+import { TemplatePortal } from '@angular/cdk/portal';
+
+import { FloatPanelMoreComponent } from './float-panel-more/float-panel-more.component';
 
 @Component({
     selector: 'dsh-float-panel',
     templateUrl: 'float-panel.component.html',
     styleUrls: ['float-panel.component.scss']
 })
-export class FloatPanelComponent {
-    constructor(private overlay: Overlay) {}
+export class FloatPanelComponent implements OnDestroy {
+    @ContentChild(FloatPanelMoreComponent) floatPanelMore;
+
+    private _overlayRef: OverlayRef;
+    get overlayRef() {
+        return this._overlayRef || (this._overlayRef = this.createOverlayRef());
+    }
+
+    constructor(private overlay: Overlay, private viewContainerRef: ViewContainerRef) {}
+
+    ngOnDestroy() {
+        if (this._overlayRef) {
+            this._overlayRef.dispose();
+            this._overlayRef = null;
+        }
+    }
 
     open() {
-        // const overlayRef = this.overlay.create();
-        // const filePreviewPortal = new ComponentPortal(FilePreviewOverlayComponent);
-        // overlayRef.attach(filePreviewPortal);
+        if (!this.overlayRef.hasAttached()) {
+            const portal = this.getPortal();
+            this.overlayRef.attach(portal);
+        }
+    }
+
+    private getPortal(): TemplatePortal {
+        return new TemplatePortal(this.floatPanelMore.templateRef, this.viewContainerRef);
+    }
+
+    private createOverlayRef(): OverlayRef {
+        const config = this.getOverlayConfig();
+        const overlayRef = this.overlay.create(config);
+        return overlayRef;
+    }
+
+    private getOverlayConfig(): OverlayConfig {
+        return new OverlayConfig({});
     }
 }
