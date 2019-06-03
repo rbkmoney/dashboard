36e77276af3667220fa4582ec0290b28f6f84738
Rinat Arsaev <krickray@gmail.com>
2019-06-03 15:00:46 +0300

    revert scroll startegy

--- src/app/layout/float-panel/float-panel.component.ts
+++ src/app/layout/float-panel/float-panel.component.ts
@@ -151,6 +151,7 @@ export class FloatPanelComponent implements AfterViewInit {
         const overlayConfig = new OverlayConfig({
             width: get(this.substrateRuler, 'value.width', 0) + 'px',
             positionStrategy,
+            scrollStrategy: this.overlay.scrollStrategies.reposition(),
             panelClass: 'panel'
         });
 
