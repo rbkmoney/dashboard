51e134910a039cba7cf0fb2133f2beade575ec37
Ildar Galeev <KeinAsylum@gmail.com>
2019-06-07 20:01:22 +0300

    Card module refactoring (#35)

--- src/app/layout/card/card-header/card-header.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'dsh-card-header',
-    templateUrl: './card-header.component.html',
-    styleUrls: ['./card-header.component.scss']
-})
-export class CardHeaderComponent {}
