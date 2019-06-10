b4f18d906f7c8dcc42bdbd8647c41642826b68a3
Rinat Arsaev <krickray@gmail.com>
2019-06-10 18:22:07 +0300

    fix typeography and theme

--- src/app/side-menu/side-menu-item/side-menu-item.component.ts
+++ src/app/side-menu/side-menu-item/side-menu-item.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, Input } from '@angular/core';
 
 @Component({
     selector: 'dsh-side-menu-item',
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
     styleUrls: ['side-menu-item.component.scss']
 })
 export class SideMenuItemComponent {
-    constructor() {}
+    @HostBinding('class.dsh-side-menu-item') root = true;
+
+    private _selected = false;
+    @HostBinding('class.dsh-side-menu-item-selected')
+    @Input()
+    get selected() {
+        return this._selected;
+    }
+    set selected(selected) {
+        this._selected = selected !== false;
+    }
 }
--- src/app/side-menu/side-menu.component.ts
+++ src/app/side-menu/side-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 
 @Component({
     selector: 'dsh-side-menu',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
     styleUrls: ['side-menu.component.scss']
 })
 export class SideMenuComponent {
-    constructor() {}
+    @HostBinding('class.dsh-side-menu') root = true;
 }
