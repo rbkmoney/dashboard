69cdf0c5d441e92994578a4bb524cf60cd34a8a5
Rinat Arsaev <krickray@gmail.com>
2019-06-13 19:51:34 +0300

    add nav component

--- src/app/container/container.module.ts
+++ src/app/container/container.module.ts
@@ -5,10 +5,11 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { ContainerComponent } from './container.component';
 import { ToolbarModule } from './toolbar';
 import { StateNavModule } from '../state-nav';
+import { NavComponent } from './nav';
 
 @NgModule({
     imports: [ToolbarModule, RouterModule, StateNavModule, FlexLayoutModule],
-    declarations: [ContainerComponent],
+    declarations: [ContainerComponent, NavComponent],
     exports: [ContainerComponent]
 })
 export class ContainerModule {}
