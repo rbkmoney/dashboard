6a85e1a52131db6cb010b0ca3c251de4a7a3ede6
Rinat Arsaev <krickray@gmail.com>
2019-06-14 15:04:27 +0300

    add icons

--- src/app/container/container.module.ts
+++ src/app/container/container.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatIconModule } from '@angular/material';
 
 import { ContainerComponent } from './container.component';
 import { ToolbarModule } from './toolbar';
@@ -8,7 +9,7 @@ import { StateNavModule } from '../state-nav';
 import { NavComponent } from './nav';
 
 @NgModule({
-    imports: [ToolbarModule, RouterModule, StateNavModule, FlexLayoutModule],
+    imports: [ToolbarModule, RouterModule, StateNavModule, FlexLayoutModule, MatIconModule],
     declarations: [ContainerComponent, NavComponent],
     exports: [ContainerComponent]
 })
