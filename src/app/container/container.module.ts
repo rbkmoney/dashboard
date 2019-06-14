c78f41ebbde680640764c3cee46895d965c00722
Rinat Arsaev <krickray@gmail.com>
2019-06-14 15:21:17 +0300

    FE-852: Side menu component (#37)

--- src/app/container/container.module.ts
+++ src/app/container/container.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatIconModule } from '@angular/material';
 
 import { ContainerComponent } from './container.component';
 import { ToolbarModule } from './toolbar';
+import { StateNavModule } from '../state-nav';
+import { NavComponent } from './nav';
 
 @NgModule({
-    imports: [ToolbarModule, RouterModule],
-    declarations: [ContainerComponent],
+    imports: [ToolbarModule, RouterModule, StateNavModule, FlexLayoutModule, MatIconModule],
+    declarations: [ContainerComponent, NavComponent],
     exports: [ContainerComponent]
 })
 export class ContainerModule {}
