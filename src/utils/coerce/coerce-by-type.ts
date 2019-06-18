1d4abc9647972888ec80f49fb15868837cc5fa0b
Rinat Arsaev <krickray@gmail.com>
2019-06-18 13:05:50 +0300

    fix coercions

--- src/utils/coerce/coerce-by-type.ts
+++ src/utils/coerce/coerce-by-type.ts
@@ -8,8 +8,22 @@ import {
 
 import { coerce } from './coerce';
 
-export const coerceBoolean = coerce(coerceBooleanProperty);
-export const coerceNumber = coerce(coerceNumberProperty);
-export const coerceArray = coerce(coerceArrayProperty);
-export const coercePixel = coerce(coerceCssPixelValue);
-export const coerceElement = coerce(coerceElementProperty);
+export function coerceBoolean(target, key) {
+    return coerce(coerceBooleanProperty)(target, key);
+}
+
+export function coerceNumber(target, key) {
+    return coerce(coerceNumberProperty)(target, key);
+}
+
+export function coerceArray(target, key) {
+    return coerce(coerceArrayProperty)(target, key);
+}
+
+export function coercePixel(target, key) {
+    return coerce(coerceCssPixelValue)(target, key);
+}
+
+export function coerceElement(target, key) {
+    return coerce(coerceElementProperty)(target, key);
+}
