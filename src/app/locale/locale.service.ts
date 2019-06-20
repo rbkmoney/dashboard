7ac1aee5512531c82377383f784738909078bd57
Alexandra Usacheva <lazercore@me.com>
2019-06-20 19:10:08 +0300

    FE-861: finish

--- src/app/locale/locale.service.ts
+++ src/app/locale/locale.service.ts
@@ -18,8 +18,7 @@ export class LocaleService {
     constructor(private http: HttpClient, private settingsService: SettingsService) {}
 
     async init() {
-        const lang = this.settingsService.language;
-        registerLocaleData(angularLocaleData[lang], lang);
+        const lang = this.settingsService.language;registerLocaleData(angularLocaleData[lang], lang);
         const path = `/assets/locales/${lang}.json`;
 
         this.locale = await this.http
