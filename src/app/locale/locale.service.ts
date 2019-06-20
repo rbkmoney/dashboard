313c5278dfa60c87140b1d0a4d15e5bfd22f573a
Alexandra Usacheva <lazercore@me.com>
2019-06-20 17:27:51 +0300

    FE-861: finish

--- src/app/locale/locale.service.ts
+++ src/app/locale/locale.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError, retry } from 'rxjs/operators';
+import { catchError } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { registerLocaleData } from '@angular/common';
 import localeRu from '@angular/common/locales/ru';
@@ -30,7 +30,6 @@ export class LocaleService {
         this.locale = await this.http
             .get(path)
             .pipe(
-                retry(5),
                 catchError(err => {
                     console.error(err);
                     return new Observable(observer => {
