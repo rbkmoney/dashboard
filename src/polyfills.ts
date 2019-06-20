d6b68f265c5f35ff973961f034019eb17190cb8b
Rinat Arsaev <krickray@gmail.com>
2019-06-20 16:58:05 +0300

    revert polyfills

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,3 +1,4 @@
+// tslint:disable:ordered-imports
 /**
  * This file includes polyfills needed by Angular and is loaded before the app.
  * You can add your own extra polyfills to this file.
@@ -73,8 +74,8 @@
  * Zone JS is required by default for Angular itself.
  */
 import 'zone.js/dist/zone'; // Included with Angular CLI.
-import 'hammerjs';
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+import 'hammerjs';
