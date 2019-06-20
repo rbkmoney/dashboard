512ba29eee9d54ea24e4fb3c1a331de1a30764a7
Alexandra Usacheva <lazercore@me.com>
2019-06-20 17:07:07 +0300

    FE-861: finish

--- src/app/locale/locale.service.ts
+++ src/app/locale/locale.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError } from 'rxjs/operators';
+import { catchError, retry } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { registerLocaleData } from '@angular/common';
 import localeRu from '@angular/common/locales/ru';
@@ -9,6 +9,9 @@ import { SettingsService } from '../settings';
 
 registerLocaleData(localeRu, 'ru');
 
+const defaultLanguage = 'ru';
+const supportedLanguages = ['ru'];
+
 @Injectable()
 export class LocaleService {
     locale = {};
@@ -16,12 +19,14 @@ export class LocaleService {
     constructor(private http: HttpClient, private settingsService: SettingsService) {}
 
     async init() {
-        const lang = this.settingsService.language;
+        let lang = this.settingsService.language;
+        lang = supportedLanguages.includes(lang) ? lang : defaultLanguage;
         const path = `/assets/locales/${lang}.json`;
 
         this.locale = await this.http
             .get(path)
             .pipe(
+                retry(5),
                 catchError(err => {
                     console.error(err);
                     return new Observable(observer => {
