bd5a75e642df586c49b7f80434d2f7808c1ce5a8
Rinat Arsaev <krickray@gmail.com>
2019-06-24 19:43:06 +0300

    fix predicate

--- src/utils/active-class/active-class.ts
+++ src/utils/active-class/active-class.ts
@@ -2,7 +2,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({ name: 'activeClass' })
 export class ActiveClassPipe implements PipeTransform {
-    transform(value: string, predicate: boolean): string {
-        return predicate ? ' ' + value.trim() : '';
+    transform(value: string, predicate: any): string {
+        return predicate ? ` ${value.trim()}` : '';
     }
 }
