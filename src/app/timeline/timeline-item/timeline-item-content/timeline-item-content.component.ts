9d5b209aef38df30c0960ba5ffc9e191625908e7
Rinat Arsaev <krickray@gmail.com>
2019-06-24 19:29:08 +0300

    refactor

--- src/app/timeline/timeline-item/timeline-item-content/timeline-item-content.component.ts
+++ src/app/timeline/timeline-item/timeline-item-content/timeline-item-content.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation, HostBinding } from '@angular/core';
 
 @Component({
-    selector: 'dsh-timeline-item-content',
-    templateUrl: 'timeline-item-content.component.html',
-    styleUrls: ['timeline-item-content.component.scss']
+    selector: 'dsh-timeline-item-content, [dsh-timeline-item-content]',
+    template: '<ng-content></ng-content>',
+    styleUrls: ['timeline-item-content.component.scss'],
+    encapsulation: ViewEncapsulation.None
 })
-export class TimelineItemContentComponent {}
+export class TimelineItemContentComponent {
+    @HostBinding('class.dsh-timeline-item-content') rootClass = true;
+}
