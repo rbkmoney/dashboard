59ceca929962db93e2c5e89801f166413d49a59c
Rinat Arsaev <krickray@gmail.com>
2019-06-24 16:44:57 +0300

    init expand panel

--- src/app/expand-panel/expand-panel.component.ts
+++ src/app/expand-panel/expand-panel.component.ts
@@ -1,8 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, ContentChild, Input } from '@angular/core';
+
+import { ExpandPanelContentComponent } from './expand-panel-content.component';
+import { coerceBoolean } from '../../utils';
+import { ExpandState, expandAnimation } from './expand-animation';
 
 @Component({
     selector: 'dsh-expand-panel',
     templateUrl: 'expand-panel.component.html',
-    styleUrls: ['expand-panel.component.scss']
+    styleUrls: ['expand-panel.component.scss'],
+    animations: [expandAnimation]
 })
-export class ExpandPanelComponent {}
+export class ExpandPanelComponent {
+    @Input()
+    @coerceBoolean
+    expanded = false;
+
+    height = 0;
+
+    expandTrigger: any = ExpandState.collapsed;
+
+    get expandable() {
+        return !!this.expandPanelContentComponent;
+    }
+
+    @ContentChild(ExpandPanelContentComponent) expandPanelContentComponent: ExpandPanelContentComponent;
+
+    toggle() {
+        this.expanded = !this.expanded;
+        if (!this.expanded) {
+            this.expandTrigger = ExpandState.collapsed;
+        }
+    }
+
+    updateExpandTrigger(height = 0) {
+        console.log(height);
+        this.expandTrigger = height ? { value: ExpandState.expanded, params: { height } } : ExpandState.collapsed;
+    }
+}
--- src/app/expand-panel/expand-panel.module.ts
+++ src/app/expand-panel/expand-panel.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { ExpandPanelComponent } from './expand-panel.component';
+import { ExpandPanelContentComponent } from './expand-panel-content.component';
+import { ResizedModule } from '../resized';
 
 @NgModule({
-    imports: [],
-    declarations: [ExpandPanelComponent],
-    exports: [ExpandPanelComponent]
+    imports: [CommonModule, MatIconModule, FlexLayoutModule, ResizedModule],
+    declarations: [ExpandPanelComponent, ExpandPanelContentComponent],
+    exports: [ExpandPanelComponent, ExpandPanelContentComponent]
 })
 export class ExpandPanelModule {}
