205a813eecf582ca6b2016b16d824d9e37a220c2
DenisEzhov <d.ezhov@rbkmoney.com>
2019-07-09 17:01:57 +0300

    fix?

--- src/app/sections/payment-details/payment-details.component.ts
+++ src/app/sections/payment-details/payment-details.component.ts
@@ -26,7 +26,7 @@ export class PaymentDetailsComponent implements OnInit {
             id: 'H3dg32Hd2',
             invoiceID: 'J3hd76G63bd2G',
             shopID: 'h83hd3s63b23f',
-            createdAt: '2019-07-12T14:46:15Z',
+            createdAt: '2019-07-12T14:46:15Z' as any,
             amount: 1500000,
             currency: 'RUB',
             fee: 16500,
@@ -47,7 +47,7 @@ export class PaymentDetailsComponent implements OnInit {
             error: {
                 code: 'Недостаточно средств'
             },
-            statusChangedAt: '2019-07-08T14:46:15Z',
+            statusChangedAt: '2019-07-08T14:46:15Z' as any,
             makeRecurrent: true
         } as PaymentSearchResult;
 
