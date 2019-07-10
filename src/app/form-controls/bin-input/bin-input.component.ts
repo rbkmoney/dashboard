28371c6e11d5da56a9871c2b6967f392bc1063fb
Alexandra Usacheva <lazercore@me.com>
2019-07-10 14:40:45 +0300

    custom inputs... :fox_face:

--- src/app/form-controls/bin-input/bin-input.component.ts
+++ src/app/form-controls/bin-input/bin-input.component.ts
@@ -1,110 +1,32 @@
-import { FocusMonitor } from '@angular/cdk/a11y';
-import { coerceBooleanProperty } from '@angular/cdk/coercion';
-import { Component, ElementRef, Input, OnDestroy, HostBinding } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { Component, forwardRef, HostBinding } from '@angular/core';
 import { MatFormFieldControl } from '@angular/material/form-field';
-import { Subject } from 'rxjs';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
 
 import { binMask } from './bin-input-mask';
+import { FormControlBoilerplate } from '../form-control-boilerplate';
 
 @Component({
     selector: 'dsh-bin-input',
     templateUrl: 'bin-input.component.html',
     styleUrls: ['bin-input.component.scss'],
-    providers: [{ provide: MatFormFieldControl, useExisting: BINInputComponent }]
+    providers: [
+        { provide: MatFormFieldControl, useExisting: BINInputComponent },
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: forwardRef(() => BINInputComponent),
+            multi: true
+        }
+    ]
 })
-export class BINInputComponent implements MatFormFieldControl<number>, OnDestroy {
+export class BINInputComponent extends FormControlBoilerplate {
     static nextId = 0;
-
-    @HostBinding('class.floating')
-    get shouldLabelFloat() {
-        return this.focused || !this.empty;
-    }
 
     @HostBinding('id')
     id = `dsh-bin-input-${BINInputComponent.nextId++}`;
 
-    @HostBinding('attr.aria-describedby')
-    describedBy = '';
-
-    formControl: FormControl;
-    stateChanges = new Subject<void>();
-    focused = false;
-    ngControl = null;
-    errorState = false;
     controlType = 'dsh-bin-input';
-
-    get empty() {
-        return !this.formControl.value;
-    }
-
-    @Input()
-    get placeholder(): string {
-        return this._placeholder;
-    }
-    set placeholder(value: string) {
-        this._placeholder = value;
-        this.stateChanges.next();
-    }
-    private _placeholder: string;
-
-    @Input()
-    get required(): boolean {
-        return this._required;
-    }
-    set required(value: boolean) {
-        this._required = coerceBooleanProperty(value);
-        this.stateChanges.next();
-    }
-    private _required = false;
-
-    @Input()
-    get disabled(): boolean {
-        return this._disabled;
-    }
-    set disabled(value: boolean) {
-        this._disabled = coerceBooleanProperty(value);
-        this._disabled ? this.formControl.disable() : this.formControl.enable();
-        this.stateChanges.next();
-    }
-    private _disabled = false;
-
-    @Input()
-    get value(): number {
-        return this.formControl.value.replace(/\D/g, '');
-    }
-    set value(v: number) {
-        this.formControl.setValue(v);
-        this.stateChanges.next();
-    }
 
     get mask() {
         return binMask;
-    }
-
-    constructor(private fm: FocusMonitor, private elRef: ElementRef<HTMLElement>) {
-        this.formControl = new FormControl();
-        fm.monitor(elRef, true).subscribe(origin => {
-            this.focused = !!origin;
-            this.stateChanges.next();
-        });
-    }
-
-    ngOnDestroy() {
-        this.stateChanges.complete();
-        this.fm.stopMonitoring(this.elRef);
-    }
-
-    setDescribedByIds(ids: string[]) {
-        this.describedBy = ids.join(' ');
-    }
-
-    onContainerClick(event: MouseEvent) {
-        if ((event.target as Element).tagName.toLowerCase() !== 'input') {
-            const input = this.elRef.nativeElement.querySelector('input');
-            if (input) {
-                input.focus();
-            }
-        }
     }
 }
---
+++ src/app/form-controls/form-control-boilerplate.ts
@@ -0,0 +1,127 @@
+import { MatFormFieldControl } from '@angular/material';
+import { DoCheck, ElementRef, HostBinding, Injector, Input, OnDestroy, OnInit } from '@angular/core';
+import { ControlValueAccessor, FormControl, NgControl } from '@angular/forms';
+import { Subject } from 'rxjs';
+import { coerceBooleanProperty } from '@angular/cdk/coercion';
+import { FocusMonitor } from '@angular/cdk/a11y';
+
+export class FormControlBoilerplate
+    implements MatFormFieldControl<any>, OnInit, OnDestroy, DoCheck, ControlValueAccessor {
+    @HostBinding('class.floating')
+    get shouldLabelFloat() {
+        return this.focused || !this.empty;
+    }
+
+    @HostBinding('attr.aria-describedby')
+    describedBy = '';
+
+    formControl: FormControl;
+    stateChanges = new Subject<void>();
+    focused = false;
+    ngControl = null;
+    id = null;
+    errorState = false;
+
+    get empty() {
+        return !this._value;
+    }
+
+    private _placeholder: string;
+    @Input()
+    get placeholder(): string {
+        return this._placeholder;
+    }
+    set placeholder(value: string) {
+        this._placeholder = value;
+        this.stateChanges.next();
+    }
+
+    private _required = false;
+    @Input()
+    get required(): boolean {
+        return this._required;
+    }
+    set required(value: boolean) {
+        this._required = coerceBooleanProperty(value);
+        this.stateChanges.next();
+    }
+
+    private _disabled = false;
+    @Input()
+    get disabled(): boolean {
+        return this._disabled;
+    }
+    set disabled(value: boolean) {
+        this._disabled = coerceBooleanProperty(value);
+        this._disabled ? this.formControl.disable() : this.formControl.enable();
+        this.stateChanges.next();
+    }
+
+    _value: any = '';
+    get value(): any {
+        return this.formControl.value;
+    }
+    set value(value) {
+        this._value = value;
+        this.formControl.setValue(this._value);
+        this.onChange(value);
+        this.stateChanges.next();
+    }
+
+    constructor(private fm: FocusMonitor, private elRef: ElementRef<HTMLElement>, public injector: Injector) {
+        this.formControl = new FormControl();
+        fm.monitor(elRef, true).subscribe(origin => {
+            this.focused = !!origin;
+            this.stateChanges.next();
+        });
+    }
+
+    ngOnInit() {
+        this.ngControl = this.injector.get(NgControl);
+        if (this.ngControl != null) {
+            this.ngControl.valueAccessor = this;
+        }
+    }
+
+    ngOnDestroy() {
+        this.stateChanges.complete();
+        this.fm.stopMonitoring(this.elRef);
+    }
+
+    ngDoCheck(): void {
+        if (this.ngControl) {
+            this.errorState = this.ngControl.invalid && this.ngControl.touched;
+            this.stateChanges.next();
+        }
+    }
+
+    setDescribedByIds(ids: string[]) {
+        this.describedBy = ids.join(' ');
+    }
+
+    onContainerClick(event: MouseEvent) {
+        if ((event.target as Element).tagName.toLowerCase() !== 'input') {
+            const input = this.elRef.nativeElement.querySelector('input');
+            if (input) {
+                input.focus();
+            }
+        }
+    }
+
+    onChange = (value: any) => {};
+
+    onTouched = () => {};
+
+    writeValue(value: any): void {
+        this.value = value;
+        this.formControl.setValue(value);
+    }
+
+    registerOnChange(fn: (v: any) => void): void {
+        this.onChange = fn;
+    }
+
+    registerOnTouched(fn: () => void): void {
+        this.onTouched = fn;
+    }
+}
