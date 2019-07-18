7d5ab4ae84d8d29b337ce003c624542f0ae850b3
Rinat Arsaev <krickray@gmail.com>
2019-07-18 17:03:57 +0300

    to links$

--- src/app/sections/claims/claim/claim.component.ts
+++ src/app/sections/claims/claim/claim.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
+import { map, shareReplay } from 'rxjs/operators';
 
 import { ClaimService } from './claim.service';
 import { ViewClaim, ClaimsService } from '../../../claims';
@@ -11,20 +12,26 @@ import { ViewClaim, ClaimsService } from '../../../claims';
     styleUrls: ['claim.component.scss']
 })
 export class ClaimComponent {
-    links = [
-        {
-            path: 'conversation',
-            label: 'sections.claim.conversation.label'
-        },
-        {
-            path: 'changes',
-            label: 'sections.claim.changes.label'
-        },
-        {
-            path: 'documents',
-            label: 'sections.claim.documents.label'
-        }
-    ];
+    links$ = this.claim$.pipe(
+        map(({ id }) => {
+            const getPath = (p: string) => `/claim/${id}/${p}`;
+            return [
+                {
+                    path: getPath('conversation'),
+                    label: 'sections.claim.conversation.label'
+                },
+                {
+                    path: getPath('changes'),
+                    label: 'sections.claim.changes.label'
+                },
+                {
+                    path: getPath('documents'),
+                    label: 'sections.claim.documents.label'
+                }
+            ];
+        }),
+        shareReplay(1)
+    );
 
     get claim$(): Observable<ViewClaim> {
         return this.claimService.claim$;
