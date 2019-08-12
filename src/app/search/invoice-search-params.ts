60e7671de9dfdcaf96e91b5f7e60aef73c0cacdc
Alexandra Usacheva <lazercore@me.com>
2019-08-12 16:48:59 +0300

    🦊 typings 🦊

--- src/app/search/invoice-search-params.ts
+++ src/app/search/invoice-search-params.ts
@@ -1,1 +1,37 @@
-export interface InvoiceSearchParams {}
+import { Moment } from 'moment';
+
+import {
+    BankCardPaymentSystem,
+    BankCardTokenProvider,
+    InvoiceStatus,
+    PaymentFlow,
+    PaymentMethod,
+    PaymentStatus,
+    PaymentTerminalProvider
+} from '../api/capi/swagger-codegen';
+
+export interface InvoiceSearchParams {
+    fromTime: Moment;
+    toTime: Moment;
+    limit: number;
+    shopID?: string;
+    invoiceStatus?: InvoiceStatus.StatusEnum;
+    paymentStatus?: PaymentStatus.StatusEnum;
+    paymentFlow?: PaymentFlow.TypeEnum;
+    paymentMethod?: PaymentMethod.MethodEnum;
+    paymentTerminalProvider?: PaymentTerminalProvider;
+    invoiceID?: string;
+    paymentID?: string;
+    payerEmail?: string;
+    payerIP?: string;
+    payerFingerprint?: string;
+    customerID?: string;
+    bankCardTokenProvider?: BankCardTokenProvider;
+    bankCardPaymentSystem?: BankCardPaymentSystem;
+    first6?: string;
+    last4?: string;
+    paymentAmount?: number;
+    invoiceAmount?: number;
+    excludedShops?: string[];
+    continuationToken?: string;
+}
