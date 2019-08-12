351958225f25e50e28c85dc28d0e6dd201f8c73a
Rinat Arsaev <krickray@gmail.com>
2019-08-12 15:26:56 +0300

    fix ClaimsService types

--- src/app/claims/claims.service.ts
+++ src/app/claims/claims.service.ts
@@ -2,25 +2,22 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { genXRequestID } from '../api';
-import { ClaimsService as APIClaimsService, Claim } from '../api/claim-management';
+import { ClaimsService as APIClaimsService, Claim, StatusModificationUnit } from '../api/claim-management';
 import { ClaimsWithToken } from './claims-with-token';
-
-type searchClaimsParams = Parameters<APIClaimsService['searchClaims']>;
-type getClaimByIDParams = Parameters<APIClaimsService['getClaimByID']>;
 
 @Injectable()
 export class ClaimsService {
     constructor(private claimsService: APIClaimsService) {}
 
     searchClaims(
-        limit: searchClaimsParams[1],
-        continuationToken?: searchClaimsParams[3],
-        claimStatuses?: searchClaimsParams[4]
+        limit: number,
+        continuationToken?: string,
+        claimStatuses?: StatusModificationUnit.StatusEnum[]
     ): Observable<ClaimsWithToken> {
         return this.claimsService.searchClaims(genXRequestID(), limit, undefined, continuationToken, claimStatuses);
     }
 
-    getClaimByID(claimID: getClaimByIDParams[1]): Observable<Claim> {
+    getClaimByID(claimID: number): Observable<Claim> {
         return this.claimsService.getClaimByID(genXRequestID(), claimID);
     }
 }
