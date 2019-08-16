fe862d1c8e1e28c171eacfd9f444829609a01ce4
Rinat Arsaev <krickray@gmail.com>
2019-08-16 15:35:40 +0300

    refactor tests

--- src/app/sections/claim/claim.service.spec.ts
+++ src/app/sections/claim/claim.service.spec.ts
@@ -9,45 +9,42 @@ function assertDeepEqual(actual: any, expected: any) {
     expect(actual).toEqual(expected);
 }
 
+function createScheduler() {
+    return new TestScheduler(assertDeepEqual);
+}
+
 describe('ClaimService', () => {
-    function createClaimService(claim: ColdObservable<string>, params: ColdObservable<any>) {
+    function createClaimService(getClaim: () => ColdObservable<string>, params: ColdObservable<any>) {
         return new ClaimService(
-            { getClaimByID: (..._: any[]): any => claim } as ClaimsService,
+            { getClaimByID: (..._: any[]): any => getClaim() } as ClaimsService,
             { params: params as any } as ActivatedRoute
         );
     }
 
     it('should load claim', () => {
-        new TestScheduler(assertDeepEqual).run(helpers => {
-            const { cold, expectObservable } = helpers;
-            const service = createClaimService(cold('-x|'), cold('-a-|', { a: { id: '10' } }));
+        createScheduler().run(({ cold, expectObservable }) => {
+            const service = createClaimService(() => cold('-x|'), cold('-a-|', { a: { id: '10' } }));
             expectObservable(service.claim$).toBe('--x|');
         });
     });
 
     it('should load 2 claims', () => {
-        new TestScheduler(assertDeepEqual).run(helpers => {
-            const { cold, expectObservable, flush } = helpers;
-            const service = createClaimService(cold('-x|'), cold('-a-b-|', { a: { id: '10' }, b: { id: '20' } }));
+        createScheduler().run(({ cold, expectObservable }) => {
+            const service = createClaimService(() => cold('-x|'), cold('-a-b-|', { a: { id: '10' }, b: { id: '20' } }));
             expectObservable(service.claim$).toBe('--x-x|');
         });
     });
 
     it('empty and wrong id shouldnt load claim', () => {
-        new TestScheduler(assertDeepEqual).run(helpers => {
-            const { cold, expectObservable } = helpers;
-            const service = createClaimService(cold('-x|'), cold('-a-b-|', { a: {}, b: { id: 'test' } }));
+        createScheduler().run(({ cold, expectObservable }) => {
+            const service = createClaimService(() => cold('-x|'), cold('-a-b-|', { a: {}, b: { id: 'test' } }));
             expectObservable(service.claim$).toBe('-----|');
         });
     });
 
     it('duplicate id shouldnt load claim', () => {
-        new TestScheduler(assertDeepEqual).run(helpers => {
-            const { cold, expectObservable } = helpers;
-            const service = createClaimService(
-                cold('-x|'),
-                cold('-a-b-|', { a: { id: '20' }, b: { id: { id: '20' } } })
-            );
+        createScheduler().run(({ cold, expectObservable }) => {
+            const service = createClaimService(() => cold('-x|'), cold('-a-b-|', { a: { id: '20' }, b: { id: '20' } }));
             expectObservable(service.claim$).toBe('--x--|');
         });
     });
