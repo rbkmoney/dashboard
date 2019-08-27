e7704408d17df8292f6aa50e142a686a87a115a2
Ildar Galeev <KeinAsylum@gmail.com>
2019-08-27 12:03:10 +0300

    Used partial fetcher for payment details refunds

--- src/app/sections/payment-details/refunds/refunds.service.ts
+++ src/app/sections/payment-details/refunds/refunds.service.ts
@@ -1,34 +1,29 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 
 import { RefundSearchService } from '../../../search';
-import { RefundSearchResult } from '../../../api/capi/swagger-codegen';
+import { RefundSearchResult } from '../../../api/capi';
+import { PartialFetcher, FetchResult } from '../../partial-fetcher';
+import { RefundsSearchParams } from './refunds-search-params';
 
 @Injectable()
-export class RefundsService {
-    private refunds$ = new BehaviorSubject<RefundSearchResult[]>([]);
+export class RefundsService extends PartialFetcher<RefundSearchResult, RefundsSearchParams> {
+    private readonly searchLimit = 3;
 
-    private hasMoreRefunds$ = new BehaviorSubject<boolean>(false);
-
-    private continuationToken: string;
-
-    constructor(private refundSearchService: RefundSearchService) {}
-
-    loadRefunds(invoiceID: string, paymentID: string) {
-        this.refundSearchService
-            .searchRefundsByDuration({ amount: 1, unit: 'y' }, invoiceID, paymentID, 3, this.continuationToken)
-            .subscribe(({ continuationToken, result }) => {
-                this.continuationToken = continuationToken;
-                this.hasMoreRefunds$.next(!!continuationToken);
-                this.refunds$.next(this.refunds$.getValue().concat(result));
-            });
+    constructor(private refundSearchService: RefundSearchService) {
+        super();
     }
 
-    refunds(): Observable<RefundSearchResult[]> {
-        return this.refunds$.asObservable();
-    }
-
-    hasMoreRefunds(): Observable<boolean> {
-        return this.hasMoreRefunds$.asObservable();
+    protected fetch(
+        { invoiceID, paymentID }: RefundsSearchParams,
+        continuationToken: string
+    ): Observable<FetchResult<RefundSearchResult>> {
+        return this.refundSearchService.searchRefundsByDuration(
+            { amount: 1, unit: 'y' },
+            invoiceID,
+            paymentID,
+            this.searchLimit,
+            continuationToken
+        );
     }
 }
