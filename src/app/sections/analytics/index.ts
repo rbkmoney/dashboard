d1529c5caae0b816ec99c83883647a382d4e74e6
Ildar Galeev <KeinAsylum@gmail.com>
2019-08-29 12:57:04 +0300

    Remove / disabled rudiment functionality. Set up lazy load route config.

--- src/app/sections/analytics/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './analytics.module';
-export * from './analytics.component';
--- src/app/sections/page-not-found/page-not-found.module.ts
+++ src/app/sections/page-not-found/page-not-found.module.ts
@@ -3,10 +3,11 @@ import { RouterModule } from '@angular/router';
 
 import { PageNotFoundComponent } from './page-not-found.component';
 import { LocaleModule } from '../../locale/locale.module';
+import { PageNotFoundRoutingModule } from './page-not-found-routing.module';
 
 @NgModule({
     declarations: [PageNotFoundComponent],
-    imports: [RouterModule, LocaleModule],
+    imports: [RouterModule, LocaleModule, PageNotFoundRoutingModule],
     exports: [PageNotFoundComponent]
 })
 export class PageNotFoundModule {}
