cbf2af3e20a517903e931aef3fe6dded906b45cf
Rinat Arsaev <krickray@gmail.com>
2019-09-05 18:37:09 +0300

    ft/FE-920: Add questionary API (#82)

---
+++ src/app/api/questionary/questionary.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+
+import { QuestionaryService } from './questionary.service';
+
+@NgModule({
+    providers: [QuestionaryService]
+})
+export class QuestionaryModule {}
