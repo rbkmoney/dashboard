f36a893249426ec3c6257c20ccc1d74b14091e9a
Ildar Galeev <KeinAsylum@gmail.com>
2019-09-05 17:55:20 +0300

    Wetkitty config adaptation

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,5 +1,5 @@
 import { getBaseClass } from '../../utils';
 
-export type Config = typeof import('../../assets/appConfig.json');
+export type Config = typeof import('../../appConfig.json');
 
 export const BaseConfig = getBaseClass<Config>();
