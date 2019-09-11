cb72bda719e84be7b98f73b85e08e463ee7b286c
Rinat Arsaev <krickray@gmail.com>
2019-09-11 19:26:09 +0300

    add data selection

--- src/app/sections/questionary/beneficial-owner/get-data.ts
+++ src/app/sections/questionary/beneficial-owner/get-data.ts
@@ -1,8 +1,46 @@
-import { BeneficialOwner } from '../../../api-codegen/questionary';
+import { BeneficialOwner, RussianDomesticPassport, IndividualResidencyInfo } from '../../../api-codegen/questionary';
+import { getFIO, toYesNo } from '../select-data';
 
 export function getData(beneficialOwner: BeneficialOwner) {
     console.log(beneficialOwner);
+    const { russianPrivateEntity, migrationCardInfo, residenceApprove } = beneficialOwner;
+    const identityDocument = beneficialOwner.identityDocument as RussianDomesticPassport;
+    // TODO: определять тип
+    const residencyInfo = beneficialOwner.residencyInfo as IndividualResidencyInfo;
     return {
-        test: 1
+        indicatedInDocuments: -1, // TODO
+        ownershipPercentage: beneficialOwner.ownershipPercentage,
+        fio: getFIO(russianPrivateEntity.personAnthroponym),
+        birthDate: russianPrivateEntity.birthDate,
+        birthPlace: russianPrivateEntity.birthPlace,
+        citizenship: russianPrivateEntity.citizenship,
+        inn: beneficialOwner.inn,
+        identityDocument: {
+            name: 'Паспорт РФ',
+            series: identityDocument.series,
+            number: identityDocument.number,
+            issuer: [identityDocument.issuer, identityDocument.issuerCode].filter(i => !!i).join(', '),
+            issuedAt: identityDocument.issuedAt
+        },
+        migrationCardInfo: {
+            cardNumber: migrationCardInfo.cardNumber,
+            beginningDate: migrationCardInfo.beginningDate,
+            expirationDate: migrationCardInfo.expirationDate
+        },
+        residenceApprove: {
+            name: residenceApprove.name,
+            series: residenceApprove.series,
+            number: residenceApprove.number,
+            beginningDate: residenceApprove.beginningDate,
+            expirationDate: residenceApprove.expirationDate
+        },
+        address: russianPrivateEntity.actualAddress,
+        pdl: {
+            pdlCategory: toYesNo(beneficialOwner.pdlCategory),
+            pdlRelation: toYesNo(!!beneficialOwner.pdlRelationDegree),
+            pdlRelationDegree: beneficialOwner.pdlRelationDegree,
+            usaTaxResident: toYesNo(residencyInfo.usaTaxResident),
+            exceptUsaTaxResident: toYesNo(residencyInfo.exceptUsaTaxResident)
+        }
     };
 }
