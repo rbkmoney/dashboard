270ee6048fdd6ad8addd42a8155cf798d52ae62b
Aleksandra Usacheva <lazercore@me.com>
2019-09-23 20:47:39 +0300

    🦊 prettify 🦊

--- src/app/sections/payment-section/operations/operators/remove-empty-properties.ts
+++ src/app/sections/payment-section/operations/operators/remove-empty-properties.ts
@@ -3,4 +3,8 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 export const removeEmptyProperties = <T>(s: Observable<T>) =>
-    s.pipe(map( obj => Object.keys(obj).reduce((acc, cur) => (!isNil(obj[cur]) ? { ...acc, [cur]: obj[cur] } : acc), {} as T)));
+    s.pipe(
+        map(obj =>
+            Object.keys(obj).reduce((acc, cur) => (!isNil(obj[cur]) ? { ...acc, [cur]: obj[cur] } : acc), {} as T)
+        )
+    );
