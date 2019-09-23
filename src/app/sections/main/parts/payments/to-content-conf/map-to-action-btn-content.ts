973763e9e4a1ae431d5a199bf49e877cf99b77ee
DenisEzhov <d.ezhov@rbkmoney.com>
2019-09-23 18:59:16 +0300

    to-content-conf updated for transloco

--- src/app/sections/main/parts/payments/to-content-conf/map-to-action-btn-content.ts
+++ src/app/sections/main/parts/payments/to-content-conf/map-to-action-btn-content.ts
@@ -5,34 +5,34 @@ import { Claim, StatusModificationUnit } from '../../../../../api-codegen/claim-
 import { routeEnv } from '../../../../route-env';
 import { ActionBtnContent } from '../content-config';
 
-const toActionBtnContent = (p, actionPath, routerLink): ActionBtnContent => ({
+const toActionBtnContent = (actionLabel: string, routerLink: string): ActionBtnContent => ({
     routerLink,
-    actionLabel: `${p}.action.${actionPath}`,
+    actionLabel,
     disabled: false
 });
 
-const claimToActionBtnContent = (path: string, claim: Claim | null): ActionBtnContent => {
+const claimToActionBtnContent = (claim: Claim | null): ActionBtnContent => {
     if (claim === null) {
-        return toActionBtnContent(path, 'join', '/onboarding');
+        return toActionBtnContent('join', '/onboarding');
     }
     const s = StatusModificationUnit.StatusEnum;
     switch (claim.status) {
         case s.Pending:
-            return toActionBtnContent(path, 'continue', `/onboarding/${claim.id}`);
+            return toActionBtnContent('continue', `/onboarding/${claim.id}`);
         case s.Review:
-            return toActionBtnContent(path, 'claimDetails', `/claim/${claim.id}`);
+            return toActionBtnContent('claimDetails', `/claim/${claim.id}`);
         case s.PendingAcceptance:
-            return toActionBtnContent(path, 'claimDetails', `/claim/${claim.id}`);
+            return toActionBtnContent('claimDetails', `/claim/${claim.id}`);
     }
     throw new Error('Unsupported claim status');
 };
 
-export const mapToActionBtnContent = (basePath: string, claim: Observable<Claim>) => (
+export const mapToActionBtnContent = (claim: Observable<Claim>) => (
     s: Observable<boolean>
 ): Observable<ActionBtnContent> => {
     const realEnvContent = of(
-        toActionBtnContent(basePath, 'details', `/payment-section/env/${routeEnv['1']}/operations`)
+        toActionBtnContent('details', `/payment-section/env/${routeEnv['1']}/operations`)
     );
-    const fromClaimContent = claim.pipe(map(c => claimToActionBtnContent(basePath, c)));
+    const fromClaimContent = claim.pipe(map(c => claimToActionBtnContent(c)));
     return s.pipe(switchMap(isRealEnv => iif(() => isRealEnv, realEnvContent, fromClaimContent)));
 };
