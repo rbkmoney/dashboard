823527cc5a489efaff9c0f1b5e49869f1d1c7b5e
Aleksandra Usacheva <lazercore@me.com>
2019-09-23 22:43:14 +0300

    🦊 fix remove empty operator 🦊

--- src/app/sections/payment-section/operations/operators/remove-empty-properties.ts
+++ src/app/sections/payment-section/operations/operators/remove-empty-properties.ts
@@ -1,10 +1,10 @@
-import isNil from 'lodash.isnil';
+import isEmpty from 'lodash.isempty';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 export const removeEmptyProperties = <T>(s: Observable<T>) =>
     s.pipe(
         map(obj =>
-            Object.keys(obj).reduce((acc, cur) => (!isNil(obj[cur]) ? { ...acc, [cur]: obj[cur] } : acc), {} as T)
+            Object.keys(obj).reduce((acc, cur) => (!isEmpty(obj[cur])  ? { ...acc, [cur]: obj[cur] } : acc), {} as T)
         )
     );
