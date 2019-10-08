6fdee2f708cd4181db30cb2ed5cf2cf9f294080f
Rinat Arsaev <krickray@gmail.com>
2019-10-08 19:50:06 +0300

    add generateReport

--- src/app/sections/payment-section/reports/reports.module.ts
+++ src/app/sections/payment-section/reports/reports.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { TranslocoModule } from '@ngneat/transloco';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { ReportsComponent } from './reports.component';
 import { ReportsRoutingModule } from './reports-routing.module';
+import { LayoutModule } from '../../../layout';
+import { ButtonModule } from '../../../button';
 
 @NgModule({
-    imports: [TranslocoModule, ReportsRoutingModule],
+    imports: [TranslocoModule, ReportsRoutingModule, LayoutModule, ButtonModule, FlexLayoutModule],
     declarations: [ReportsComponent],
     exports: [ReportsComponent]
 })
