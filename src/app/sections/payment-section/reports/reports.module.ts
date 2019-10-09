6316ebde485eb222f1ee0b810e8b6845f5f72efb
Rinat Arsaev <krickray@gmail.com>
2019-10-09 15:07:58 +0300

    init search-form

--- src/app/sections/payment-section/reports/reports.module.ts
+++ src/app/sections/payment-section/reports/reports.module.ts
@@ -1,15 +1,26 @@
 import { NgModule } from '@angular/core';
 import { TranslocoModule } from '@ngneat/transloco';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatFormFieldModule, MatDatepickerModule, MatSelectModule } from '@angular/material';
 
 import { ReportsComponent } from './reports.component';
 import { ReportsRoutingModule } from './reports-routing.module';
 import { LayoutModule } from '../../../layout';
 import { ButtonModule } from '../../../button';
+import { SearchFormComponent } from './search-form';
 
 @NgModule({
-    imports: [TranslocoModule, ReportsRoutingModule, LayoutModule, ButtonModule, FlexLayoutModule],
-    declarations: [ReportsComponent],
+    imports: [
+        TranslocoModule,
+        ReportsRoutingModule,
+        LayoutModule,
+        ButtonModule,
+        FlexLayoutModule,
+        MatFormFieldModule,
+        MatDatepickerModule,
+        MatSelectModule
+    ],
+    declarations: [ReportsComponent, SearchFormComponent],
     exports: [ReportsComponent]
 })
 export class ReportsModule {}
---
+++ src/app/sections/payment-section/reports/search-form/search-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+import { SearchFormService } from './search-form.service';
+
+@Component({
+    selector: 'dsh-search-form',
+    templateUrl: 'search-form.component.html',
+    providers: [SearchFormService]
+})
+export class SearchFormComponent {}
