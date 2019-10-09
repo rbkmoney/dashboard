ebc3df3d55ea16a32119865b4bb0db2b0154ca38
Rinat Arsaev <krickray@gmail.com>
2019-10-09 19:08:58 +0300

    init reports api module

---
+++ src/app/api/reports/reports.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+
+import { ReportsService as ReportsApiService, Report } from '../../api-codegen/anapi';
+import { genXRequestID } from '../gen-x-request-id';
+
+interface CreateReportReq {
+    fromTime: string;
+    toTime: string;
+    reportType: Report.ReportTypeEnum;
+    partyID: string;
+    shopID?: string;
+}
+
+interface GetReportsReq {
+    fromTime: string;
+    toTime: string;
+    reportTypes: Report.ReportTypeEnum[];
+    partyID: string;
+    shopID?: string;
+}
+
+@Injectable()
+export class ReportsService {
+    constructor(private reportsService: ReportsApiService) {}
+
+    createReport({ fromTime, toTime, reportType, partyID, shopID }: CreateReportReq) {
+        return this.reportsService.createReport(
+            genXRequestID(),
+            fromTime as any,
+            toTime as any,
+            reportType,
+            partyID,
+            undefined,
+            shopID
+        );
+    }
+
+    getReport(reportID: number) {
+        return this.reportsService.getReport(genXRequestID(), reportID);
+    }
+
+    getReports({ fromTime, toTime, reportTypes, partyID, shopID }: GetReportsReq) {
+        return this.reportsService.getReports(
+            genXRequestID(),
+            fromTime as any,
+            toTime as any,
+            reportTypes,
+            partyID,
+            undefined,
+            shopID
+        );
+    }
+
+    downloadFile(reportID: number, filedID: string) {
+        return this.reportsService.downloadFile(genXRequestID(), reportID, filedID);
+    }
+}
