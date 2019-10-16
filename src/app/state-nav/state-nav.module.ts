3ed97cdab9c23247a5d41af18b3a926ac5a22800
Aleksandra Usacheva <lazercore@me.com>
2019-10-16 18:29:39 +0300

    FE-893: build fix

--- src/app/state-nav/state-nav.module.ts
+++ src/app/state-nav/state-nav.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { FlexModule } from '@angular/flex-layout';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material';
 
 import { StateNavComponent } from './state-nav.component';
 import { StateNavItemComponent } from './state-nav-item';
-import { MatIconModule } from '@angular/material';
 
 @NgModule({
     imports: [FlexModule, CommonModule, MatIconModule],
