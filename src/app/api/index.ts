3b21bf46b462b1342186bf1b6ea40db8196b07ff
Rinat Arsaev <krickray@gmail.com>
2019-10-16 18:57:49 +0300

    add api

--- src/app/api/index.ts
+++ src/app/api/index.ts
@@ -3,4 +3,5 @@ export * from './shop';
 export * from './invoice';
 export * from './search';
 export * from './questionary';
+export * from './reports';
 export * from './dadata';
---
+++ src/app/api/reports/reports.service.ts
@@ -0,0 +1,59 @@
+import { Injectable } from '@angular/core';
+
+import { ReportsService as ReportsApiService, Report } from '../../api-codegen/anapi';
+import { genXRequestID } from '../gen-x-request-id';
+
+interface CreateReportReq {
+    fromTime: string;
+    toTime: string;
+    reportType: Report.ReportTypeEnum;
+    partyID: string;
+    shopID?: string;
+}
+
+interface SearchReportsReq {
+    fromTime: string;
+    toTime: string;
+    reportTypes: Report.ReportTypeEnum[];
+    partyID: string;
+    shopID?: string;
+    continuationToken?: string;
+}
+
+@Injectable()
+export class ReportsService {
+    constructor(private reportsService: ReportsApiService) {}
+
+    createReport({ fromTime, toTime, reportType, partyID, shopID }: CreateReportReq) {
+        return this.reportsService.createReport(
+            genXRequestID(),
+            fromTime as any,
+            toTime as any,
+            reportType,
+            partyID,
+            undefined,
+            shopID
+        );
+    }
+
+    getReport(reportID: number) {
+        return this.reportsService.getReport(genXRequestID(), reportID);
+    }
+
+    searchReports({ fromTime, toTime, reportTypes, partyID, shopID, continuationToken }: SearchReportsReq) {
+        return this.reportsService.searchReports(
+            genXRequestID(),
+            new Date(fromTime),
+            new Date(toTime),
+            reportTypes,
+            partyID,
+            undefined,
+            shopID,
+            continuationToken
+        );
+    }
+
+    downloadFile(reportID: number, filedID: string) {
+        return this.reportsService.downloadFile(genXRequestID(), reportID, filedID);
+    }
+}
