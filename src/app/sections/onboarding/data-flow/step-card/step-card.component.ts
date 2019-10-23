7b943b681220517b6e68073c378c70da483efa09
Ildar Galeev <keinasylum@gmail.com>
2019-10-23 11:22:03 +0300

    FE-926: Initial validation service

--- src/app/sections/onboarding/data-flow/step-card/step-card.component.ts
+++ src/app/sections/onboarding/data-flow/step-card/step-card.component.ts
@@ -1,6 +1,9 @@
 import { Component, Output, EventEmitter, Input } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { StepName } from '../step-flow';
+import { ValidityService } from '../validity.service';
 
 @Component({
     selector: 'dsh-step-card',
@@ -13,6 +16,12 @@ export class StepCardComponent {
 
     @Input() actionDisabled = true;
     @Output() doAction = new EventEmitter<void>();
+
+    constructor(private validityService: ValidityService) {}
+
+    getStepStatus(step: StepName): Observable<'success' | null> {
+        return this.validityService.isValid(step).pipe(map(isValid => (isValid ? 'success' : null)));
+    }
 
     selectStepFlowIndex(index: number) {
         this.stepSelected.emit(this.stepFlow[index]);
