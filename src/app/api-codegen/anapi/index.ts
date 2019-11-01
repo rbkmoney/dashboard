81f3e2c0c7199a211da3a04c486e4ab94fab8382
Rinat Arsaev <krickray@gmail.com>
2019-11-01 17:14:04 +0300

    add content-type

--- src/app/api-codegen/anapi/index.ts
+++ src/app/api-codegen/anapi/index.ts
@@ -1,2 +1,7 @@
 export * from './anapi.module';
-export * from './swagger-codegen';
+export { SearchService } from './swagger-codegen/api/api';
+export * from './swagger-codegen/model/models';
+export * from './swagger-codegen/variables';
+export * from './swagger-codegen/configuration';
+export * from './swagger-codegen/api.module';
+export * from './reports.service';
