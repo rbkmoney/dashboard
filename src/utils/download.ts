8cb9926c8d015494cd2e5d7ac98b954b2e85e1f7
Rinat Arsaev <krickray@gmail.com>
2019-11-05 18:32:17 +0300

    fix download

--- src/utils/download.ts
+++ src/utils/download.ts
@@ -1,13 +1,15 @@
 /**
  * https://github.com/sindresorhus/multi-download/blob/master/index.js
  */
-export async function download(url: string, name?: string) {
+export function download(url: string, name?: string) {
     const a = document.createElement('a');
     a.download = name;
     a.href = url;
     a.style.display = 'none';
     document.body.append(a);
     a.click();
-    await new Promise(res => setTimeout(res, 100)); // Chrome requires the timeout
-    a.remove();
+    // Chrome requires the timeout
+    setTimeout(() => {
+        a.remove();
+    }, 100);
 }
