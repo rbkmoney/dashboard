329c8246be5d69a4d4af3668fe64f0a0cc074cd3
Aleksandra Usacheva <lazercore@me.com>
2019-11-11 15:52:58 +0300

    some clean up

--- src/app/sections/invoice-details/invoice-details.module.ts
+++ src/app/sections/invoice-details/invoice-details.module.ts
@@ -3,11 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { InvoiceDetailsComponent } from './invoice-details.component';
 import { InvoiceDetailsRoutingModule } from './invoice-details-routing.module';
-import { InvoiceModule } from '../../api/invoice';
 import { InvoiceSearchService } from '../../api/search';
 
 @NgModule({
-    imports: [CommonModule, InvoiceModule, InvoiceDetailsRoutingModule],
+    imports: [CommonModule, InvoiceDetailsRoutingModule],
     declarations: [InvoiceDetailsComponent],
     providers: [InvoiceSearchService]
 })
