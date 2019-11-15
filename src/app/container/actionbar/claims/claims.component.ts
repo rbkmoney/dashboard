767836a0894c3786700682ad9df1a799b27d3328
Rinat Arsaev <krickray@gmail.com>
2019-11-15 18:31:05 +0300

    lint fixes

--- src/app/container/actionbar/claims/claims.component.ts
+++ src/app/container/actionbar/claims/claims.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter, OnInit, AfterContentInit, AfterViewInit } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 import { ClaimsService } from './claims.service';
 
