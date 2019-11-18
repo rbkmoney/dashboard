c45e36ebf2e11cd71e9d5a98ea8ddaeced4bafe2
Rinat Arsaev <krickray@gmail.com>
2019-11-18 15:57:24 +0300

    fix type guards

--- src/app/api/claims/type-guards/claim-modification/is-claim-modification.ts
+++ src/app/api/claims/type-guards/claim-modification/is-claim-modification.ts
@@ -1,6 +1,5 @@
 import { Modification, ClaimModification } from '../../../../api-codegen/claim-management';
 
 export const isClaimModification = (m: Modification): m is ClaimModification => {
-    const ClaimModificationType = Modification.ModificationTypeEnum.ClaimModification;
-    return (m as ClaimModification).modificationType === ClaimModificationType;
+    return m.modificationType === Modification.ModificationTypeEnum.ClaimModification;
 };
--- src/app/api/claims/type-guards/claim-modification/is-document-modification-unit.ts
+++ src/app/api/claims/type-guards/claim-modification/is-document-modification-unit.ts
@@ -1,8 +1,4 @@
 import { DocumentModificationUnit, ClaimModificationType } from '../../../../api-codegen/claim-management';
-import { isDocumentCreated } from './is-document-created';
-
-const isDocumentModification = (m: ClaimModificationType): m is DocumentModificationUnit =>
-    m.claimModificationType === 'DocumentModificationUnit';
 
 export const isDocumentModificationUnit = (m: ClaimModificationType): m is DocumentModificationUnit =>
-    isDocumentModification(m) && isDocumentCreated(m.documentModification);
+    m.claimModificationType === ClaimModificationType.ClaimModificationTypeEnum.DocumentModificationUnit;
