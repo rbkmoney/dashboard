20a1707c214075ad8aa7538bc6120adc446d2b66
Aleksandra Usacheva <lazercore@me.com>
2019-11-19 15:46:18 +0300

    mapping added

--- src/app/sections/onboarding/data-flow/forms/russian-private-entity/apply-to-questionary-data.ts
+++ src/app/sections/onboarding/data-flow/forms/russian-private-entity/apply-to-questionary-data.ts
@@ -1,8 +1,47 @@
+import get from 'lodash.get';
+
 import { QuestionaryData, IndividualEntityContractor } from '../../../../../api-codegen/questionary/swagger-codegen';
 import { FormValue } from '../form-value';
 
-const applyToContractor = (t: IndividualEntityContractor, {  }: FormValue): IndividualEntityContractor => {
-    return { ...t };
+const applyToContractor = (
+    t: IndividualEntityContractor,
+    {
+        russianPrivateEntity: { fio, birthDate, birthPlace, residenceAddress, snils },
+        russianDomesticPassport,
+        pdlInfo: { pdlCategory, pdlRelationDegree },
+        individualResidencyInfo
+    }: FormValue
+): IndividualEntityContractor => {
+    const individualEntity = get(t, ['individualEntity']);
+    const russianIndividualEntity = get(individualEntity, ['russianIndividualEntity']);
+    const russianPrivateEntity = get(russianIndividualEntity, ['russianPrivateEntity']);
+    const identityDocument = get(russianIndividualEntity, ['identityDocument']);
+    const residencyInfo = get(russianIndividualEntity, ['residencyInfo']);
+    return {
+        ...t,
+        individualEntity: {
+            ...individualEntity,
+            russianIndividualEntity: {
+                ...russianIndividualEntity,
+                snils,
+                russianPrivateEntity: {
+                    ...russianPrivateEntity,
+                    fio,
+                    birthDate,
+                    birthPlace,
+                    residenceAddress
+                },
+                identityDocument: {
+                    ...identityDocument,
+                    russianDomesticPassport
+                },
+                residencyInfo: {
+                    ...residencyInfo,
+                    individualResidencyInfo
+                }
+            }
+        }
+    };
 };
 
 export const applyToQuestionaryData = (d: QuestionaryData, v: FormValue): QuestionaryData => ({
--- src/app/sections/onboarding/data-flow/forms/russian-private-entity/to-form-value.ts
+++ src/app/sections/onboarding/data-flow/forms/russian-private-entity/to-form-value.ts
@@ -1,6 +1,19 @@
 import { QuestionaryData } from '../../../../../api-codegen/questionary';
 import { FormValue } from '../form-value';
+import get from "../index";
 
 export const toFormValue = (d: QuestionaryData): FormValue => {
-    return { ...d };
+    const russianIndividualEntity = get(d, ['contractor', 'individualEntity', 'russianIndividualEntity'], {});
+    const russianPrivateEntity = get(russianIndividualEntity, ['russianPrivateEntity'], {});
+    const russianDomesticPassport = get(russianIndividualEntity, ['identityDocument', 'russianDomesticPassport'], {});
+    const individualResidencyInfo = get(russianIndividualEntity, ['residencyInfo', 'individualResidencyInfo'], {});
+
+    return {
+        russianPrivateEntity: {
+            ...russianPrivateEntity,
+            snils: russianIndividualEntity.snils
+        },
+        russianDomesticPassport,
+        individualResidencyInfo
+    };
 };
