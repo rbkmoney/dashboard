3d86b326b61c67ae7e025dc334aa635190d4ba7f
Rinat Arsaev <krickray@gmail.com>
2019-11-19 13:29:48 +0300

    filterViewClaims to view-utils

--- src/app/container/actionbar/claims/claims.service.ts
+++ src/app/container/actionbar/claims/claims.service.ts
@@ -3,7 +3,7 @@ import { shareReplay, pluck, map } from 'rxjs/operators';
 
 import { ClaimsService as ClaimsApiService } from '../../../api/claims/claims.service';
 import { booleanDelay, takeError } from '../../../custom-operators';
-import { filterViewClaims } from './claims-list/claims-list-item/filter-view-claims';
+import { filterViewClaims } from '../../../view-utils';
 
 @Injectable()
 export class ClaimsService {
--- src/app/view-utils/claims/index.ts
+++ src/app/view-utils/claims/index.ts
@@ -1,2 +1,3 @@
 export * from './get-claim-status-view-info';
 export * from './get-claim-type';
+export * from './filter-view-claims';
