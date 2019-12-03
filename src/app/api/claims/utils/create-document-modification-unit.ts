ca80a5545e3cca9f763396ab0d50cb19974e2bd5
Ildar Galeev <keinasylum@gmail.com>
2019-12-03 14:15:14 +0300

    Bump claim mgt swag

--- src/app/api/claims/utils/create-document-modification-unit.ts
+++ src/app/api/claims/utils/create-document-modification-unit.ts
@@ -1,26 +1,15 @@
-import * as moment from 'moment';
-
 import { DocumentModificationUnit } from '../../../api-codegen/claim-management';
-import { SpecificModificationUnit } from './specific-modification-unit';
 import { SpecificClaimModificationUnit } from './specific-claim-modification-unit';
 
 export const createDocumentModificationUnit = (
-    modificationID: number,
-    documentId: string,
-    createdAt = moment()
-        .utc()
-        .format() as any
-): SpecificModificationUnit<SpecificClaimModificationUnit<DocumentModificationUnit>> => ({
-    modificationID,
-    createdAt,
-    modification: {
-        modificationType: 'ClaimModification',
-        claimModificationType: {
-            claimModificationType: 'DocumentModificationUnit',
-            documentId,
-            documentModification: {
-                documentModificationType: 'DocumentCreated'
-            }
+    documentId: string
+): SpecificClaimModificationUnit<DocumentModificationUnit> => ({
+    modificationType: 'ClaimModification',
+    claimModificationType: {
+        claimModificationType: 'DocumentModificationUnit',
+        documentId,
+        documentModification: {
+            documentModificationType: 'DocumentCreated'
         }
     }
 });
