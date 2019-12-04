2b92406cf06c04c55ab36b0ff1678242ec5b5ca4
DenisEzhov <d.ezhov@rbkmoney.com>
2019-12-04 18:23:16 +0300

    prettier

--- src/app/custom-operators/progress.ts
+++ src/app/custom-operators/progress.ts
@@ -2,10 +2,7 @@ import { merge, Observable, of } from 'rxjs';
 import { catchError, map, startWith } from 'rxjs/operators';
 
 export const progress = (start$: Observable<any>, end$: Observable<any>): Observable<boolean> =>
-    merge(
-        start$.pipe(map(() => true)),
-        end$.pipe(map(() => false))
-    ).pipe(
+    merge(start$.pipe(map(() => true)), end$.pipe(map(() => false))).pipe(
         catchError(() => of(false)),
         startWith(false)
     );
