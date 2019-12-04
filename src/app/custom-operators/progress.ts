8ba7db15024629284fa53be9bddb9d3e8c2faedd
DenisEzhov <d.ezhov@rbkmoney.com>
2019-12-04 18:07:23 +0300

    removed delay

--- src/app/custom-operators/progress.ts
+++ src/app/custom-operators/progress.ts
@@ -1,13 +1,10 @@
 import { merge, Observable, of } from 'rxjs';
-import { catchError, delay, map, startWith } from 'rxjs/operators';
+import { catchError, map, startWith } from 'rxjs/operators';
 
 export const progress = (start$: Observable<any>, end$: Observable<any>): Observable<boolean> =>
     merge(
         start$.pipe(map(() => true)),
-        end$.pipe(
-            delay(1),
-            map(() => false)
-        )
+        end$.pipe(map(() => false))
     ).pipe(
         catchError(() => of(false)),
         startWith(false)
