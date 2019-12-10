24e939beedf6cd127cd6f264ba688b5f2fdb421a
Rinat Arsaev <krickray@gmail.com>
2019-12-10 18:14:50 +0300

    FE-951: Add message API (#127)

---
+++ src/app/api/messages/messages.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+
+import { MessagesService } from './messages.service';
+
+@NgModule({
+    providers: [MessagesService]
+})
+export class MessagesModule {}
