932ffc768a084e55208c8b7e7b57f68fa921190e
Rinat Arsaev <krickray@gmail.com>
2019-12-12 19:40:59 +0300

    translate download buttons

--- src/app/sections/claim-modification-containers/change-container/document-modification-info/document-modification-info.component.ts
+++ src/app/sections/claim-modification-containers/change-container/document-modification-info/document-modification-info.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { pluck, map } from 'rxjs/operators';
 import isEmpty from 'lodash.isempty';
 import negate from 'lodash.negate';
+import { TCreatedPdf } from 'pdfmake/build/pdfmake';
 
 import { DocumentModificationUnit } from '../../../../api-codegen/claim-management';
 import { DocumentModificationInfoService } from './document-modification-info.service';
@@ -26,9 +27,7 @@ export class DocumentModificationInfoComponent implements OnChanges {
     bankAccount$ = this.questionary$.pipe(pluck('bankAccount'));
     isNotEmptyBankAccount$ = this.bankAccount$.pipe(map(negate(isEmpty)));
 
-    hasBeneficialOwnerDocuments$ = this.documentModificationInfoService.beneficialOwnersDocuments$.pipe(
-        map(b => Array.isArray(b) && b.length)
-    );
+    beneficialOwnersDocuments$ = this.documentModificationInfoService.beneficialOwnersDocuments$;
 
     constructor(private documentModificationInfoService: DocumentModificationInfoService) {}
 
@@ -40,9 +39,7 @@ export class DocumentModificationInfoComponent implements OnChanges {
         this.documentModificationInfoService.document$.subscribe(doc => doc.download('russian-individual-entity'));
     }
 
-    downloadBeneficialOwnerDocument() {
-        this.documentModificationInfoService.beneficialOwnersDocuments$.subscribe(docs =>
-            docs.forEach(doc => doc.download('beneficial-owner'))
-        );
+    downloadBeneficialOwnerDocument(beneficialOwnerDocument: TCreatedPdf) {
+        beneficialOwnerDocument.download('beneficial-owner');
     }
 }
