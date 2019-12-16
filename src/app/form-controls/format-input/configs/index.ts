b610653011a3f086f0d2a0b8aa8f352b854ed21f
Rinat Arsaev <krickray@gmail.com>
2019-12-16 18:13:30 +0300

    rm unneccessary

--- src/app/form-controls/format-input/configs/index.ts
+++ src/app/form-controls/format-input/configs/index.ts
@@ -2,8 +2,6 @@ export * from './format-input-config';
 
 export * from './configs';
 
-export * from './common/individual-or-legal-entity-inn';
-
 export * from './common';
 export * from './bank';
 export * from './bank-card';
