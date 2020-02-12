c39576d5df61f89418587a18d6518d264a0b8a72
Rinat Arsaev <krickray@gmail.com>
2020-02-12 18:48:58 +0300

    load payouts

--- src/app/sections/payment-section/payouts/payouts.component.ts
+++ src/app/sections/payment-section/payouts/payouts.component.ts
@@ -1,6 +1,9 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { shareReplay } from 'rxjs/operators';
 
 import { PayoutsService } from './payouts.service';
+import { mapToTimestamp } from '../operations/operators';
+import { booleanDebounceTime } from '../../../custom-operators';
 
 @Component({
     selector: 'dsh-payouts',
@@ -9,4 +12,25 @@ import { PayoutsService } from './payouts.service';
     changeDetection: ChangeDetectionStrategy.OnPush,
     providers: [PayoutsService]
 })
-export class PayoutsComponent {}
+export class PayoutsComponent {
+    payouts$ = this.payoutsService.searchResult$;
+    isLoading$ = this.payoutsService.doAction$.pipe(
+        booleanDebounceTime(),
+        shareReplay(1)
+    );
+    hasMore$ = this.payoutsService.hasMore$;
+    lastUpdated$ = this.payoutsService.searchResult$.pipe(
+        mapToTimestamp,
+        shareReplay(1)
+    );
+
+    constructor(private payoutsService: PayoutsService) {}
+
+    fetchMore() {
+        this.payoutsService.fetchMore();
+    }
+
+    refresh() {
+        this.payoutsService.refresh();
+    }
+}
