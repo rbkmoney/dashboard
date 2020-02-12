e52afd4c471d7edbd38784ec7d1c370d7cdac593
Rinat Arsaev <krickray@gmail.com>
2020-02-12 16:15:53 +0300

    add payouts header

--- src/app/sections/payment-section/payouts/payouts.module.ts
+++ src/app/sections/payment-section/payouts/payouts.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
+import { MatCommonModule } from '@angular/material';
+import { TranslocoModule } from '@ngneat/transloco';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { PayoutsComponent } from './payouts.component';
 import { PayoutsRoutingModule } from './payouts-routing.module';
+import { ButtonModule } from '../../../button';
 
 @NgModule({
-    imports: [PayoutsRoutingModule],
+    imports: [PayoutsRoutingModule, MatCommonModule, FlexLayoutModule, ButtonModule, TranslocoModule],
     declarations: [PayoutsComponent],
     exports: [PayoutsComponent]
 })
