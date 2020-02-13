3b84facb97b6d2922380c3c8b2b303017b1d011b
DenisEzhov <d.ezhov@rbkmoney.com>
2020-02-13 18:40:07 +0300

    d3 remove

--- src/app/charts/bar-chart/bar-chart.component.ts
+++ src/app/charts/bar-chart/bar-chart.component.ts
@@ -1,37 +1,7 @@
-import { Component, ElementRef, Input, OnChanges, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
-import { Selection } from 'd3-selection';
-
-import { BarChartConfig, PeriodData } from '../models/chart-data-models';
-import { BarChartService } from './bar-chart.service';
-import { LegendTooltipService } from '../legend-tooltip/legend-tooltip.service';
-
-export type BarType = Selection<SVGGElement, {}, null, PeriodData>;
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'dsh-bar-chart',
-    templateUrl: './bar-chart.component.html',
-    styleUrls: ['./bar-chart.component.scss', '../legend-tooltip/_legend-tooltip-theme.scss'],
-    encapsulation: ViewEncapsulation.None,
-    providers: [BarChartService, LegendTooltipService]
+    templateUrl: './bar-chart.component.html'
 })
-export class BarChartComponent implements OnChanges, OnInit {
-    @ViewChild('barChart', { static: true })
-    private chartContainer: ElementRef;
-
-    @Input()
-    data: PeriodData[];
-
-    @Input()
-    config: BarChartConfig;
-
-    constructor(private barChartService: BarChartService) {}
-
-    ngOnInit() {
-        const element = this.chartContainer.nativeElement;
-        this.barChartService.initChart(this.data, element, this.config);
-    }
-
-    ngOnChanges() {
-        this.barChartService.updateChart(this.data);
-    }
-}
+export class BarChartComponent { }
--- src/app/charts/donut-chart/donut-chart.component.ts
+++ src/app/charts/donut-chart/donut-chart.component.ts
@@ -1,33 +1,7 @@
-import { Component, ElementRef, Input, OnChanges, OnInit, ViewChild } from '@angular/core';
-
-import { DonutChartConfig, SegmentData } from '../models/chart-data-models';
-import { DonutChartService } from './donut-chart.service';
-import { LegendTooltipService } from '../legend-tooltip/legend-tooltip.service';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'dsh-donut-chart',
-    templateUrl: './donut-chart.component.html',
-    styleUrls: ['./donut-chart.component.scss', '../legend-tooltip/_legend-tooltip-theme.scss'],
-    providers: [DonutChartService, LegendTooltipService]
+    templateUrl: './donut-chart.component.html'
 })
-export class DonutChartComponent implements OnChanges, OnInit {
-    @ViewChild('donutChart', { static: true })
-    private chartContainer: ElementRef;
-
-    @Input()
-    data: SegmentData[];
-
-    @Input()
-    config: DonutChartConfig;
-
-    constructor(private donutChartService: DonutChartService) {}
-
-    ngOnInit() {
-        const element = this.chartContainer.nativeElement;
-        this.donutChartService.initChart(this.data, element, this.config);
-    }
-
-    ngOnChanges() {
-        this.donutChartService.updateChart(this.data);
-    }
-}
+export class DonutChartComponent { }
