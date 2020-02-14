58e72392c3b85f3f1eb303c305c0b397034a9190
DenisEzhov <d.ezhov@rbkmoney.com>
2020-02-14 12:05:23 +0300

    remove charts

--- src/app/charts/bar-chart/bar-chart.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'dsh-bar-chart',
-    templateUrl: './bar-chart.component.html'
-})
-export class BarChartComponent {}
--- src/app/charts/donut-chart/donut-chart.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'dsh-donut-chart',
-    templateUrl: './donut-chart.component.html'
-})
-export class DonutChartComponent {}
