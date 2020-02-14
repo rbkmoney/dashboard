f321f9bcc870948fb35852f31fb789872bb70e0d
Rinat Arsaev <krickray@gmail.com>
2020-02-14 18:06:38 +0300

    update structure

--- src/app/sections/payment-section/payouts/payout-panel/payout-panel.module.ts
+++ src/app/sections/payment-section/payouts/payout-panel/payout-panel.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
-import { MatIconModule } from '@angular/material';
+import { MatIconModule, MatDividerModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { CommonModule } from '@angular/common';
 
 import { PayoutPanelComponent } from './payout-panel.component';
 import { LayoutModule } from '../../../../layout';
 import { ButtonModule } from '../../../../button';
+import { DetailsItemModule } from '../../../../details-item';
 
 @NgModule({
-    imports: [LayoutModule, MatIconModule, ButtonModule],
+    imports: [
+        LayoutModule,
+        MatIconModule,
+        ButtonModule,
+        FlexLayoutModule,
+        CommonModule,
+        MatDividerModule,
+        DetailsItemModule
+    ],
     declarations: [PayoutPanelComponent],
     exports: [PayoutPanelComponent]
 })
