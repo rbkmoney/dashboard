ac8e4cc6883141cace4a9e22643d8c6cb38e7bbb
Rinat Arsaev <krickray@gmail.com>
2020-02-26 16:14:13 +0300

    to replace error

---
+++ src/app/custom-operators/replace-error.ts
@@ -0,0 +1,18 @@
+import { Observable, of } from 'rxjs';
+import { catchError, filter, pluck } from 'rxjs/operators';
+
+export class BasicError<T = any> {
+    constructor(public error: T) {}
+}
+
+export const replaceError = <T, E = any>(source: Observable<T>): Observable<T | BasicError<E>> =>
+    source.pipe(catchError(value => of(new BasicError(value))));
+
+export const filterError = <E>(source: Observable<any | BasicError<E>>): Observable<E> =>
+    source.pipe(
+        filter(value => value instanceof BasicError),
+        pluck('error')
+    );
+
+export const filterPayload = <T>(source: Observable<T | BasicError<any>>): Observable<T> =>
+    source.pipe(filter(value => !(value instanceof BasicError))) as Observable<T>;
