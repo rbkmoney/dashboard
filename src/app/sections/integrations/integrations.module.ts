d4888550f3c49484e054ea34ed0bbfa30b86ca4d
Aleksandra Usacheva <lazercore@me.com>
2020-03-26 20:12:15 +0300

    imports order

--- src/app/sections/integrations/integrations.module.ts
+++ src/app/sections/integrations/integrations.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { FlexModule } from '@angular/flex-layout';
 import { TranslocoModule } from '@ngneat/transloco';
 
 import { LayoutModule } from '@dsh/components/layout';
 import { ScrollUpModule } from '@dsh/components/navigation';
+
 import { IntegrationsRoutingModule } from './integrations-routing.module';
 import { IntegrationsComponent } from './integrations.component';
 
