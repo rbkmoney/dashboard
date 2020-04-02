07b9022eead6b3ac630ec9a54ba394ce9c8980b8
Rinat Arsaev <krickray@gmail.com>
2020-04-02 13:02:23 +0300

    data to service

--- src/app/sections/payment-section/operations/invoices/create-invoice-dialog/create-invoice-dialog.component.ts
+++ src/app/sections/payment-section/operations/invoices/create-invoice-dialog/create-invoice-dialog.component.ts
@@ -4,40 +4,23 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { TranslocoService } from '@ngneat/transloco';
-import moment from 'moment';
 import { Observable } from 'rxjs';
-import { map, shareReplay, startWith } from 'rxjs/operators';
-import { SHARE_REPLAY_CONF } from 'src/app/custom-operators';
 
-import { InvoiceService } from '../../../../../api';
-import { InvoiceLine, InvoiceLineTaxMode, InvoiceLineTaxVAT } from '../../../../../api-codegen/anapi';
+import { InvoiceLineTaxVAT } from '../../../../../api-codegen/anapi';
 import { ShopInfo } from '../../operators';
-
-const WITHOUT_VAT = Symbol('without VAT');
-const EMPTY_CART_ITEM = { product: '', quantity: null, price: null, taxVatRate: WITHOUT_VAT };
+import { CreateInvoiceDialogService, WITHOUT_VAT } from './create-invoice-dialog.service';
 
 @Component({
     selector: 'dsh-create-invoice-dialog',
     templateUrl: 'create-invoice-dialog.component.html',
     styleUrls: ['create-invoice-dialog.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    providers: [CreateInvoiceDialogService]
 })
 export class CreateInvoiceDialogComponent {
-    form = this.fb.group({
-        shopID: null,
-        dueDate: null,
-        product: '',
-        currency: 'RUB',
-        cart: this.fb.array([this.fb.group(EMPTY_CART_ITEM)])
-    });
+    form = this.createInvoiceDialogService.form;
     shopsInfo$: Observable<ShopInfo[]>;
-
-    totalAmount$ = this.cart.valueChanges.pipe(
-        startWith(this.cart.value),
-        map(v => v.map(({ price, quantity }) => price * quantity).reduce((sum, s) => (sum += s), 0)),
-        shareReplay(SHARE_REPLAY_CONF)
-    );
-
+    totalAmount$ = this.createInvoiceDialogService.totalAmount$;
     taxVatRates = Object.values(InvoiceLineTaxVAT.RateEnum);
     withoutVAT = WITHOUT_VAT;
 
@@ -47,9 +30,8 @@ export class CreateInvoiceDialogComponent {
 
     constructor(
         private dialogRef: MatDialogRef<CreateInvoiceDialogComponent, 'cancel' | 'create'>,
-        private invoiceService: InvoiceService,
+        private createInvoiceDialogService: CreateInvoiceDialogService,
         private transloco: TranslocoService,
-        private fb: FormBuilder,
         private snackBar: MatSnackBar,
         @Inject(MAT_DIALOG_DATA) data: { shopsInfo$: Observable<ShopInfo[]> },
         private router: Router
@@ -62,48 +44,22 @@ export class CreateInvoiceDialogComponent {
     }
 
     create() {
-        const { value } = this.form;
-        const cart = this.cart.controls.map(({ value: v }) => {
-            const product: InvoiceLine = {
-                product: v.product,
-                quantity: v.quantity,
-                price: v.price * 100
-            };
-            if (v.taxVatRate !== WITHOUT_VAT) {
-                product.taxMode = {
-                    type: InvoiceLineTaxMode.TypeEnum.InvoiceLineTaxVAT,
-                    rate: v.taxVatRate
-                } as InvoiceLineTaxMode;
+        this.createInvoiceDialogService.createInvoice().subscribe(
+            ({ invoice }) => {
+                this.dialogRef.close('create');
+                this.router.navigate(['invoice', invoice.id]);
+            },
+            () => {
+                this.snackBar.open(this.transloco.translate('commonError'), this.transloco.translate('ok'));
             }
-            return product;
-        });
-        this.invoiceService
-            .createInvoice({
-                shopID: value.shopID,
-                dueDate: moment(value.dueDate)
-                    .utc()
-                    .format(),
-                currency: value.currency,
-                product: value.product,
-                cart,
-                metadata: {}
-            })
-            .subscribe(
-                ({ invoice }) => {
-                    this.dialogRef.close('create');
-                    this.router.navigate(['invoice', invoice.id]);
-                },
-                () => {
-                    this.snackBar.open(this.transloco.translate('commonError'), this.transloco.translate('ok'));
-                }
-            );
+        );
     }
 
     add() {
-        this.cart.push(this.fb.group(EMPTY_CART_ITEM));
+        this.createInvoiceDialogService.addCartItem();
     }
 
     remove(idx: number) {
-        this.cart.removeAt(idx);
+        this.createInvoiceDialogService.removeCartItem(idx);
     }
 }
