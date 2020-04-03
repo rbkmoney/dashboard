c8aa20fe703aa648d539edbe8f66fcb70a8f9089
Rinat Arsaev <krickray@gmail.com>
2020-04-03 18:42:41 +0300

    unused

--- src/utils/active-class/active-class.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-
-import { ActiveClassPipe } from './active-class';
-
-@NgModule({
-    declarations: [ActiveClassPipe],
-    exports: [ActiveClassPipe]
-})
-export class ActiveClassModule {}
--- src/utils/active-class/active-class.ts
+++
@@ -1,8 +0,0 @@
-import { Pipe, PipeTransform } from '@angular/core';
-
-@Pipe({ name: 'activeClass' })
-export class ActiveClassPipe implements PipeTransform {
-    transform(value: string, predicate: any): string {
-        return predicate ? ` ${value.trim()}` : '';
-    }
-}
