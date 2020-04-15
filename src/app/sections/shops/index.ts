562214b81590437e02d8587766f66533bd25fa15
Rinat Arsaev <krickray@gmail.com>
2020-04-15 19:00:04 +0300

    remove unused

--- src/app/sections/shops/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './shops.module';
-export * from './shops-routing.module';
-export * from './shops.component';
--- src/app/sections/shops/shops.component.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-    selector: 'dsh-shops',
-    templateUrl: 'shops.component.html',
-    styleUrls: ['shops.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush
-})
-export class ShopsComponent {}
