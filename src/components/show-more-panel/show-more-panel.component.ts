48ec413809abc03c904be181354236880a5b45a5
Aleksandra Usacheva <lazercore@me.com>
2020-04-17 17:51:05 +0300

    default isloading value for empty component added

--- src/components/show-more-panel/show-more-panel.component.ts
+++ src/components/show-more-panel/show-more-panel.component.ts
@@ -7,7 +7,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class ShowMorePanelComponent {
     @Input()
-    isLoading: boolean;
+    isLoading = false;
 
     @Output()
     showMore: EventEmitter<void> = new EventEmitter();
