e049875baaa1af7f9de2538edc19177f2e3fe0a9
DenisHedgehog <ezhoden@mail.ru>
2020-04-20 15:56:11 +0300

    payment update refactoring

--- src/app/sections/payment-details/payment-details.component.ts
+++ src/app/sections/payment-details/payment-details.component.ts
@@ -1,20 +1,21 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import get from 'lodash.get';
 
 import { PaymentFlow, PaymentSearchResult, PaymentToolDetails } from '../../api-codegen/capi/swagger-codegen';
 import { LAYOUT_GAP } from '../constants';
 import { PayerType } from './payer-details';
-import { ReceivePaymentService } from './receive-payment.service';
-import { RouteParamPaymentService } from './route-param-payment.service';
+import { ReceivePaymentService, ReceivePaymentType } from './receive-payment.service';
 
 @Component({
     templateUrl: './payment-details.component.html',
     styleUrls: ['./payment-details.component.scss'],
-    providers: [ReceivePaymentService, RouteParamPaymentService]
+    providers: [ReceivePaymentService]
 })
-export class PaymentDetailsComponent implements OnInit {
+export class PaymentDetailsComponent {
     payment$ = this.receivePaymentService.payment$;
     isLoading$ = this.receivePaymentService.isLoading$;
+
+    ReceivePaymentType = ReceivePaymentType;
 
     PayerType = PayerType;
     PaymentFlow = PaymentFlow.TypeEnum;
@@ -25,11 +26,7 @@ export class PaymentDetailsComponent implements OnInit {
         return get(payment, 'payer.paymentToolDetails') as PaymentToolDetails;
     }
 
-    ngOnInit() {
-        this.receivePayment();
-    }
-
-    receivePayment() {
-        this.receivePaymentService.receivePayment();
+    receivePayment(type?: ReceivePaymentType) {
+        this.receivePaymentService.receivePayment(type);
     }
 }
--- src/app/sections/payment-details/receive-payment.service.ts
+++ src/app/sections/payment-details/receive-payment.service.ts
@@ -1,44 +1,82 @@
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute } from '@angular/router';
 import { TranslocoService } from '@ngneat/transloco';
-import { BehaviorSubject, Observable, Subject } from 'rxjs';
-import { filter, shareReplay, switchMap } from 'rxjs/operators';
+import { BehaviorSubject, EMPTY, Observable, Subject, timer } from 'rxjs';
+import { catchError, filter, first, shareReplay, startWith, switchMap } from 'rxjs/operators';
 
-import { PaymentSearchResult } from '../../api-codegen/anapi/swagger-codegen';
-import { progress } from '../../custom-operators';
-import { RouteParamPaymentService } from './route-param-payment.service';
+import { PaymentSearchResult } from '../../api-codegen/capi/swagger-codegen';
+import { PaymentSearchService } from '../../api/search';
+import { progress, SHARE_REPLAY_CONF } from '../../custom-operators';
+
+const completedStatuses: PaymentSearchResult.StatusEnum[] = [
+    PaymentSearchResult.StatusEnum.Captured,
+    PaymentSearchResult.StatusEnum.Cancelled
+];
+
+export enum ReceivePaymentType {
+    Hold = 'hold',
+    Receive = 'receive'
+}
 
 @Injectable()
 export class ReceivePaymentService {
-    private paymentState$ = new BehaviorSubject(null);
     private receivePaymentError$ = new BehaviorSubject(false);
-    private receivePayment$ = new Subject();
+    private receivePayment$ = new Subject<ReceivePaymentType>();
 
-    payment$: Observable<PaymentSearchResult> = this.paymentState$.pipe(
-        filter(s => !!s),
-        shareReplay(1)
+    payment$ = this.receivePayment$.pipe(
+        startWith(ReceivePaymentType.Receive),
+        switchMap(type => {
+            const { invoiceID, paymentID } = this.route.snapshot.params;
+            console.log(type, type === ReceivePaymentType.Hold);
+            return type === ReceivePaymentType.Hold
+                ? timer(0, 500).pipe(
+                      switchMap(() =>
+                          this.paymentSearchService
+                              .getPaymentByDuration(
+                                  {
+                                      amount: 3,
+                                      unit: 'y'
+                                  },
+                                  invoiceID,
+                                  paymentID
+                              )
+                              .pipe(
+                                  catchError(() => {
+                                      this.receivePaymentError$.next(true);
+                                      return EMPTY;
+                                  })
+                              )
+                      ),
+                      filter(p => completedStatuses.includes(p?.status)),
+                      first()
+                  )
+                : this.paymentSearchService.getPaymentByDuration(
+                      {
+                          amount: 3,
+                          unit: 'y'
+                      },
+                      invoiceID,
+                      paymentID
+                  );
+        }),
+        shareReplay(SHARE_REPLAY_CONF)
     );
 
     isLoading$ = progress(this.receivePayment$, this.payment$);
 
     error$: Observable<any> = this.receivePaymentError$.asObservable();
 
-    receivePayment() {
-        this.receivePayment$.next();
+    receivePayment(type = ReceivePaymentType.Receive) {
+        this.receivePayment$.next(type);
     }
 
     constructor(
-        private routeParamPaymentService: RouteParamPaymentService,
         private snackBar: MatSnackBar,
-        private transloco: TranslocoService
+        private transloco: TranslocoService,
+        private route: ActivatedRoute,
+        private paymentSearchService: PaymentSearchService
     ) {
-        this.receivePayment$.pipe(switchMap(() => this.routeParamPaymentService.payment$)).subscribe(
-            payment => this.paymentState$.next(payment),
-            err => {
-                console.error(err);
-                this.snackBar.open(this.transloco.translate('commonError'), 'OK');
-                this.receivePaymentError$.next(true);
-            }
-        );
+        this.error$.subscribe(() => this.snackBar.open(this.transloco.translate('commonError'), 'OK'));
     }
 }
