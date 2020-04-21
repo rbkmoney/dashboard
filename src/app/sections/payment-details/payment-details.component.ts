ba4288009faa476e0b190413019d6b1165d1e9a1
Denis Ezhov <ezhoden@mail.ru>
2020-04-21 19:45:00 +0300

    FE-897. Holds (#148)
    * payment-details decompose to modules, create refund
    
    * fix
    
    * hold actions
    
    * confirm hold fix
    
    * build fix
    
    * up to date fixes
    
    * disable refund creation for some statuses
    
    * merge fixes
    
    * fixes
    
    * refactoring and fixes
    
    * refactoring and fixes
    
    * prettier
    
    * catch error
    
    * improvements
    
    * improvements
    
    * payment update after hold action
    
    * payment update refactoring
    
    * fix
    
    * refactoring
    
    * refactoring
    
    Co-authored-by: DenisEzhov <d.ezhov@rbkmoney.com>
    Co-authored-by: Aleksandra Usacheva <lazercore@me.com>

--- src/app/sections/payment-details/payment-details.component.ts
+++ src/app/sections/payment-details/payment-details.component.ts
@@ -4,22 +4,27 @@ import get from 'lodash.get';
 import { PaymentFlow, PaymentSearchResult, PaymentToolDetails } from '../../api-codegen/capi/swagger-codegen';
 import { LAYOUT_GAP } from '../constants';
 import { PayerType } from './payer-details';
-import { PaymentDetailsService } from './payment-details.service';
+import { ReceivePaymentService } from './receive-payment.service';
 
 @Component({
     templateUrl: './payment-details.component.html',
     styleUrls: ['./payment-details.component.scss'],
-    providers: [PaymentDetailsService]
+    providers: [ReceivePaymentService]
 })
 export class PaymentDetailsComponent {
-    payment$ = this.paymentDetailsService.payment$;
+    payment$ = this.receivePaymentService.payment$;
+    isLoading$ = this.receivePaymentService.isLoading$;
 
     PayerType = PayerType;
     PaymentFlow = PaymentFlow.TypeEnum;
 
-    constructor(@Inject(LAYOUT_GAP) public layoutGap: string, private paymentDetailsService: PaymentDetailsService) {}
+    constructor(@Inject(LAYOUT_GAP) public layoutGap: string, private receivePaymentService: ReceivePaymentService) {}
 
     getPaymentToolDetails(payment: PaymentSearchResult): PaymentToolDetails {
         return get(payment, 'payer.paymentToolDetails') as PaymentToolDetails;
+    }
+
+    holdPayment() {
+        this.receivePaymentService.holdPayment();
     }
 }
