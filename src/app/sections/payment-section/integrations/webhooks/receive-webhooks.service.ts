d3a7bc4f86ff71e5b76b9f48276cd4667747bf11
Aleksandra Usacheva <lazercore@me.com>
2020-04-23 08:49:07 +0300

    FE-1040: build.

--- src/app/sections/payment-section/integrations/webhooks/receive-webhooks.service.ts
+++ src/app/sections/payment-section/integrations/webhooks/receive-webhooks.service.ts
@@ -5,7 +5,8 @@ import { TranslocoService } from '@ngneat/transloco';
 import sortBy from 'lodash.sortby';
 import { BehaviorSubject, combineLatest, Observable, of, Subject } from 'rxjs';
 import {
-    catchError, debounceTime,
+    catchError,
+    debounceTime,
     distinctUntilChanged,
     filter,
     first,
