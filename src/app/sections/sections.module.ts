2cc5d29f7dc961efd5d807a6d52c357060b62973
Aleksandra Usacheva <lazercore@me.com>
2020-04-28 07:17:43 +0300

    FE-1046: prettier.

--- src/app/sections/sections.module.ts
+++ src/app/sections/sections.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 
 import { ShopModule } from '../api/shop';
+import { WalletModule } from '../api/wallet';
 import { LAYOUT_GAP } from './constants';
 import { MainModule } from './main';
 import { SectionsRoutingModule } from './sections-routing.module';
 import { SectionsComponent } from './sections.component';
-import { WalletModule } from '../api/wallet';
 
 @NgModule({
     imports: [MainModule, SectionsRoutingModule, ShopModule, WalletModule],
