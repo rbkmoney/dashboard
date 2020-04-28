1804395aea9a66e9b8cc050c7fbd1788acf3425e
Aleksandra Usacheva <lazercore@me.com>
2020-04-28 07:07:39 +0300

    FE-1046: prettier.

--- src/app/sections/main/main.component.ts
+++ src/app/sections/main/main.component.ts
@@ -13,8 +13,10 @@ export class MainComponent {
     docsEndpoint = this.configService.ext.docsEndpoint;
     supportMailto = `mailto:${this.configService.ext.supportEmail}`;
     oldDashboardEndpoint = this.configService.ext.oldDashboardEndpoint;
-    hasWallets$ = this.walletsService.listWallets(1).pipe(map(w => w.result.length > 0), shareReplay(SHARE_REPLAY_CONF));
+    hasWallets$ = this.walletsService.listWallets(1).pipe(
+        map(w => w.result.length > 0),
+        shareReplay(SHARE_REPLAY_CONF)
+    );
 
-    constructor(private configService: ConfigService, private walletsService: WalletService) {
-    }
+    constructor(private configService: ConfigService, private walletsService: WalletService) {}
 }
