19e6b2e2a34871457a4901255e1dd0e27487b3ec
Ildar Galeev <keinasylum@gmail.com>
2020-05-06 16:16:20 +0300

    After merge fixes

--- src/app/sections/payment-section/analytics/refunds-amount/refunds-amount.module.ts
+++ src/app/sections/payment-section/analytics/refunds-amount/refunds-amount.module.ts
@@ -8,7 +8,6 @@ import { SpinnerModule } from '@dsh/components/indicators';
 import { CardModule } from '@dsh/components/layout';
 
 import { AnalyticsModule } from '../../../../api/analytics';
-import { FromMinorModule } from '../../../../from-minor';
 import { PercentDifferenceModule } from '../percent-difference';
 import { StatItemModule } from '../stat-item/stat-item.module';
 import { RefundsAmountComponent } from './refunds-amount.component';
@@ -23,7 +22,6 @@ import { RefundsAmountComponent } from './refunds-amount.component';
         AnalyticsModule,
         CardModule,
         PercentDifferenceModule,
-        FromMinorModule,
         StatItemModule
     ],
     declarations: [RefundsAmountComponent],
