045d97634cffbb7747026c21a1705d7e97fc2576
Aleksandra Usacheva <lazercore@me.com>
2020-05-06 16:59:49 +0300

    FE-1048: pr fixes

--- src/app/sections/payment-section/integrations/api-key/api-key.component.ts
+++ src/app/sections/payment-section/integrations/api-key/api-key.component.ts
@@ -2,18 +2,17 @@ import { Component, Inject } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { TranslocoService } from '@ngneat/transloco';
 import { KeycloakService } from 'keycloak-angular';
-import { fromPromise } from 'rxjs/internal-compatibility';
+import { from } from 'rxjs';
 
 import { ConfigService } from '../../../../config';
 import { LAYOUT_GAP } from '../../../constants';
 
 @Component({
-    templateUrl: 'api-key.component.html',
-    styleUrls: ['api-key.component.scss']
+    templateUrl: 'api-key.component.html'
 })
 export class ApiKeyComponent {
-    token$ = fromPromise(this.keycloakService.getToken());
-    paymentsEndpoint = this.configService.ext.paymentsEndpoint;
+    token$ = from(this.keycloakService.getToken());
+    paymentsApiSpecEndpoint = this.configService.ext.paymentsApiSpecEndpoint;
 
     constructor(
         @Inject(LAYOUT_GAP) public layoutGap: string,
