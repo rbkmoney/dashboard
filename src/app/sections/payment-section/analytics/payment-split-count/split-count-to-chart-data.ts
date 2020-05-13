44984def35a20346da9dbbaa81fa9a7d38ac115e
Denis Ezhov <ezhoden@mail.ru>
2020-05-13 17:23:31 +0300

    fill empty values by zero (#226)

--- src/app/sections/payment-section/analytics/payment-split-count/split-count-to-chart-data.ts
+++ src/app/sections/payment-section/analytics/payment-split-count/split-count-to-chart-data.ts
@@ -2,57 +2,22 @@ import { translate } from '@ngneat/transloco';
 import sortBy from 'lodash.sortby';
 import moment from 'moment';
 
-import {
-    OffsetCount,
-    SplitCountResult,
-    SplitUnit,
-    StatusOffsetCount
-} from '../../../../api-codegen/anapi/swagger-codegen';
+import { SplitCountResult, SplitUnit, StatusOffsetCount } from '../../../../api-codegen/anapi/swagger-codegen';
 import { ChartData, splitUnitToTimeFormat } from '../utils';
 
-const fixExtraInterval = (offsetCounts: OffsetCount[]): OffsetCount[] =>
-    offsetCounts.reduce(
-        (acc, curr, index) =>
-            index === 1
-                ? [
-                      {
-                          ...curr,
-                          count: acc[0].count + curr.count
-                      }
-                  ]
-                : [...acc, curr],
-        []
-    );
-
-const prepareOffsetCounts = (statusOffsetCounts: StatusOffsetCount[], unit: SplitUnit): StatusOffsetCount[] =>
+const prepareOffsetCounts = (statusOffsetCounts: StatusOffsetCount[]): StatusOffsetCount[] =>
     statusOffsetCounts.map(
         (statusOffsetCount): StatusOffsetCount => {
             const sorted = sortBy(statusOffsetCount.offsetCount, 'offset');
             return {
                 ...statusOffsetCount,
-                offsetCount: unit !== 'hour' ? fixExtraInterval(sorted) : sorted
+                offsetCount: sorted
             };
         }
     );
 
-const getOffsetSortedArray = (offset: StatusOffsetCount[]): number[] => {
-    const offsetsArrays = offset.map(o => o.offsetCount.map(c => c.offset));
-    const allOffsets = offsetsArrays.reduce((acc, curr) => acc.concat(curr), []);
-    const distinctOffsets = [...new Set(allOffsets)];
-    return sortBy(distinctOffsets);
-};
-
-const fillOffset = (offset: StatusOffsetCount[]): StatusOffsetCount[] => {
-    const offsetArray: number[] = getOffsetSortedArray(offset);
-    return offset.map(({ status, offsetCount }) => ({
-        status,
-        offsetCount: offsetArray.map((o, i) => ({ offset: o, count: offsetCount[i]?.count ?? 0 }))
-    }));
-};
-
 const statusOffsetCountsToSeries = (statusOffsetCounts: StatusOffsetCount[], unit: SplitUnit) => {
-    const filledOffset = fillOffset(statusOffsetCounts);
-    return filledOffset.map(({ status, offsetCount }) => ({
+    return statusOffsetCounts.map(({ status, offsetCount }) => ({
         name: translate(`analytics.paymentStatuses.${status.toString()}`, null, 'payment-section|scoped'),
         data: offsetCount.map(c => ({
             x: moment(c.offset).format(splitUnitToTimeFormat(unit)),
@@ -63,7 +28,7 @@ const statusOffsetCountsToSeries = (statusOffsetCounts: StatusOffsetCount[], uni
 
 export const splitCountToChartData = (splitCounts: SplitCountResult[]): ChartData[] =>
     splitCounts.map(({ currency, statusOffsetCounts, splitUnit }) => {
-        const prepared = prepareOffsetCounts(statusOffsetCounts, splitUnit);
+        const prepared = prepareOffsetCounts(statusOffsetCounts);
         return {
             currency,
             series: statusOffsetCountsToSeries(prepared, splitUnit)
