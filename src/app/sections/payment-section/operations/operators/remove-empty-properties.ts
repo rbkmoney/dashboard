59834b0302f1d68cd13bc6ddd1cee9f3304ada14
Aleksandra Usacheva <lazercore@me.com>
2020-05-14 05:31:43 +0300

    FE-1049: consider its done!

--- src/app/sections/payment-section/operations/operators/remove-empty-properties.ts
+++ src/app/sections/payment-section/operations/operators/remove-empty-properties.ts
@@ -1,13 +1,19 @@
 import isNil from 'lodash.isnil';
+import isArray from 'lodash/isArray';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 export const removeEmptyProperties = <T>(s: Observable<T>) =>
     s.pipe(
         map(obj =>
-            Object.keys(obj).reduce(
-                (acc, cur) => (!isNil(obj[cur]) && obj[cur] !== '' ? { ...acc, [cur]: obj[cur] } : acc),
-                {} as T
-            )
+            Object.keys(obj).reduce((acc, cur) => {
+                if (!isNil(obj[cur]) && obj[cur] !== '' && !isArray(obj[cur])) {
+                    return { ...acc, [cur]: obj[cur] };
+                } else if (isArray(obj[cur])) {
+                    return obj[cur].filter(i => !!i).length > 0 ? { ...acc, [cur]: obj[cur] } : acc;
+                } else {
+                    return acc;
+                }
+            }, {} as T)
         )
     );
--- src/components/form-controls/shop-selector/shop-selector.component.ts
+++ src/components/form-controls/shop-selector/shop-selector.component.ts
@@ -1,10 +1,25 @@
-import { ChangeDetectionStrategy, Component, ElementRef, Input, ViewChild } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FocusMonitor } from '@angular/cdk/a11y';
+import { Platform } from '@angular/cdk/platform';
+import { AutofillMonitor } from '@angular/cdk/text-field';
+import {
+    ChangeDetectionStrategy,
+    Component,
+    ElementRef,
+    Input,
+    OnInit,
+    Optional,
+    Self,
+    ViewChild
+} from '@angular/core';
+import { FormControl, FormGroupDirective, NgControl, NgForm } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { ActivatedRoute } from '@angular/router';
 import { combineLatest, Observable, of } from 'rxjs';
-import { debounceTime, shareReplay, startWith, switchMap } from 'rxjs/operators';
+import { debounceTime, pluck, shareReplay, startWith, switchMap, take } from 'rxjs/operators';
 
 import { SHARE_REPLAY_CONF } from '../../../app/custom-operators';
 import { filterByNameAndId, ShopInfo } from '../../../app/sections/payment-section/operations/operators';
+import { routeEnv } from '../../../app/sections/route-env';
 import { CustomFormControl } from '../utils';
 
 @Component({
@@ -13,7 +28,7 @@ import { CustomFormControl } from '../utils';
     styleUrls: ['shop-selector.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ShopSelectorComponent extends CustomFormControl {
+export class ShopSelectorComponent extends CustomFormControl implements OnInit {
     @Input() shopsInfos: ShopInfo[];
 
     @ViewChild('shopsSelector') clearShopIDsSearchInput: ElementRef;
@@ -26,6 +41,30 @@ export class ShopSelectorComponent extends CustomFormControl {
         filterByNameAndId,
         shareReplay(SHARE_REPLAY_CONF)
     );
+
+    constructor(focusMonitor: FocusMonitor,
+                elementRef: ElementRef<HTMLElement>,
+                platform: Platform,
+                @Optional() @Self() ngControl: NgControl,
+                autofillMonitor: AutofillMonitor,
+                defaultErrorStateMatcher: ErrorStateMatcher,
+                @Optional() parentForm: NgForm,
+                @Optional() parentFormGroup: FormGroupDirective,
+                private route: ActivatedRoute) {
+        super(focusMonitor, elementRef, platform, ngControl, autofillMonitor, defaultErrorStateMatcher, parentForm, parentFormGroup);
+    }
+
+    ngOnInit(): void {
+        this.route.params.pipe(
+            take(1),
+            pluck('envID')
+        ).subscribe(env => {
+            if (env === routeEnv[0]) {
+                this.value = ['TEST'];
+                this.setDisabledState(true);
+            }
+        });
+    }
 
     resetShopFilter() {
         this.value = [];
