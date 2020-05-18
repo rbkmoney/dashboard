12ef9d1d8c5a283d71fdb0bab89b43315b2dbe32
Rinat Arsaev <krickray@gmail.com>
2020-05-19 01:53:53 +0300

    new import type

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,5 +1,5 @@
+import type AppConfig from '../../appConfig.json'
 import { getBaseClass } from '../../utils';
 
-export type Config = typeof import('../../appConfig.json');
-
+export type Config = typeof AppConfig;
 export const BaseConfig = getBaseClass<Config>();
