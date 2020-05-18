f2e3a3abbb564de349a3d872b96ba9bd1599d677
Rinat Arsaev <krickray@gmail.com>
2020-05-18 18:07:33 +0300

    revert with new

--- src/app/sections/onboarding/data-flow/step-navigation/step-navigation.service.ts
+++ src/app/sections/onboarding/data-flow/step-navigation/step-navigation.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { filter, take } from 'rxjs/operators';
+import { combineLatest, of, Subject } from 'rxjs';
+import { filter, pluck, switchMap, take, tap } from 'rxjs/operators';
 
 import { QuestionaryStateService } from '../questionary-state.service';
 import { Direction, StepFlowService } from '../step-flow';
@@ -8,31 +9,31 @@ import { ValidityService } from '../validity';
 
 @Injectable()
 export class StepNavigationService {
+    private goByDirection$ = new Subject<Direction>();
+
     constructor(
         private questionaryStateService: QuestionaryStateService,
         private stepFlowService: StepFlowService,
         private validityService: ValidityService,
         private validationCheckService: ValidationCheckService
-    ) {}
+    ) {
+        this.goByDirection$
+            .pipe(
+                tap(() => this.questionaryStateService.save()),
+                switchMap(direction =>
+                    combineLatest([of(direction), this.validityService.isCurrentStepValid$.pipe(take(1))])
+                ),
+                filter(([direction, isValid]) => direction === 'back' || isValid),
+                pluck(0)
+            )
+            .subscribe(direction => this.stepFlowService.go(direction));
+    }
 
     forward() {
-        this.goByDirection('forward');
+        this.goByDirection$.next('forward');
     }
 
     back() {
-        this.goByDirection('back');
-    }
-
-    goByDirection(direction: Direction) {
-        this.questionaryStateService.save();
-        if (direction === 'forward') {
-            this.validationCheckService.validationCheck();
-        }
-        this.validityService.isCurrentStepValid$
-            .pipe(
-                take(1),
-                filter(isValid => isValid)
-            )
-            .subscribe(() => this.stepFlowService.go(direction));
+        this.goByDirection$.next('back');
     }
 }
