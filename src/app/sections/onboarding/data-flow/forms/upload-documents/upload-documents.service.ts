f42e3d6abaec4ec836dbb0344611fe4e4ae08a31
Rinat Arsaev <krickray@gmail.com>
2020-05-27 11:58:45 +0300

    lint fix

--- src/app/sections/onboarding/data-flow/forms/upload-documents/upload-documents.service.ts
+++ src/app/sections/onboarding/data-flow/forms/upload-documents/upload-documents.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { FormGroup } from '@angular/forms';
-import { Observable, Subject } from 'rxjs';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { TranslocoService } from '@ngneat/transloco';
+import { Observable, Subject } from 'rxjs';
 import { map, pluck, switchMap, withLatestFrom } from 'rxjs/operators';
 
 import { ClaimsService, createFileModificationUnit, takeFileModificationUnits } from '../../../../../api';
