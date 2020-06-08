e0ca4b1f91800069924f50cd29ed72dc83761ba5
Rinat Arsaev <krickray@gmail.com>
2020-06-09 01:35:31 +0500

    send errors only on prod

--- src/app/error-handler/error-handler.service.ts
+++ src/app/error-handler/error-handler.service.ts
@@ -8,9 +8,10 @@ import { parseError } from './parse-error';
 @Injectable()
 export class ErrorHandlerService implements ErrorHandler {
     handleError(error: any): void {
-        const parsedError = parseError(error);
-        Sentry.captureException(parsedError.error);
-        if (!environment.production) {
+        if (environment.production) {
+            const parsedError = parseError(error);
+            Sentry.captureException(parsedError.error);
+        } else {
             console.error(error);
         }
     }
