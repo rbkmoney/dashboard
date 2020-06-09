9a540b500ece396206a026ab4f6e5cf15f568e59
DenisHedgehog <ezhoden@mail.ru>
2020-06-09 12:54:51 +0300

    fixes

--- src/app/sections/payment-section/analytics/payment-split-amount/split-amount-to-chart-data.ts
+++ src/app/sections/payment-section/analytics/payment-split-amount/split-amount-to-chart-data.ts
@@ -4,10 +4,16 @@ import { toMajor } from '../../../../../utils';
 import { OffsetAmount, SplitAmountResult, SplitUnit } from '../../../../api-codegen/anapi/swagger-codegen';
 import { ChartData, Series, splitUnitToTimeFormat } from '../utils';
 
+const indexToVisibility = (index: number, length: number): 'show' | 'hide' =>
+    length > 24 ? (index % 2 ? 'hide' : 'show') : 'show';
+
+const offsetToX = (offset: number, unit: SplitUnit, index: number, length: number): string =>
+    `${moment(offset).format(splitUnitToTimeFormat(unit))}#${indexToVisibility(index, length)}`;
+
 const offsetAmountsToSeries = (offsetAmounts: OffsetAmount[], unit: SplitUnit): Series[] => [
     {
-        data: offsetAmounts.map((offsetAmount) => ({
-            x: moment(offsetAmount.offset).format(splitUnitToTimeFormat(unit)),
+        data: offsetAmounts.map((offsetAmount, i) => ({
+            x: offsetToX(offsetAmount.offset, unit, i, offsetAmounts.length),
             y: toMajor(offsetAmount.amount),
         })),
     },
