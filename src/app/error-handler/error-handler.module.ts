4c184b2fe0dd1dd8bc57e9b35408a26cbf35a8c2
Rinat Arsaev <krickray@gmail.com>
2020-06-09 10:47:22 +0500

    remove unused

--- src/app/error-handler/error-handler.module.ts
+++ src/app/error-handler/error-handler.module.ts
@@ -2,12 +2,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 
 import { ErrorHandlerService } from './error-handler.service';
 
-const EXPORTED_DECLARATIONS = [];
-
 @NgModule({
-    imports: [],
-    declarations: EXPORTED_DECLARATIONS,
-    exports: EXPORTED_DECLARATIONS,
     providers: [{ provide: ErrorHandler, useClass: ErrorHandlerService }],
 })
 export class ErrorHandlerModule {}
