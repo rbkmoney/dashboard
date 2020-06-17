2bc13bf9769482737b1128e2b7612f24cce801e2
Aleksandra Usacheva <lazercore@me.com>
2020-06-17 13:05:55 +0300

    pr fixes

--- src/components/form-controls/form-controls.module.ts
+++ src/components/form-controls/form-controls.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 
-import { ShopSelectorModule } from '../../app/sections/shop-selector';
 import { FileUploaderModule } from './file-uploader';
 import { FormatInputModule } from './format-input';
 import { RangeDatepickerModule } from './range-datepicker';
 
-const EXPORTED_DECLARATIONS = [FormatInputModule, RangeDatepickerModule, FileUploaderModule, ShopSelectorModule];
+const EXPORTED_DECLARATIONS = [FormatInputModule, RangeDatepickerModule, FileUploaderModule];
 
 @NgModule({
     imports: EXPORTED_DECLARATIONS,
