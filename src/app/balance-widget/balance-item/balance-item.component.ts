d0f99fe25cfdb392645c3221c916613d806160ac
Ildar Galeev <keinasylum@gmail.com>
2020-06-23 17:09:17 +0300

    Removed useless balance widget. Changed payment section route

--- src/app/balance-widget/balance-item/balance-item.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, Input } from '@angular/core';
-
-@Component({
-    selector: 'dsh-balance-item',
-    templateUrl: './balance-item.component.html',
-})
-export class BalanceItemComponent {
-    @Input() balance: { amount: number; currency: string };
-}
