ffa5934f641f3511f51290cecac89e28aeb2bdbe
DenisHedgehog <ezhoden@mail.ru>
2020-06-29 16:14:55 +0300

    fix

--- src/app/sections/payment-section/analytics/utils/distribution-chart-data.ts
+++ src/app/sections/payment-section/analytics/utils/distribution-chart-data.ts
@@ -2,9 +2,3 @@ export interface DistributionChartData {
     series: number[];
     labels: string[];
 }
-
-// export interface DistributionChartData {
-//     value: number[];
-//     label: string[];
-//     details?: DistributionChartData;
-// }
