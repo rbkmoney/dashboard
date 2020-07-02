e54ce4d2076b49859abb6320711191dc14b2ab7e
DenisHedgehog <ezhoden@mail.ru>
2020-07-02 12:06:39 +0300

    func to pipe

--- src/app/sections/payment-details/details/details.component.ts
+++ src/app/sections/payment-details/details/details.component.ts
@@ -1,7 +1,6 @@
 import { Component, Inject, Input, OnChanges, SimpleChanges } from '@angular/core';
-import { translate } from '@ngneat/transloco';
 
-import { PaymentError, PaymentSearchResult, PaymentStatus } from '../../../api-codegen/capi/swagger-codegen';
+import { PaymentSearchResult, PaymentStatus } from '../../../api-codegen/capi/swagger-codegen';
 import { StatusColor as Color } from '../../../theme-manager';
 import { LAYOUT_GAP } from '../../constants';
 import { getPaymentStatusInfo } from '../../get-payment-status-info';
@@ -29,24 +28,5 @@ export class DetailsComponent implements OnChanges {
         const { color, status } = getPaymentStatusInfo(paymentStatus);
         this.color = color;
         this.status = status;
-    }
-
-    getMessage(paymentError: PaymentError) {
-        return this.mapErrors(paymentError);
-    }
-
-    private mapErrors({ code, subError }: PaymentError, accCode: string = 'paymentErrors', acc: string = ''): string {
-        const getTranslation = (path: string) => {
-            const errorTranslation = translate(path);
-            return errorTranslation && errorTranslation !== path ? errorTranslation : translate('unknownError');
-        };
-        const translationPath = accCode ? `${accCode}.${code}` : code;
-        if (subError) {
-            const message = getTranslation(`${translationPath}.message`);
-            return this.mapErrors(subError, translationPath, acc.concat(acc === '' ? message : ` -> ${message}`));
-        } else {
-            const message = getTranslation(translationPath);
-            return acc === '' ? message : `${acc} -> ${message}.`;
-        }
     }
 }
