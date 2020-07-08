1bf938e8235ba968c988944818618b237b4ebf7d
Rinat Arsaev <krickray@gmail.com>
2020-07-08 17:12:33 +0300

    rej reactive forms

--- src/components/filter/multiselect-filter/multiselect-filter-content/multiselect-filter-content.component.ts
+++ src/components/filter/multiselect-filter/multiselect-filter-content/multiselect-filter-content.component.ts
@@ -1,7 +1,7 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnChanges, OnInit, Output } from '@angular/core';
-import { FormArray, FormBuilder } from '@angular/forms';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnChanges, Output } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { BehaviorSubject, combineLatest, Observable } from 'rxjs';
-import { debounceTime, distinctUntilChanged, map, shareReplay, startWith } from 'rxjs/operators';
+import { debounceTime, map, shareReplay, startWith } from 'rxjs/operators';
 
 import { ComponentChanges } from '../../../../type-utils';
 import { MultiselectFilterItem } from '../multiselect-filter-item';
@@ -12,7 +12,7 @@ import { MultiselectFilterItem } from '../multiselect-filter-item';
     styleUrls: ['multiselect-filter-content.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MultiselectFilterContentComponent<T = any> implements OnInit, OnChanges {
+export class MultiselectFilterContentComponent<T = any> implements OnChanges {
     @Input() label: string;
     @Input() items: MultiselectFilterItem<T>[];
     @Input() selectedItems?: (MultiselectFilterItem<T> | MultiselectFilterItem<T>['id'])[];
@@ -20,36 +20,26 @@ export class MultiselectFilterContentComponent<T = any> implements OnInit, OnCha
     @Output() selectedChange = new EventEmitter<MultiselectFilterItem<T>[]>();
     @Output() foundChange = new EventEmitter<MultiselectFilterItem<T>[]>();
 
-    form = this.fb.group({
-        search: '',
-        items: this.fb.array([]),
-    });
-    get itemsForm() {
-        return this.form.controls.items as FormArray;
-    }
+    searchControl = this.fb.control('');
 
-    items$ = new BehaviorSubject([]);
-    displayedItemsIdx$ = combineLatest([
-        (this.form.controls.search.valueChanges as Observable<string>).pipe(
-            startWith(this.form.controls.search.value as string)
-        ),
+    items$ = new BehaviorSubject<MultiselectFilterItem<T>[]>([]);
+    selectedItems$ = new BehaviorSubject<{ [N in MultiselectFilterItem<T>['id']]: boolean }>({});
+    displayedItems$ = combineLatest([
+        this.searchControl.valueChanges.pipe(startWith(this.searchControl.value), debounceTime(250)) as Observable<
+            string
+        >,
         this.items$,
     ]).pipe(
-        debounceTime(200),
-        map(([value, items]) =>
-            items.reduce(
-                (acc, item, idx) =>
-                    this.itemsForm.controls[idx].value || this.filterPredicate(value, item, idx, items)
-                        ? acc.concat(idx)
-                        : acc,
-                [] as number[]
+        map(([search, items]) =>
+            (items || []).filter(
+                (item, idx) => this.selectedItems$.value[item.id] || this.filterPredicate(search, item, idx, items)
             )
         ),
         shareReplay(1)
     );
 
     @Input() filterPredicate: (
-        value: string,
+        search: string,
         item: MultiselectFilterItem<T>,
         idx: number,
         items: MultiselectFilterItem<T>[]
@@ -57,48 +47,31 @@ export class MultiselectFilterContentComponent<T = any> implements OnInit, OnCha
 
     constructor(private fb: FormBuilder) {}
 
-    ngOnInit() {
-        (this.itemsForm.valueChanges as Observable<boolean[]>)
-            .pipe(
-                map((values) => this.getSelected(values)),
-                distinctUntilChanged((x, y) => x.length === y.length && x.every((v, idx) => v === y[idx]))
-            )
-            .subscribe((found) => this.foundChange.emit(found));
-    }
-
     ngOnChanges({ items, selectedItems }: ComponentChanges<MultiselectFilterContentComponent<T>>) {
         if (items && items.currentValue !== items.previousValue) {
-            const currentItems = items.currentValue || [];
-            this.items$.next(currentItems);
-            this.itemsForm.clear();
-            for ({} of currentItems) {
-                this.itemsForm.push(this.fb.control(false));
-            }
+            this.items$.next(items.currentValue);
         }
         if (selectedItems && selectedItems.currentValue !== selectedItems.previousValue) {
-            if (this.items) {
-                const currentSelectedItems = selectedItems.currentValue || [];
-                const newValue = new Array(this.items.length || 0).fill(false);
-                for (const selected of currentSelectedItems) {
-                    const selectedId = typeof selected === 'object' ? selected.id : selected;
-                    const idx = this.items.findIndex(({ id }) => id === selectedId);
-                    this.itemsForm.controls[idx].patchValue(true);
-                }
-            }
+            this.selectedItems$.next(
+                Object.fromEntries(selectedItems.currentValue.map((i) => [typeof i === 'object' ? i.id : i, true]))
+            );
         }
-    }
-
-    private getSelected(values: boolean[] = this.itemsForm.value) {
-        return values.reduce((acc, v, idx) => (v ? [...acc, this.items[idx]] : acc), [] as MultiselectFilterItem<T>[]);
     }
 
     clear() {
-        this.form.patchValue({ search: '' });
-        this.itemsForm.reset();
+        this.searchControl.patchValue('');
+        this.selectedItems$.next({});
     }
 
     save() {
-        this.form.patchValue({ search: '' });
-        this.selectedChange.emit(this.getSelected(this.form.value.items));
+        if (this.items) {
+            this.searchControl.patchValue('');
+            this.selectedChange.emit(this.items.filter(({ id }) => this.selectedItems$.value[id]));
+        }
+    }
+
+    toggle(id: MultiselectFilterItem<T>['id']) {
+        this.selectedItems$.next({ ...this.selectedItems$.value, [id]: !this.selectedItems$.value[id] });
+        console.log(this.selectedItems$.value);
     }
 }
