4c4d4e5d7f2e193762f03cb5396ddb0a56dd010e
Rinat Arsaev <krickray@gmail.com>
2020-07-16 16:41:04 +0300

    refactor to panel info

--- src/app/sections/claim-modification-containers/document-container/to-panel-info.ts
+++ src/app/sections/claim-modification-containers/document-container/to-panel-info.ts
@@ -1,43 +1,18 @@
 import isEmpty from 'lodash.isempty';
-import last from 'lodash.last';
-import negate from 'lodash.negate';
-import { combineLatest, iif, merge, Observable, of } from 'rxjs';
-import { filter, first, map, pluck, reduce, switchMap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
+import { isRussianIndividualEntityContractor, isRussianLegalEntityContractor } from '../../../api';
 import {
     BankAccount,
     ContactInfo,
+    Contractor,
     LegalOwnerInfo,
     QuestionaryData,
     RussianIndividualEntity,
-    RussianLegalEntity,
     ShopInfo,
 } from '../../../api-codegen/questionary';
 import { OrgInfo } from './org-info';
-
-const toPanelItem = <T>(type: PanelInfoType) => (s: Observable<T>): Observable<PanelInfo | null> =>
-    s.pipe(
-        map((item) => (item ? { type, item } : null)),
-        first()
-    );
-
-const toOrgInfo = (s: Observable<RussianLegalEntity | RussianIndividualEntity | null>): Observable<OrgInfo | null> =>
-    s.pipe(
-        switchMap((entity) =>
-            iif(
-                () => isEmpty(entity),
-                of(null),
-                of(entity).pipe(
-                    map(({ additionalInfo, name, inn, registrationInfo }) => ({
-                        additionalInfo,
-                        name,
-                        inn,
-                        registrationInfo,
-                    }))
-                )
-            )
-        )
-    );
 
 export type PanelInfoType =
     | 'shopInfo'
@@ -52,33 +27,48 @@ export interface PanelInfo {
     item: ShopInfo | BankAccount | LegalOwnerInfo | RussianIndividualEntity | OrgInfo | ContactInfo;
 }
 
-export const toPanelInfo = (s: Observable<QuestionaryData>): Observable<PanelInfo[]> => {
-    const individualEntityItem$ = s.pipe(pluck('contractor', 'individualEntity'), toPanelItem('individualEntityInfo'));
-    const shopInfoItem$ = s.pipe(pluck('shopInfo'), toPanelItem('shopInfo'));
-    const legalOwnerInfoItem$ = s.pipe(
-        pluck('contractor', 'legalEntity', 'legalOwnerInfo'),
-        toPanelItem('legalOwnerInfo')
-    );
-    const bankAccountItem$ = s.pipe(pluck('bankAccount'), toPanelItem('bankAccountInfo'));
-    const contactInfoItem$ = s.pipe(pluck('contactInfo'), toPanelItem('contactInfo'));
-    const orgInfoItem$ = combineLatest([
-        s.pipe(pluck('contractor', 'legalEntity')),
-        s.pipe(pluck('contractor', 'individualEntity')),
-    ]).pipe(
-        map((entities) => entities.filter(negate(isEmpty))),
-        map(last),
-        toOrgInfo,
-        toPanelItem('orgInfo')
-    );
-    return merge(
-        orgInfoItem$,
-        individualEntityItem$,
-        legalOwnerInfoItem$,
-        shopInfoItem$,
-        bankAccountItem$,
-        contactInfoItem$
-    ).pipe(
-        filter(negate(isEmpty)),
-        reduce((a, c) => [...a, c], [])
-    );
+const contractorToPanelInfo = (contractor: Contractor): PanelInfo => {
+    if (isRussianIndividualEntityContractor(contractor)) {
+        return { type: 'individualEntityInfo', item: contractor.individualEntity };
+    } else if (isRussianLegalEntityContractor(contractor)) {
+        return { type: 'legalOwnerInfo', item: contractor.legalEntity.legalOwnerInfo };
+    }
+    console.error('Unknown contractor');
+    return null;
 };
+
+const contractorToEntity = (contractor: Contractor) => {
+    if (isRussianIndividualEntityContractor(contractor)) {
+        return contractor.individualEntity;
+    } else if (isRussianLegalEntityContractor(contractor)) {
+        return contractor.legalEntity;
+    }
+    console.error('Unknown contractor');
+    return null;
+};
+
+const contractorToOrgInfo = (contractor: Contractor): PanelInfo => {
+    const entity = contractorToEntity(contractor);
+    if (entity) {
+        const { additionalInfo, name, inn, registrationInfo } = entity;
+        return { type: 'orgInfo', item: { additionalInfo, name, inn, registrationInfo } };
+    }
+    console.error('Unknown contractor');
+    return null;
+};
+
+export const toPanelInfo = (s: Observable<QuestionaryData>): Observable<PanelInfo[]> =>
+    s.pipe(
+        map((data) => {
+            const panelInfo: PanelInfo[] = [];
+            if (data.contractor) {
+                panelInfo.push(contractorToOrgInfo(data.contractor), contractorToPanelInfo(data.contractor));
+            }
+            panelInfo.push(
+                { type: 'shopInfo', item: data.shopInfo },
+                { type: 'bankAccountInfo', item: data.bankAccount },
+                { type: 'contactInfo', item: data.contactInfo }
+            );
+            return panelInfo.filter((e) => !isEmpty(e?.item));
+        })
+    );
