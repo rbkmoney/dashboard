015e8e1f631398543f1f5f83f925d5314b0f0ed5
Aleksandra Usacheva <lazercore@me.com>
2020-07-21 11:58:17 +0300

    FE-1070: small margin fix

--- src/components/global-banner/global-banner.component.ts
+++ src/components/global-banner/global-banner.component.ts
@@ -37,7 +37,7 @@ export class GlobalBannerComponent implements AfterViewInit, OnDestroy {
     }
 
     ngAfterViewInit(): void {
-        document.body.style.paddingBottom = this.elRef.nativeElement.getBoundingClientRect().height + 32 + 16 + 'px';
+        document.body.style.paddingBottom = this.elRef.nativeElement.getBoundingClientRect().height + 32 + 'px';
     }
 
     ngOnDestroy(): void {
