5165014f599cf4b4cdd3ccc3758136f6f2c24714
Denis Ezhov <ezhoden@mail.ru>
2020-07-23 15:33:17 +0300

    wallet payout (#265)
    * wallet payout
    
    * master merge fix
    
    * fixes
    
    * logs removed

--- src/app/api/payouts/payouts.service.ts
+++ src/app/api/payouts/payouts.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
-import { PayoutsService as PayoutsAPIService } from '../../api-codegen/capi/swagger-codegen';
+import {
+    Payout,
+    PayoutParams,
+    PayoutsService as PayoutsAPIService,
+    PayoutTool,
+} from '../../api-codegen/capi/swagger-codegen';
 import { genXRequestID } from '../utils';
 
 @Injectable()
 export class PayoutsService {
     constructor(private payoutsService: PayoutsAPIService) {}
+
+    createPayout(params: PayoutParams): Observable<Payout> {
+        return this.payoutsService.createPayout(genXRequestID(), params);
+    }
+
+    getPayoutTools(contractID: string): Observable<PayoutTool[]> {
+        return this.payoutsService.getPayoutTools(genXRequestID(), contractID);
+    }
 
     getPayoutToolByID(contractID: string, payoutToolID: string) {
         return this.payoutsService.getPayoutToolByID(genXRequestID(), contractID, payoutToolID);
---
+++ src/app/sections/payment-section/payouts/create-payout-dialog/index.ts
@@ -0,0 +1,1 @@
+export * from './create-payout-dialog.component';
