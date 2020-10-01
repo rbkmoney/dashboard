0c44a45bf9c059ac267e34c531d6b1e032effaeb
DenisHedgehog <ezhoden@mail.ru>
2020-10-01 19:19:59 +0300

    prettier

--- src/app/sections/payment-section/reports/create-report/form-value-to-create-value.ts
+++ src/app/sections/payment-section/reports/create-report/form-value-to-create-value.ts
@@ -5,9 +5,10 @@ import { CreateReportReq } from '../../../../api/reports/create-reports';
 export const formValueToCreateValue = ({ fromDate, fromTime, toDate, toTime, shopID }): CreateReportReq => ({
     fromTime: getDateWithTime(fromDate, fromTime),
     toTime: getDateWithTime(toDate, toTime),
-    shopID: shopID || undefined
+    shopID: shopID || undefined,
 });
 
-const getDateWithTime = (date: string, time: string): string => moment(`${moment(date).utc().format().substring(0, 10)}, ${time}`, 'YYYY-MM-DD, HH:mm:ss')
-    .utc()
-    .format();
+const getDateWithTime = (date: string, time: string): string =>
+    moment(`${moment(date).utc().format().substring(0, 10)}, ${time}`, 'YYYY-MM-DD, HH:mm:ss')
+        .utc()
+        .format();
