0afa2fae834e5b5c9c0abb89f153c7311a1a8b03
Denis Ezhov <ezhoden@mail.ru>
2020-10-09 10:39:41 +0300

    analytics filters refactoring (#295)
    * analytics filters refactoring
    
    * fixes
    
    * fixes
    
    * prettier

--- src/app/sections/payment-section/analytics/analytics-search-filters-store.service.ts
+++ src/app/sections/payment-section/analytics/analytics-search-filters-store.service.ts
@@ -5,24 +5,24 @@ import pickBy from 'lodash.pickby';
 import { QueryParamsStore } from '@dsh/app/shared/services';
 
 import { wrapValuesToArray } from '../../../../utils';
-import { SearchFiltersParams } from './reports-search-filters';
+import { SearchParams } from './search-params';
 
-const reportTypesAndPrimitives = (v, k) => (typeof v === 'string' || typeof v === 'number') && k === 'reportTypes';
+const shopTypeToArray = (v, k) => typeof v === 'string' && k === 'shopIDs';
 
 @Injectable()
-export class ReportsSearchFiltersStore extends QueryParamsStore<SearchFiltersParams> {
+export class AnalyticsSearchFiltersStore extends QueryParamsStore<SearchParams> {
     constructor(protected route: ActivatedRoute, protected router: Router) {
         super(router, route);
     }
 
-    mapToData(queryParams: Params): Partial<SearchFiltersParams> {
+    mapToData(queryParams: Params): Partial<SearchParams> {
         return {
             ...queryParams,
-            ...wrapValuesToArray(pickBy(queryParams, reportTypesAndPrimitives)),
+            ...wrapValuesToArray(pickBy(queryParams, shopTypeToArray)),
         };
     }
 
-    mapToParams(data: SearchFiltersParams): Params {
+    mapToParams(data: SearchParams): Params {
         return data;
     }
 }
