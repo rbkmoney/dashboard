42b5216d255aea11684378f2f221400e37190886
Rinat Arsaev <krickray@gmail.com>
2020-10-23 15:22:22 +0300

    new tab links style

--- src/components/layout/tabs/ink-bar.directive.ts
+++ src/components/layout/tabs/ink-bar.directive.ts
@@ -1,18 +1,28 @@
-import { Directive, ElementRef, HostBinding, Inject, InjectionToken, NgZone } from '@angular/core';
+import { Directive, ElementRef, HostBinding, Inject, InjectionToken, Input, NgZone } from '@angular/core';
 
-export type _DshInkBarPositioner = (element: HTMLElement) => { left: string; width: string };
+export type _DshInkBarPositioner = (element: HTMLElement, options?: StyleOptions) => { left: string; width: string };
 
 export const _DSH_INK_BAR_POSITIONER = new InjectionToken<_DshInkBarPositioner>('DshInkBarPositioner', {
     providedIn: 'root',
     factory: _DSH_INK_BAR_POSITIONER_FACTORY,
 });
 
-export function _DSH_INK_BAR_POSITIONER_FACTORY(): _DshInkBarPositioner {
-    const method = (element: HTMLElement) => ({
-        left: element ? (element.offsetLeft || 0) + 'px' : '0',
-        width: element ? (element.offsetWidth || 0) + 'px' : '0',
-    });
+interface StyleOptions {
+    offset?: { left?: number; right?: number };
+}
 
+export function _DSH_INK_BAR_POSITIONER_FACTORY(): _DshInkBarPositioner {
+    const method = (element: HTMLElement, options: StyleOptions) =>
+        element
+            ? {
+                  left: (element.offsetLeft || 0) + (options?.offset?.left || 0) + 'px',
+                  width:
+                      (element.offsetWidth || 0) - (options?.offset?.left || 0) - (options?.offset?.right || 0) + 'px',
+              }
+            : {
+                  left: '0',
+                  width: '0',
+              };
     return method;
 }
 
@@ -20,24 +30,23 @@ export function _DSH_INK_BAR_POSITIONER_FACTORY(): _DshInkBarPositioner {
     selector: 'dsh-ink-bar',
 })
 export class DshInkBarDirective {
-    @HostBinding('class.dsh-ink-bar')
-    classInkBar = true;
+    @HostBinding('class.dsh-ink-bar') classInkBar = true;
 
     constructor(
         private _elementRef: ElementRef<HTMLElement>,
         private _ngZone: NgZone,
-        @Inject(_DSH_INK_BAR_POSITIONER) private _inkBarPositioner: any
+        @Inject(_DSH_INK_BAR_POSITIONER) private _inkBarPositioner: _DshInkBarPositioner
     ) {}
 
-    alignToElement(element: HTMLElement) {
+    alignToElement(element: HTMLElement, options?: StyleOptions) {
         this.show();
 
         if (typeof requestAnimationFrame !== 'undefined') {
             this._ngZone.runOutsideAngular(() => {
-                requestAnimationFrame(() => this._setStyles(element));
+                requestAnimationFrame(() => this._setStyles(element, options));
             });
         } else {
-            this._setStyles(element);
+            this._setStyles(element, options);
         }
     }
 
@@ -45,8 +54,8 @@ export class DshInkBarDirective {
 
     hide = () => (this._elementRef.nativeElement.style.visibility = 'hidden');
 
-    private _setStyles(element: HTMLElement) {
-        const positions = this._inkBarPositioner(element);
+    private _setStyles(element: HTMLElement, options?: StyleOptions) {
+        const positions = this._inkBarPositioner(element, options);
         const inkBar: HTMLElement = this._elementRef.nativeElement;
 
         inkBar.style.left = positions.left;
