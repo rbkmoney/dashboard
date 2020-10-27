08fa4a2cea71d273ead88d18eef6fddd53c27602
Rinat Arsaev <krickray@gmail.com>
2020-10-28 01:45:54 +0300

    remove claims dropdown

--- src/app/container/actionbar/claims/claims-list/claims-list.component.ts
+++
@@ -1,8 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-    selector: 'dsh-claims-list',
-    templateUrl: 'claims-list.component.html',
-    changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class ClaimsListComponent {}
