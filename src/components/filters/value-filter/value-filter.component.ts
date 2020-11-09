878e90e728abb342d767363398e02b0e2c277bd9
Rinat Arsaev <krickray@gmail.com>
2020-11-09 04:32:05 +0300

    add value filters

---
+++ src/components/filters/value-filter/value-filter.component.ts
@@ -0,0 +1,64 @@
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnChanges, Output } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { ReplaySubject } from 'rxjs';
+import { distinctUntilChanged, map, shareReplay, startWith } from 'rxjs/operators';
+
+import { ComponentChanges } from '../../../type-utils';
+
+@Component({
+    selector: 'dsh-value-filter',
+    templateUrl: 'value-filter.component.html',
+    styleUrls: ['value-filter.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ValueFilterComponent implements OnChanges {
+    @Input() label: string;
+    @Input() selectedLabel?: string;
+    @Input() selectedLabelPredicate?: (value: string) => string;
+    @Input() placeholder?: string;
+
+    @Input() value: string;
+    @Output() valueChanges = new EventEmitter<string>();
+    private savedValue$ = new ReplaySubject<string>();
+
+    control = new FormControl();
+    label$ = this.savedValue$.pipe(
+        startWith(undefined),
+        map((v) => {
+            if (!v) {
+                return this.label;
+            }
+            if (this.selectedLabelPredicate) {
+                return this.selectedLabelPredicate(v);
+            }
+            return this.selectedLabel || this.label;
+        }),
+        shareReplay(1)
+    );
+    active$ = this.savedValue$.pipe(
+        map((v) => !!v),
+        shareReplay(1)
+    );
+
+    constructor() {
+        this.savedValue$.pipe(distinctUntilChanged()).subscribe((v) => {
+            this.valueChanges.next(v);
+            this.value = v;
+        });
+    }
+
+    ngOnChanges({ value }: ComponentChanges<ValueFilterComponent>) {
+        if (value) {
+            this.control.setValue(value.currentValue);
+            this.save();
+        }
+    }
+
+    clear() {
+        this.control.setValue(undefined);
+    }
+
+    save() {
+        this.savedValue$.next(this.control.value);
+    }
+}
