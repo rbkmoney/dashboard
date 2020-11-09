807564a751b3688eaddaf1b7f4d420b1689bfc04
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2020-11-09 15:44:40 +0300

    Add value filters (#311)

--- src/components/filters/filters.module.ts
+++ src/components/filters/filters.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 
 import { DaterangeFilterModule } from './daterange-filter';
 import { FilterModule } from './filter';
+import { MultiValueFilterModule } from './multi-value-filter/multi-value-filter.module';
 import { MultiselectFilterModule } from './multiselect-filter';
+import { ValueFilterModule } from './value-filter';
 
 @NgModule({
-    exports: [FilterModule, DaterangeFilterModule, MultiselectFilterModule],
+    exports: [FilterModule, DaterangeFilterModule, MultiselectFilterModule, ValueFilterModule, MultiValueFilterModule],
 })
 export class FiltersModule {}
