c96e2b0837058e1a65d9c3493963b66d9fcd309d
Ildar Galeev <KeinAsylum@gmail.com>
2020-11-27 11:33:13 +0300

    Add bootstrap service with getMyParty call (#328)

---
+++ src/app/bootstrap.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { iif, Observable, of, ReplaySubject } from 'rxjs';
+import { catchError, first, map, shareReplay, switchMap, switchMapTo, tap } from 'rxjs/operators';
+
+import { ApiShopsService, CAPIClaimsService, CAPIPartiesService, createTestShopClaimChangeset } from './api';
+
+@Injectable()
+export class BootstrapService {
+    bootstrapped$: Observable<boolean>;
+
+    private bootstrap$ = new ReplaySubject(1);
+
+    constructor(
+        private partiesService: CAPIPartiesService,
+        private shopService: ApiShopsService,
+        private capiClaimsService: CAPIClaimsService
+    ) {
+        this.bootstrapped$ = this.bootstrap$.pipe(
+            first(),
+            switchMapTo(this.partiesService.getMyParty()),
+            switchMapTo(this.shopService.shops$.pipe(first())),
+            switchMap((shops) =>
+                iif(
+                    () => shops.length === 0,
+                    this.createTestShop().pipe(tap(() => this.shopService.reloadShops())),
+                    of(true)
+                )
+            ),
+            catchError((err) => {
+                console.error(err);
+                return of(false);
+            }),
+            shareReplay(1)
+        );
+    }
+
+    bootstrap() {
+        this.bootstrap$.next();
+    }
+
+    private createTestShop(): Observable<boolean> {
+        return this.capiClaimsService.createClaim(createTestShopClaimChangeset()).pipe(map(() => true));
+    }
+}
