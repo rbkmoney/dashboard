0b1e5471ae3cc60b93bfca1bce257910a0076d5b
Aleksandra Usacheva <lazercore@me.com>
2020-12-01 08:48:08 +0300

    Build fix

--- src/components/layout/accordion/accordion.module.ts
+++ src/components/layout/accordion/accordion.module.ts
@@ -1,13 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatDividerModule } from "@angular/material/divider";
+import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 
 import { ButtonModule } from '../../buttons';
 import { ResizedModule } from '../../indicators';
 import { AccordionItemComponent } from './accordion-item';
-import { AccordionItemActionsComponent } from "./accordion-item-actions";
+import { AccordionItemActionsComponent } from './accordion-item-actions';
 import { AccordionItemContentComponent } from './accordion-item-content';
 import { AccordionItemContentHeaderComponent } from './accordion-item-content-header';
 import { AccordionComponent } from './accordion.component';
