11d09568aed29ebd25dd6182e644d4f4a1da723f
Aleksandra Usacheva <lazercore@me.com>
2020-12-03 06:15:02 +0300

    trying build

--- src/app/sections/claims/claims.component.ts
+++ src/app/sections/claims/claims.component.ts
@@ -1,15 +1,12 @@
-import { Component, Inject } from '@angular/core';
-import { take } from 'rxjs/operators';
 import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { take } from 'rxjs/operators';
 
 import { SpinnerType } from '@dsh/components/indicators';
 
 import { LAYOUT_GAP } from '../constants';
 import { ClaimsSearchFiltersStore } from './claims-search-filters-store.service';
 import { ClaimsSearchFiltersSearchParams } from './claims-search-filters/claims-search-filters-search-params';
-import { ClaimsService } from './claims.service';
-import { ClaimSearchFormValue } from './search-form';
 import { ClaimsExpandedIdManagerService } from './services/claims-expanded-id-manager/claims-expanded-id-manager.service';
 import { FetchClaimsService } from './services/fetch-claims/fetch-claims.service';
 
@@ -17,34 +14,29 @@ import { FetchClaimsService } from './services/fetch-claims/fetch-claims.service
     selector: 'dsh-claims',
     templateUrl: 'claims.component.html',
     styleUrls: ['claims.component.scss'],
-    providers: [ClaimsService, ClaimsSearchFiltersStore],
-    providers: [FetchClaimsService, ClaimsExpandedIdManagerService],
+    providers: [FetchClaimsService, ClaimsExpandedIdManagerService, ClaimsSearchFiltersStore],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ClaimsComponent {
     claimsList$ = this.fetchClaimsService.searchResult$;
+    expandedId$ = this.claimsExpandedIdManagerService.expandedId$;
     isLoading$ = this.fetchClaimsService.isLoading$;
     lastUpdated$ = this.fetchClaimsService.lastUpdated$;
     hasMore$ = this.fetchClaimsService.hasMore$;
-    expandedId$ = this.claimsExpandedIdManagerService.expandedId$;
-    tableData$ = this.claimsService.claimsTableData$;
-    isLoading$ = this.claimsService.isLoading$;
-    lastUpdated$ = this.claimsService.lastUpdated$;
-    hasMore$ = this.claimsService.hasMore$;
     initSearchParams$ = this.claimsSearchFiltersStore.data$.pipe(take(1));
 
     spinnerType = SpinnerType.FulfillingBouncingCircle;
 
     constructor(
         @Inject(LAYOUT_GAP) public layoutGap: string,
-        private claimsSearchFiltersStore: ClaimsSearchFiltersStore
+        private claimsSearchFiltersStore: ClaimsSearchFiltersStore,
         private fetchClaimsService: FetchClaimsService,
         private claimsExpandedIdManagerService: ClaimsExpandedIdManagerService,
         private router: Router
     ) {}
 
     search(val: ClaimsSearchFiltersSearchParams) {
-        this.claimsService.search(val);
+        this.fetchClaimsService.search(val);
     }
 
     fetchMore() {
--- src/app/sections/claims/services/fetch-claims/fetch-claims.service.ts
+++ src/app/sections/claims/services/fetch-claims/fetch-claims.service.ts
@@ -4,34 +4,27 @@ import { TranslocoService } from '@ngneat/transloco';
 import { Observable } from 'rxjs';
 import { shareReplay } from 'rxjs/operators';
 
-import { Claim } from '../../api-codegen/claim-management/swagger-codegen';
-import { ClaimsService as ApiClaimsService } from '../../api/claims/claims.service';
-import { booleanDebounceTime, takeError } from '../../custom-operators';
-import { FetchResult, PartialFetcher } from '../partial-fetcher';
-import { mapToTimestamp } from '../payment-section/operations/operators';
-import { ClaimsSearchFiltersSearchParams } from './claims-search-filters/claims-search-filters-search-params';
-import { mapToClaimsTableData } from './map-to-claims-table-data';
-import { ClaimsTableData } from './table';
+import { Claim } from '../../../../api-codegen/claim-management/swagger-codegen';
+import { ClaimsService } from '../../../../api/claims';
+import { booleanDebounceTime } from '../../../../custom-operators';
+import { FetchResult, PartialFetcher } from '../../../partial-fetcher';
+import { mapToTimestamp } from '../../../payment-section/operations/operators';
+import { ClaimsSearchFiltersSearchParams } from '../../claims-search-filters/claims-search-filters-search-params';
 
 @Injectable()
-export class ClaimsService extends PartialFetcher<Claim, ClaimsSearchFiltersSearchParams> {
+export class FetchClaimsService extends PartialFetcher<Claim, ClaimsSearchFiltersSearchParams> {
     private readonly searchLimit = 20;
 
     lastUpdated$: Observable<string> = this.searchResult$.pipe(mapToTimestamp);
-
-    claimsTableData$: Observable<ClaimsTableData[]> = this.searchResult$.pipe(mapToClaimsTableData);
-
-    error$ = this.claimsTableData$.pipe(takeError);
-
     isLoading$: Observable<boolean> = this.doAction$.pipe(booleanDebounceTime(), shareReplay(1));
 
     constructor(
-        private claimsService: ApiClaimsService,
+        private claimsService: ClaimsService,
         private snackBar: MatSnackBar,
         private transloco: TranslocoService
     ) {
         super();
-        this.error$.subscribe(() => {
+        this.errors$.subscribe(() => {
             this.snackBar.open(this.transloco.translate('httpError'), 'OK');
             return [];
         });
