57521d1dbc2c92e4586af7f1ea189a3f98ddb342
DenisHedgehog <ezhoden@mail.ru>
2020-12-08 15:32:20 +0300

    search limit to 10

--- src/app/sections/constants.ts
+++ src/app/sections/constants.ts
@@ -2,5 +2,5 @@ import { InjectionToken } from '@angular/core';
 
 export const LAYOUT_GAP = new InjectionToken<string>('layoutGap');
 
-export const DEFAULT_SEARCH_LIMIT = 20;
+export const DEFAULT_SEARCH_LIMIT = 10;
 export const SEARCH_LIMIT = new InjectionToken('searchLimit');
