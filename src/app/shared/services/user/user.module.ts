62d1069e60eb1a64fee52bafa9be1801f4f50bd7
Rinat Arsaev <krickray@gmail.com>
2020-12-24 13:34:03 +0300

    rem epty

--- src/app/shared/services/user/user.module.ts
+++ src/app/shared/services/user/user.module.ts
@@ -2,12 +2,7 @@ import { NgModule } from '@angular/core';
 
 import { UserService } from './user.service';
 
-const EXPORTED_DECLARATIONS = [];
-
 @NgModule({
-    imports: [],
-    declarations: EXPORTED_DECLARATIONS,
-    exports: EXPORTED_DECLARATIONS,
     providers: [UserService],
 })
 export class UserModule {}
