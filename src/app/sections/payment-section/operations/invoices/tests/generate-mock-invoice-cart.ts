dda340817d63cb4428aea18add8b8e4b4177cfc4
DenisHedgehog <ezhoden@mail.ru>
2020-12-28 13:12:11 +0300

    one more thing (fix)

--- src/app/sections/payment-section/operations/invoices/tests/generate-mock-invoice-cart.ts
+++ src/app/sections/payment-section/operations/invoices/tests/generate-mock-invoice-cart.ts
@@ -3,9 +3,5 @@ import { InvoiceCart } from '@dsh/api-codegen/capi/swagger-codegen';
 import { generateMockInvoiceLine } from './generate-mock-invoice-line';
 
 export function generateMockInvoiceCart(count: number): InvoiceCart {
-    const cart: InvoiceCart = [];
-    for (let i = 0; i < count; i += 1) {
-        cart.push(generateMockInvoiceLine());
-    }
-    return cart;
+    return new Array(count).fill(generateMockInvoiceLine());
 }
