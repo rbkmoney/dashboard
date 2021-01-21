86b65523e94d0ffcc5048163ca3154dab1be2ef5
Rinat Arsaev <krickray@gmail.com>
2021-01-21 11:31:54 +0300

    lint fix

--- src/app/api/sender/services/messages/messages.service.ts
+++ src/app/api/sender/services/messages/messages.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@angular/core';
 import { MessagesService as ApiMessagesService } from '../../../../api-codegen/sender';
 import { UuidGeneratorService } from '../../../../shared/services/uuid-generator/uuid-generator.service';
 
-
 @Injectable()
 export class MessagesService {
     constructor(private messagesService: ApiMessagesService, private idGeneratorService: UuidGeneratorService) {}
