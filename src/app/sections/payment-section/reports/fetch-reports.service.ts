1b5ce66461facb5a43c4ef55e0700dfbaa02661b
hedgehog <ezhoden@mail.ru>
2021-01-28 17:56:19 +0300

    fix

--- src/app/sections/payment-section/reports/fetch-reports.service.ts
+++ src/app/sections/payment-section/reports/fetch-reports.service.ts
@@ -1,18 +1,26 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import isEmpty from 'lodash.isempty';
 import { pluck, switchMap, take } from 'rxjs/operators';
 
 import { Report } from '@dsh/api-codegen/anapi';
 import { ReportsService as ReportsApiService } from '@dsh/api/reports';
+import { SEARCH_LIMIT } from '@dsh/app/sections/tokens';
 
-import { IndicatorsPartialFetcher } from '../../partial-fetcher';
+import { DEBOUNCE_FETCHER_ACTION_TIME, IndicatorsPartialFetcher } from '../../partial-fetcher';
 import { SearchFiltersParams } from './reports-search-filters';
 
 @Injectable()
 export class FetchReportsService extends IndicatorsPartialFetcher<Report, SearchFiltersParams> {
-    constructor(private reportsService: ReportsApiService, private route: ActivatedRoute) {
-        super();
+    constructor(
+        private reportsService: ReportsApiService,
+        private route: ActivatedRoute,
+        @Inject(SEARCH_LIMIT)
+        protected searchLimit: number,
+        @Inject(DEBOUNCE_FETCHER_ACTION_TIME)
+        protected debounceActionTime: number
+    ) {
+        super(searchLimit, debounceActionTime);
     }
 
     protected fetch(p: SearchFiltersParams, continuationToken: string) {
