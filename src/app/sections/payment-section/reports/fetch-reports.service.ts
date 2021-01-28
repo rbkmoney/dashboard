f12c81a30caf8c6c2062f6d0111dbd40de09b548
hedgehog <ezhoden@mail.ru>
2021-01-28 17:37:01 +0300

    use IndicatorsPartialFetcher

--- src/app/sections/payment-section/reports/fetch-reports.service.ts
+++ src/app/sections/payment-section/reports/fetch-reports.service.ts
@@ -1,21 +1,16 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import isEmpty from 'lodash.isempty';
-import { Observable } from 'rxjs';
-import { pluck, shareReplay, switchMap, take } from 'rxjs/operators';
+import { pluck, switchMap, take } from 'rxjs/operators';
 
 import { Report } from '@dsh/api-codegen/anapi';
 import { ReportsService as ReportsApiService } from '@dsh/api/reports';
 
-import { booleanDebounceTime, mapToTimestamp } from '../../../custom-operators';
-import { PartialFetcher } from '../../partial-fetcher';
+import { IndicatorsPartialFetcher } from '../../partial-fetcher';
 import { SearchFiltersParams } from './reports-search-filters';
 
 @Injectable()
-export class FetchReportsService extends PartialFetcher<Report, SearchFiltersParams> {
-    isLoading$: Observable<boolean> = this.doAction$.pipe(booleanDebounceTime(), shareReplay(1));
-    lastUpdated$: Observable<string> = this.searchResult$.pipe(mapToTimestamp, shareReplay(1));
-
+export class FetchReportsService extends IndicatorsPartialFetcher<Report, SearchFiltersParams> {
     constructor(private reportsService: ReportsApiService, private route: ActivatedRoute) {
         super();
     }
