69b6a72f6b5d9e28ef2ca5571126c431b62ed61c
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2021-01-29 12:33:57 +0300

    rename IdGeneratorService

--- src/app/api/organizations/organizations.service.ts
+++ src/app/api/organizations/organizations.service.ts
@@ -21,60 +21,60 @@ export class OrganizationsService {
         private orgsService: OrgsService,
         private rolesService: RolesService,
         private membersService: MembersService,
-        private uuidGeneratorService: IdGeneratorService,
+        private idGeneratorService: IdGeneratorService,
         private invitationsService: InvitationsService
     ) {}
 
     listOrgMembership(limit?: number, continuationToken?: string) {
-        return this.orgsService.listOrgMembership(this.uuidGeneratorService.generateUUID(), limit, continuationToken);
+        return this.orgsService.listOrgMembership(this.idGeneratorService.generateUUID(), limit, continuationToken);
     }
 
     getOrg(orgId: Organization['id']) {
-        return this.orgsService.getOrg(this.uuidGeneratorService.generateUUID(), orgId);
+        return this.orgsService.getOrg(this.idGeneratorService.generateUUID(), orgId);
     }
 
     createOrg(org: WritableOrganization) {
         // expects only non-readable fields
-        return this.orgsService.createOrg(this.uuidGeneratorService.generateUUID(), org as Organization);
+        return this.orgsService.createOrg(this.idGeneratorService.generateUUID(), org as Organization);
     }
 
     patchOrg(orgId: Organization['id'], org: InlineObject) {
-        return this.orgsService.patchOrg(this.uuidGeneratorService.generateUUID(), orgId, org);
+        return this.orgsService.patchOrg(this.idGeneratorService.generateUUID(), orgId, org);
     }
 
     joinOrg(request: OrganizationJoinRequest) {
-        return this.orgsService.joinOrg(this.uuidGeneratorService.generateUUID(), request);
+        return this.orgsService.joinOrg(this.idGeneratorService.generateUUID(), request);
     }
 
     getOrgRole(orgId: Organization['id'], roleId: RoleId) {
-        return this.rolesService.getOrgRole(this.uuidGeneratorService.generateUUID(), orgId, roleId);
+        return this.rolesService.getOrgRole(this.idGeneratorService.generateUUID(), orgId, roleId);
     }
 
     getOrgMember(orgId: Organization['id'], userId: string) {
-        return this.membersService.getOrgMember(this.uuidGeneratorService.generateUUID(), orgId, userId);
+        return this.membersService.getOrgMember(this.idGeneratorService.generateUUID(), orgId, userId);
     }
 
     listOrgMembers(orgId: Organization['id']) {
-        return this.membersService.listOrgMembers(this.uuidGeneratorService.generateUUID(), orgId);
+        return this.membersService.listOrgMembers(this.idGeneratorService.generateUUID(), orgId);
     }
 
     expelOrgMember(orgId: Organization['id'], userId: string) {
-        return this.membersService.expelOrgMember(this.uuidGeneratorService.generateUUID(), orgId, userId);
+        return this.membersService.expelOrgMember(this.idGeneratorService.generateUUID(), orgId, userId);
     }
 
     cancelOrgMembership(orgId: Organization['id']) {
-        return this.orgsService.cancelOrgMembership(this.uuidGeneratorService.generateUUID(), orgId);
+        return this.orgsService.cancelOrgMembership(this.idGeneratorService.generateUUID(), orgId);
     }
 
     createInvitation(orgId: Organization['id'], invitation: Omit<Invitation, 'id' | 'createdAt'>) {
         return this.invitationsService.createInvitation(
-            this.uuidGeneratorService.generateUUID(),
+            this.idGeneratorService.generateUUID(),
             orgId,
             invitation as Invitation
         );
     }
 
     listInvitations(orgId: Organization['id']) {
-        return this.invitationsService.listInvitations(this.uuidGeneratorService.generateUUID(), orgId);
+        return this.invitationsService.listInvitations(this.idGeneratorService.generateUUID(), orgId);
     }
 }
--- src/app/api/utils/gen-x-request-id.ts
+++ src/app/api/utils/gen-x-request-id.ts
@@ -1,7 +1,7 @@
 import { randomString as s } from '../../../utils';
 
 /**
- * @deprecated use UuidGeneratorService
+ * @deprecated use IdGeneratorService
  */
 export function genXRequestID() {
     return `${s(8)}-${s(4)}-${s(4)}`;
