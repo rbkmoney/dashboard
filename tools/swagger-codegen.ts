26dd53e80d4de719f0a93a83d913d590e96231d5
KisyaLisya <42087770+KisyaLisya@users.noreply.github.com>
2021-02-04 16:56:06 +0300

    fixed swagger cross-platform issues

--- tools/swagger-codegen.ts
+++ tools/swagger-codegen.ts
@@ -1,11 +1,29 @@
 import * as del from 'del';
 import * as path from 'path';
+import * as shell from 'shelljs';
 
 import * as config from '../swagger-codegen-config.json';
 import { createLog } from './utils/create-log';
 import { execWithLog } from './utils/exec-with-log';
 
 type Schemes = { [name: string]: string };
+
+async function replaceModuleWithProvidersNg10(outputDirPath: string) {
+    const modulePath = `${outputDirPath}/api.module.ts`;
+    return new Promise((resolve, reject) => {
+        try {
+            const result = shell.sed('-i', /ModuleWithProviders\s/, `ModuleWithProviders<ApiModule> `, modulePath);
+            // tslint:disable-next-line:no-console
+            console.log(`Replaced "ModuleWithProviders" in "${modulePath}"`);
+            resolve(result);
+        } catch (err) {
+            // tslint:disable-next-line:no-console
+            console.error(err);
+            console.error(`Error on path "${modulePath}"`);
+            reject(err);
+        }
+    });
+}
 
 async function swaggerCodegenAngularCli({
     schemes,
@@ -31,9 +49,8 @@ async function swaggerCodegenAngularCli({
             const cmd = `java -jar ${cliPath} generate -l typescript-angular --additional-properties ngVersion=7 -i ${inputPath} -o ${outputDirPath}`;
             // angular 10 requires change "ModuleWithProviders" with "ModuleWithProviders<ApiModule>"
             // swagger-codegen has a fix in v2.4.17 but it breaks our backward compatibility
-            const postSwagger = `sed -i \'s/ModuleWithProviders {/ModuleWithProviders<ApiModule> {/\' ${outputDirPath}/api.module.ts\n`;
             swaggerLog(`> ${cmd}`);
-            return execWithLog(cmd).then(() => execWithLog(postSwagger));
+            return execWithLog(cmd).then(() => replaceModuleWithProvidersNg10(outputDirPath));
         })
     );
     swaggerLog('Successfully generated 😀');
