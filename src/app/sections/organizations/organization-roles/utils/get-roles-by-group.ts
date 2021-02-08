ac488a82c0c2569fadeb6ec97e3b934cc54c0e27
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2021-02-08 13:41:30 +0300

    revert

--- src/app/sections/organizations/organization-roles/utils/get-roles-by-group.ts
+++ src/app/sections/organizations/organization-roles/utils/get-roles-by-group.ts
@@ -8,30 +8,20 @@ const ROLE_PRIORITY_DESC: { [N in RoleId]: number } = {
     Manager: 3,
 };
 
-function addRoleGroup(groups: RoleGroup[], role: MemberRole): RoleGroup {
-    let group: RoleGroup = groups.find((g) => g.id === role.roleId);
-    if (!group) {
-        group = { id: role.roleId, scopes: [] };
-        groups.push(group);
-    }
-    return group;
-}
-
-function addRoleGroupScope(group: RoleGroup, role: MemberRole): void {
-    let scope: RoleGroupScope = group.scopes.find((s) => s.id === role.scope.id);
-    if (!scope) {
-        scope = { id: role.scope.id, resourcesIds: [] };
-        group.scopes.push(scope);
-    }
-    scope.resourcesIds.push(role.scope.resourceId);
-}
-
 export function getRolesByGroup(roles: MemberRole[]): RoleGroup[] {
     return roles
-        .reduce((groups, role) => {
-            const group = addRoleGroup(groups, role);
-            addRoleGroupScope(group, role);
+        .reduce<RoleGroup[]>((groups, role) => {
+            let group: RoleGroup = groups.find((g) => g.id === role.roleId);
+            if (!group) {
+                group = { id: role.roleId, scopes: [] };
+                groups.push(group);
+            }
+            const scope: RoleGroupScope = group.scopes.find((s) => s.id === role.scope.id) ?? {
+                id: role.scope.id,
+                resourcesIds: [],
+            };
+            scope.resourcesIds.push(role.scope.resourceId);
             return groups;
-        }, [] as RoleGroup[])
+        }, [])
         .sort((a, b) => ROLE_PRIORITY_DESC[a.id] - ROLE_PRIORITY_DESC[b.id]);
 }
