b89d174f3778359007ae9adf15910e139cf68091
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2021-02-17 13:43:09 +0300

    rem unused

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -10,25 +10,6 @@ export type Theme = {
 
 export const THEME = new InjectionToken<Theme>('theme');
 
-// const CHART_PALETTE = [
-//     '#67DAAA',
-//     '#FC9B51',
-//     '#FB7777',
-//     '#9E9E9E',
-//     '#695BFF',
-//     '#E9E452',
-//     '#5B9FFF',
-//     '#BB5BFF',
-//     '#CD3876',
-//     '#FFAB91',
-//     '#38C1CD',
-//     '#FB7777',
-//     '#81C784',
-//     '#FFCC80',
-//     '#BEF67A',
-//     '#FFF8E1',
-// ];
-
 export const DEFAULT_THEME_CONFIG: Theme = {
     charts: {
         stackedBarChartColors: ['#67DAAA', '#FC9B51', '#FB7777', '#9E9E9E'],
