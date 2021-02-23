2592a477c8a0446b30026d71531af3684bb56925
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2021-02-23 23:49:38 +0300

    ref obs

--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -1,7 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
-import { Observable } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
+import { distinctUntilChanged, filter, mapTo, startWith } from 'rxjs/operators';
 
 import { ConfigService } from '../config';
 import { ThemeManager } from '../theme-manager';
@@ -13,7 +12,12 @@ import { ThemeManager } from '../theme-manager';
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ContainerComponent {
-    routerNavigationEnd$: Observable<boolean>;
+    routerNavigationEnd$ = this.router.events.pipe(
+        filter((e) => e instanceof NavigationEnd),
+        mapTo(true),
+        startWith(false),
+        distinctUntilChanged()
+    );
 
     get imageUrls() {
         return this.configService.theme.backgroundImageUrls;
@@ -23,10 +27,5 @@ export class ContainerComponent {
         return this.router.url === '/' && this.themeManager.hasMainBackground;
     }
 
-    constructor(private router: Router, private configService: ConfigService, private themeManager: ThemeManager) {
-        this.routerNavigationEnd$ = router.events.pipe(
-            filter((e) => e instanceof NavigationEnd),
-            map(() => true)
-        );
-    }
+    constructor(private router: Router, private configService: ConfigService, private themeManager: ThemeManager) {}
 }
