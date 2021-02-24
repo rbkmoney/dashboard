e1f0e338f6fb0216363b1f56da7f77fa87e713bf
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2021-02-24 11:46:18 +0300

    simplify appConfig.json

--- src/app/sections/sections.module.ts
+++ src/app/sections/sections.module.ts
@@ -10,7 +10,14 @@ import { DEBOUNCE_FETCHER_ACTION_TIME, DEFAULT_FETCHER_DEBOUNCE_ACTION_TIME } fr
 import { CHARTS_THEME } from './payment-section/analytics/charts-theme';
 import { SectionsRoutingModule } from './sections-routing.module';
 import { SectionsComponent } from './sections.component';
-import { DEFAULT_DIALOG_CONFIG, DEFAULT_SEARCH_LIMIT, DIALOG_CONFIG, LAYOUT_GAP, SEARCH_LIMIT } from './tokens';
+import {
+    DEFAULT_CHARTS_THEME,
+    DEFAULT_DIALOG_CONFIG,
+    DEFAULT_SEARCH_LIMIT,
+    DIALOG_CONFIG,
+    LAYOUT_GAP,
+    SEARCH_LIMIT,
+} from './tokens';
 
 @NgModule({
     imports: [MainModule, SectionsRoutingModule, ShopModule, WalletModule],
@@ -21,7 +28,7 @@ import { DEFAULT_DIALOG_CONFIG, DEFAULT_SEARCH_LIMIT, DIALOG_CONFIG, LAYOUT_GAP,
         { provide: SEARCH_LIMIT, useValue: DEFAULT_SEARCH_LIMIT },
         { provide: DIALOG_CONFIG, useValue: DEFAULT_DIALOG_CONFIG },
         { provide: DEBOUNCE_FETCHER_ACTION_TIME, useValue: DEFAULT_FETCHER_DEBOUNCE_ACTION_TIME },
-        { provide: CHARTS_THEME, useFactory: () => inject(ConfigService).theme.components.charts },
+        { provide: CHARTS_THEME, useValue: DEFAULT_CHARTS_THEME },
         { provide: SPINNER_THEME, useFactory: () => inject(ConfigService).theme.components.spinner },
     ],
 })
