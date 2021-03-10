f3d1440b18e0875385a34d47f5e3ca7be8228be5
Ildar Galeev <KeinAsylum@gmail.com>
2021-03-10 21:04:47 +0300

    Add revoke reasons list. Fix in progress state. Add UntilDestroy (#400)

--- src/app/sections/claim/revoke-claim-dialog/revoke-claim-dialog.component.ts
+++ src/app/sections/claim/revoke-claim-dialog/revoke-claim-dialog.component.ts
@@ -4,11 +4,19 @@ import { RevokeClaimDialogService } from './revoke-claim-dialog.service';
 
 @Component({
     templateUrl: 'revoke-claim-dialog.component.html',
-    styleUrls: ['revoke-claim-dialog.component.scss'],
     providers: [RevokeClaimDialogService],
 })
 export class RevokeClaimDialogComponent {
     form = this.revokeClaimDialogService.form;
+
+    // It's necessary to hardcode reasons (backend issue)
+    reasons = [
+        'Длительное ожидание подключения',
+        'Не устраивает комиссия',
+        'Большой пакет документов',
+        'Не подходит продукт',
+        'Нет сплитов',
+    ];
 
     errorCode$ = this.revokeClaimDialogService.errorCode$;
     inProgress$ = this.revokeClaimDialogService.inProgress$;
