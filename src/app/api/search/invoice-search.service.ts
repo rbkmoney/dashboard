84414c7546680f435ea2d96a0e11aa708a02d195
Ildar Galeev <keinasylum@gmail.com>
2021-03-15 19:16:11 +0300

    Added partyID param to anapi api services

--- src/app/api/search/invoice-search.service.ts
+++ src/app/api/search/invoice-search.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
 import moment from 'moment';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
 
-import { InlineResponse2009, SearchService } from '@dsh/api-codegen/anapi';
+import { InlineResponse2009, Invoice, SearchService } from '@dsh/api-codegen/anapi';
+import { KeycloakTokenInfoService } from '@dsh/app/shared/services';
 
 import { genXRequestID, toDateLike } from '../utils';
 import { Duration, InvoicesSearchParams } from './model';
 
+export type InvoicesAndContinuationToken = InlineResponse2009;
+
 @Injectable()
 export class InvoiceSearchService {
-    constructor(private searchService: SearchService) {}
+    constructor(private searchService: SearchService, private keycloakTokenInfoService: KeycloakTokenInfoService) {}
+
+    private partyID$: Observable<string> = this.keycloakTokenInfoService.partyID$;
 
     searchInvoices(
         fromTime: string,
@@ -18,35 +23,43 @@ export class InvoiceSearchService {
         params: InvoicesSearchParams,
         limit: number,
         continuationToken?: string
-    ): Observable<InlineResponse2009> {
-        return this.searchService.searchInvoices(
-            genXRequestID(),
-            toDateLike(fromTime),
-            toDateLike(toTime),
-            limit,
-            undefined,
-            undefined,
-            params.shopID,
-            params.shopIDs,
-            params.paymentInstitutionRealm,
-            params.invoiceIDs,
-            params.invoiceStatus,
-            params.invoiceID,
-            params.externalID,
-            params.invoiceAmountFrom,
-            params.invoiceAmountTo,
-            params.excludedShops,
-            continuationToken
+    ): Observable<InvoicesAndContinuationToken> {
+        return this.partyID$.pipe(
+            switchMap((partyID) =>
+                this.searchService.searchInvoices(
+                    genXRequestID(),
+                    toDateLike(fromTime),
+                    toDateLike(toTime),
+                    limit,
+                    undefined,
+                    partyID,
+                    params.shopID,
+                    params.shopIDs,
+                    params.paymentInstitutionRealm,
+                    params.invoiceIDs,
+                    params.invoiceStatus,
+                    params.invoiceID,
+                    params.externalID,
+                    params.invoiceAmountFrom,
+                    params.invoiceAmountTo,
+                    params.excludedShops,
+                    continuationToken
+                )
+            )
         );
     }
 
-    searchInvoicesByDuration({ amount, unit }: Duration, invoiceID: string, limit: number) {
+    searchInvoicesByDuration(
+        { amount, unit }: Duration,
+        invoiceID: string,
+        limit: number
+    ): Observable<InvoicesAndContinuationToken> {
         const from = moment().subtract(amount, unit).startOf('d').utc().format();
         const to = moment().endOf('d').utc().format();
         return this.searchInvoices(from, to, { invoiceID }, limit);
     }
 
-    getInvoiceByDuration(duration: Duration, invoiceID: string) {
+    getInvoiceByDuration(duration: Duration, invoiceID: string): Observable<Invoice> {
         return this.searchInvoicesByDuration(duration, invoiceID, 1).pipe(map((res) => res.result[0]));
     }
 }
