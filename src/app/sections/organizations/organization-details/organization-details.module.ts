9f17891c8969dbdb7a6527e9379831035e95ce61
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2021-03-24 19:02:24 +0300

    update titles

--- src/app/sections/organizations/organization-details/organization-details.module.ts
+++ src/app/sections/organizations/organization-details/organization-details.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { TranslocoModule } from '@ngneat/transloco';
 
+import { IndicatorsModule } from '@dsh/components/indicators';
+import { BreadcrumbModule } from '@dsh/components/navigation';
+
 import { DshTabsModule, HeadlineModule } from '../../../../components/layout';
 import { OrganizationDetailsRoutingModule } from './organization-details-routing.module';
 import { OrganizationDetailsComponent } from './organization-details.component';
@@ -13,8 +16,9 @@ import { OrganizationDetailsComponent } from './organization-details.component';
         CommonModule,
         FlexLayoutModule,
         TranslocoModule,
-        HeadlineModule,
         DshTabsModule,
+        BreadcrumbModule,
+        IndicatorsModule,
     ],
     declarations: [OrganizationDetailsComponent],
     exports: [OrganizationDetailsComponent],
