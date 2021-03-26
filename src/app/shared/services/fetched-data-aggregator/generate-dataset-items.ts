1cc6cf3e293f2845820165ebff7e815a569010bd
Aleksandra Usacheva <lazercore@me.com>
2021-03-26 05:20:55 +0300

    heh

--- src/app/shared/services/fetched-data-aggregator/generate-dataset-items.ts
+++ src/app/shared/services/fetched-data-aggregator/generate-dataset-items.ts
@@ -1,7 +1,7 @@
-import { DataSetItemID } from '../../models';
+import { DataSetItemStrID } from '../../models';
 import { createArrayOfLength } from '../../utils';
 
-export function generateDatasetItems(length: number): DataSetItemID[] {
+export function generateDatasetItems(length: number): DataSetItemStrID[] {
     return createArrayOfLength(length).map((_: null, index: number) => {
         return {
             id: `mock_item_${index}`,
