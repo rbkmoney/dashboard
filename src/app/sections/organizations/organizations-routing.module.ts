f880ae6e2ddef9a990d933ffdd8a14db7f82b4ae
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2021-03-29 19:31:24 +0300

    accept-invitation

--- src/app/sections/organizations/organizations-routing.module.ts
+++ src/app/sections/organizations/organizations-routing.module.ts
@@ -7,6 +7,12 @@ export const routes: Routes = [
     {
         path: '',
         component: OrganizationsComponent,
+        children: [
+            {
+                path: 'accept-invitation',
+                loadChildren: () => import('./accept-invitation').then((m) => m.AcceptInvitationModule),
+            },
+        ],
     },
 ];
 
