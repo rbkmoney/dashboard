b55e8d052935941ed79054edb038a96a282131a1
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2021-04-06 22:28:11 +0300

    empty scope

--- src/app/shared/components/organization-roles/utils/get-roles-by-group.ts
+++ src/app/shared/components/organization-roles/utils/get-roles-by-group.ts
@@ -12,14 +12,16 @@ export function getRolesByGroup(roles: MemberRole[]): RoleGroup[] {
                 groups.push(group);
             }
             let scope: RoleGroupScope = group.scopes.find((s) => s.id === role.scope.id);
-            if (!scope) {
-                scope = {
-                    id: role.scope.id,
-                    resourcesIds: [],
-                };
-                group.scopes.push(scope);
+            if (role.scope) {
+                if (!scope) {
+                    scope = {
+                        id: role.scope.id,
+                        resourcesIds: [],
+                    };
+                    group.scopes.push(scope);
+                }
+                scope.resourcesIds.push(role.scope.resourceId);
             }
-            scope.resourcesIds.push(role.scope.resourceId);
             return groups;
         }, [])
         .sort((a, b) => ROLE_PRIORITY_DESC[a.id] - ROLE_PRIORITY_DESC[b.id]);
