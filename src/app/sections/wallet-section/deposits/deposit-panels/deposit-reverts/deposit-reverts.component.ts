2d060e89856ddb9dd016fc9c3150b8c02a2cae2c
Aleksandra Usacheva <lazercore@me.com>
2021-04-07 15:33:06 +0300

    revert fixes

--- src/app/sections/wallet-section/deposits/deposit-panels/deposit-reverts/deposit-reverts.component.ts
+++ src/app/sections/wallet-section/deposits/deposit-panels/deposit-reverts/deposit-reverts.component.ts
@@ -1,16 +1,13 @@
 import { Component, Input, OnInit } from '@angular/core';
 
-import { Deposit } from '@dsh/api-codegen/wallet-api';
-
 import { FetchDepositRevertsService } from './services/fetch-deposit-reverts.service';
 
 @Component({
     selector: 'dsh-deposit-reverts',
     templateUrl: 'deposit-reverts.component.html',
-    providers: [FetchDepositRevertsService],
 })
 export class DepositRevertsComponent implements OnInit {
-    @Input() deposit: Deposit;
+    @Input() depositID: string;
 
     reverts$ = this.fetchDepositRevertsService.searchResult$;
     hasMore$ = this.fetchDepositRevertsService.hasMore$;
@@ -20,7 +17,7 @@ export class DepositRevertsComponent implements OnInit {
 
     ngOnInit(): void {
         this.fetchDepositRevertsService.search({
-            depositID: this.deposit.id,
+            depositID: this.depositID,
         });
     }
 
