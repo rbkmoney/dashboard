8a98d11733565639ae31b24bd4622419609d7676
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2021-04-07 19:37:23 +0300

    FetchInvitationsService, InvitationsExpandedIdManager

--- src/app/sections/organization-details/invitations/components/invitations-list/invitations-list.component.ts
+++ src/app/sections/organization-details/invitations/components/invitations-list/invitations-list.component.ts
@@ -2,6 +2,8 @@ import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from 
 
 import { Invitation, Organization } from '@dsh/api-codegen/organizations';
 
+import { InvitationsExpandedIdManager } from '../../services/invitations-expanded-id-manager/invitations-expanded-id-manager.service';
+
 @Component({
     selector: 'dsh-invitations-list',
     templateUrl: 'invitations-list.component.html',
@@ -10,8 +12,18 @@ import { Invitation, Organization } from '@dsh/api-codegen/organizations';
 export class InvitationsListComponent {
     @Input() invitations: Invitation[];
     @Input() orgId: Organization['id'];
-    @Input() expandedId: number;
 
-    @Output() expandedIdChange = new EventEmitter<number>();
     @Output() changed = new EventEmitter<void>();
+
+    expandedId$ = this.expandedIdManager.expandedId$;
+
+    constructor(private expandedIdManager: InvitationsExpandedIdManager) {}
+
+    trackInvitations(idx: number, item: Invitation): string {
+        return item.id;
+    }
+
+    expandedIdChange(id: number): void {
+        this.expandedIdManager.expandedIdChange(id);
+    }
 }
