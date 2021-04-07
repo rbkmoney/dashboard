acb6dcb71f94390dacb443a399a877b58fb6bbae
Aleksandra Usacheva <lazercore@me.com>
2021-04-07 17:04:56 +0300

    revert fixes

--- src/app/sections/wallet-section/deposits/deposit-panels/deposit-reverts/deposit-reverts.module.ts
+++ src/app/sections/wallet-section/deposits/deposit-panels/deposit-reverts/deposit-reverts.module.ts
@@ -4,13 +4,11 @@ import { FlexModule } from '@angular/flex-layout';
 import { MatDividerModule } from '@angular/material/divider';
 import { TranslocoModule } from '@ngneat/transloco';
 
-import { SEARCH_LIMIT } from '@dsh/app/sections/tokens';
 import { DepositRevertDetailsModule } from '@dsh/app/shared';
 import { IndicatorsModule } from '@dsh/components/indicators';
 import { ShowMorePanelModule } from '@dsh/components/show-more-panel';
 
 import { DepositRevertsComponent } from './deposit-reverts.component';
-import { FetchDepositRevertsService } from './services/fetch-deposit-reverts.service';
 
 @NgModule({
     declarations: [DepositRevertsComponent],
