32896c56cf404c039be7ef36eab6221711bb5d19
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2021-04-12 19:15:27 +0300

    remove date adapter

--- src/components/filters/daterange-filter/daterange-filter.module.ts
+++ src/components/filters/daterange-filter/daterange-filter.module.ts
@@ -1,14 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { DateAdapter } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { TranslocoModule } from '@ngneat/transloco';
 
 import { DaterangeModule } from '@dsh/pipes/daterange';
 
 import { FilterModule } from '../filter';
-import { DateAdapter as CustomDateAdapter } from './date-adapter';
 import { DaterangeFilterMenuComponent } from './daterange-filter-selector';
 import { DaterangeFilterComponent } from './daterange-filter.component';
 
@@ -18,6 +16,5 @@ const EXPORTED_DECLARATIONS = [DaterangeFilterComponent, DaterangeFilterMenuComp
     imports: [CommonModule, FlexLayoutModule, FilterModule, TranslocoModule, DaterangeModule, MatDatepickerModule],
     declarations: EXPORTED_DECLARATIONS,
     exports: EXPORTED_DECLARATIONS,
-    providers: [{ provide: DateAdapter, useClass: CustomDateAdapter }],
 })
 export class DaterangeFilterModule {}
