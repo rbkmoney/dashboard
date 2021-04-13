2db56df2eb344077328c8389fa152d8f365a2e3f
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2021-04-13 17:04:33 +0300

    add MatMomentDateModule all

--- src/app/sections/create-invoice/create-invoice.module.ts
+++ src/app/sections/create-invoice/create-invoice.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatMomentDateModule } from '@angular/material-moment-adapter';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
@@ -43,6 +44,7 @@ const EXPORTED_DECLARATIONS = [CreateInvoiceComponent];
         MatDialogModule,
         InvoiceModule,
         MatDatepickerModule,
+        MatMomentDateModule,
         MatDividerModule,
         ShopSelectorModule,
         MatDialogModule,
