25f1b4a5d16c94adea06bd85dd2fa473315a0569
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2021-04-16 18:00:31 +0300

    FRONTEND-528: Enable orgs menu list & org role selection bug fix (#438)

--- src/app/sections/organization-details/members/components/edit-roles-dialog/utils/equal-roles.ts
+++ src/app/sections/organization-details/members/components/edit-roles-dialog/utils/equal-roles.ts
@@ -3,7 +3,7 @@ import { PartialReadonly } from '@dsh/type-utils';
 
 export function equalRoles(a: PartialReadonly<MemberRole>, b: PartialReadonly<MemberRole>) {
     return (
-        (a && b && a.id === b.id) ||
+        (a.id && b.id && a.id === b.id) ||
         (a.roleId === b.roleId &&
             ((!a.scope && !b.scope) || (a.scope.id === b.scope.id && a.scope.resourceId === b.scope.resourceId)))
     );
