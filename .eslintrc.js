56ad1a3e238147e9d35c0cbf46d676febc9863bc
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2021-04-16 16:11:59 +0300

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -52,9 +52,9 @@ module.exports = {
                         leadingUnderscore: 'allow',
                     },
                     {
-                        "selector": "enumMember",
-                        "format": ["StrictPascalCase"]
-                    }
+                        selector: 'enumMember',
+                        format: ['StrictPascalCase'],
+                    },
                 ],
 
                 // Imports/exports rules
