d70c1341030dac7d7562c06f4fa5fca775932caf
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2021-04-16 14:49:59 +0300

    InvoiceType

--- src/app/sections/create-payment-link/create-payment-link.component.ts
+++ src/app/sections/create-payment-link/create-payment-link.component.ts
@@ -30,7 +30,7 @@ export class CreatePaymentLinkComponent implements OnInit {
     set template(template: InvoiceTemplateAndToken) {
         if (template) {
             this.createPaymentLinkService.changeInvoiceTemplate(template);
-            this.type = InvoiceType.template;
+            this.type = InvoiceType.Template;
         }
     }
 
@@ -38,7 +38,7 @@ export class CreatePaymentLinkComponent implements OnInit {
     set invoice(invoice: Invoice) {
         if (invoice) {
             this.createPaymentLinkService.changeInvoice(invoice);
-            this.type = InvoiceType.invoice;
+            this.type = InvoiceType.Invoice;
         }
     }
 
@@ -80,10 +80,10 @@ export class CreatePaymentLinkComponent implements OnInit {
 
     create() {
         switch (this.type) {
-            case InvoiceType.invoice:
+            case InvoiceType.Invoice:
                 this.createPaymentLinkService.createByInvoice();
                 break;
-            case InvoiceType.template:
+            case InvoiceType.Template:
                 this.createPaymentLinkService.createByTemplate();
                 break;
         }
