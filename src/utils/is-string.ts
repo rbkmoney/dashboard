4044a5877c3897367c742fb19b7423023e059405
Aleksandra Usacheva <lazercore@me.com>
2021-04-20 06:18:14 +0300

    Bye, lodash

--- src/utils/is-string.ts
+++ src/utils/is-string.ts
@@ -1,1 +1,4 @@
-export const isString = (str): boolean => str && typeof str.valueOf() === 'string';
+export const isString = (str): boolean => {
+    const type = typeof str;
+    return type === 'string' || (type === 'object' && str != null && !Array.isArray(str));
+};
