51bd0450e28e9231cfb27211126e604b232eb4e3
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2021-04-21 15:57:36 +0300

    StrictPascalCase

--- .eslintrc.js
+++ .eslintrc.js
@@ -50,7 +50,7 @@ module.exports = {
                     'error',
                     {
                         selector: 'typeLike',
-                        format: ['PascalCase'],
+                        format: ['StrictPascalCase'],
                         leadingUnderscore: 'allow',
                     },
                     {
