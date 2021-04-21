214c774ba3b7bc97c366bc8353655d178c57f4d4
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2021-04-21 16:00:42 +0300

    CapiConfigService

--- src/app/api-codegen/capi/capi.module.ts
+++ src/app/api-codegen/capi/capi.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 
-import { CAPIConfigService } from './capi-config.service';
+import { CapiConfigService } from './capi-config.service';
 import { InvoicesService } from './invoices.service';
 import { ShopsService } from './shops.service';
 import { ApiModule, ClaimsService, Configuration, PartiesService, PayoutsService } from './swagger-codegen';
@@ -9,9 +9,9 @@ import { ApiModule, ClaimsService, Configuration, PartiesService, PayoutsService
     imports: [
         {
             ngModule: ApiModule,
-            providers: [{ provide: Configuration, useClass: CAPIConfigService }],
+            providers: [{ provide: Configuration, useClass: CapiConfigService }],
         },
     ],
-    providers: [CAPIConfigService, ClaimsService, ShopsService, InvoicesService, PayoutsService, PartiesService],
+    providers: [CapiConfigService, ClaimsService, ShopsService, InvoicesService, PayoutsService, PartiesService],
 })
 export class CapiModule {}
