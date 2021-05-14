8dc015f5d49f3683ab30b4343614e54e8b5bb52e
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2021-05-14 14:57:05 +0300

    grids for filters and rows

--- src/app/sections/payment-section/payouts/payouts-search-filters/payouts-search-filters.module.ts
+++ src/app/sections/payment-section/payouts/payouts-search-filters/payouts-search-filters.module.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FlexModule } from '@angular/flex-layout';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { FilterShopsModule } from '@dsh/app/shared/components';
 import { DaterangeFilterModule } from '@dsh/components/filters/daterange-filter';
@@ -8,7 +8,7 @@ import { DaterangeFilterModule } from '@dsh/components/filters/daterange-filter'
 import { PayoutsSearchFiltersComponent } from './payouts-search-filters.component';
 
 @NgModule({
-    imports: [CommonModule, DaterangeFilterModule, FilterShopsModule, FlexModule],
+    imports: [CommonModule, DaterangeFilterModule, FilterShopsModule, FlexLayoutModule],
     exports: [PayoutsSearchFiltersComponent],
     declarations: [PayoutsSearchFiltersComponent],
 })
