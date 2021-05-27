8885ce5dc000b69ad1a3d8b2e6ee8d8915db41e5
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2021-05-27 16:14:19 +0300

    rem deprecated

--- src/app/shared/components/filters/invoices-filter/invoices-filter.module.ts
+++ src/app/shared/components/filters/invoices-filter/invoices-filter.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { TranslocoModule } from '@ngneat/transloco';
 
 import { InvoicesFieldModule } from '@dsh/app/shared/components/inputs/invoices-field';
