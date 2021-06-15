ed4d560e289e167255e9e9a8394feae6a25ecf74
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2021-06-15 18:05:48 +0300

    isNil

--- src/utils/validators/alpha-3-country-validator.ts
+++ src/utils/validators/alpha-3-country-validator.ts
@@ -1,10 +1,11 @@
 import { FormControl } from '@ngneat/reactive-forms';
 import { ValidationErrors } from '@ngneat/reactive-forms/lib/types';
+import isNil from 'lodash-es/isNil';
 
 import { CountryCodes } from './country-codes';
 
 export function alpha3CountryValidator(
     control: FormControl<string>
 ): ValidationErrors<{ unknownCountryCode: boolean }> {
-    return CountryCodes[control.value] ? null : { unknownCountryCode: true };
+    return isNil(CountryCodes[control.value]) ? { unknownCountryCode: true } : null;
 }
