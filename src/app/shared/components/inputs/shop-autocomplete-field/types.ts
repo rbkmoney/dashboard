ef94de3fc4e0d349322acc1b91497b3d4b07640f
Ildar Galeev <KeinAsylum@gmail.com>
2021-06-17 15:29:04 +0300

    Refactoring for types

--- src/app/shared/components/inputs/shop-autocomplete-field/types.ts
+++ src/app/shared/components/inputs/shop-autocomplete-field/types.ts
@@ -1,3 +1,5 @@
-export type ShopId = string;
-export type ShopName = string;
+import { Shop } from '@dsh/api-codegen/capi';
+
+export type ShopId = Shop['id'];
+export type ShopName = Shop['details']['name'];
 export type DisplayWithFn = (value: ShopId) => string;
--- src/app/shared/components/inputs/wallet-autocomplete-field/types.ts
+++ src/app/shared/components/inputs/wallet-autocomplete-field/types.ts
@@ -1,3 +1,5 @@
-export type WalletId = string;
-export type WalletName = string;
+import { Wallet } from '@dsh/api-codegen/wallet-api';
+
+export type WalletId = Wallet['id'];
+export type WalletName = Wallet['name'];
 export type DisplayWithFn = (value: WalletId) => string;
