8e400201e4a6221a8ab6c7a3e5a0cb265e4c97b7
Ildar Galeev <KeinAsylum@gmail.com>
2021-06-17 16:04:53 +0300

    Prettier fix

--- src/components/form-controls/autocomplete-field/utils/filter-options.ts
+++ src/components/form-controls/autocomplete-field/utils/filter-options.ts
@@ -1,6 +1,11 @@
 import { Option } from '../types';
 
-const filterPredicate = <T>(searchStr: string) => (option: Option<T>) => option.label.toLowerCase().includes(searchStr);
+const filterPredicate =
+    <T>(searchStr: string) =>
+    (option: Option<T>) =>
+        option.label.toLowerCase().includes(searchStr);
 
-export const filterOptions = <T>(options: Option<T>[]) => (controlValue: unknown): Option<T>[] =>
-    typeof controlValue === 'string' ? options.filter(filterPredicate(controlValue.toLowerCase())) : options;
+export const filterOptions =
+    <T>(options: Option<T>[]) =>
+    (controlValue: unknown): Option<T>[] =>
+        typeof controlValue === 'string' ? options.filter(filterPredicate(controlValue.toLowerCase())) : options;
