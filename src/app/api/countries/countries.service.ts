0d0780df9648c90ecfc5d300cd7f8442d429ee47
Ildar Galeev <KeinAsylum@gmail.com>
2021-06-22 16:22:34 +0300

    Used error module

--- src/app/api/countries/countries.service.ts
+++ src/app/api/countries/countries.service.ts
@@ -1,25 +1,28 @@
 import { Injectable } from '@angular/core';
 import { IdGeneratorService } from '@rbkmoney/id-generator';
-import * as Sentry from '@sentry/angular';
 import sortBy from 'lodash-es/sortBy';
 import { Observable, of } from 'rxjs';
 import { catchError, map, shareReplay } from 'rxjs/operators';
 
 import { Country, CountriesService as CountriesAPIService } from '@dsh/api-codegen/capi/swagger-codegen';
+import { ErrorService } from '@dsh/app/shared/services';
 import { SHARE_REPLAY_CONF } from '@dsh/operators';
-import { extractError } from '@dsh/utils';
 
 @Injectable()
 export class CountriesService {
     countries$: Observable<Country[]> = this.getCountries().pipe(
         catchError((error) => {
-            Sentry.captureException(extractError(error));
+            this.errorService.error(error, false);
             return of([]);
         }),
         shareReplay(SHARE_REPLAY_CONF)
     );
 
-    constructor(private countriesService: CountriesAPIService, private idGenerator: IdGeneratorService) {}
+    constructor(
+        private countriesService: CountriesAPIService,
+        private idGenerator: IdGeneratorService,
+        private errorService: ErrorService
+    ) {}
 
     getCountries(): Observable<Country[]> {
         return this.countriesService
