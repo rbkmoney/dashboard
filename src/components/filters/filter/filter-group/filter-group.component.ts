edbe7d33c5b9ebc7be44cd2d854298b3ed5c554a
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2021-06-24 18:10:54 +0300

    rem filter group

--- src/components/filters/filter/filter-group/filter-group.component.ts
+++
@@ -1,8 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-    selector: 'dsh-filter-group',
-    templateUrl: 'filter-group.component.html',
-    changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class FilterGroupComponent {}
