a797af1629a5a153fa8490118ad669b1d6e49e4c
hedgehog <ezhoden@mail.ru>
2021-07-15 13:47:42 +0300

    fix

--- src/app/sections/payment-section/payment-section.component.ts
+++ src/app/sections/payment-section/payment-section.component.ts
@@ -1,16 +1,15 @@
 import { Component } from '@angular/core';
 import { map } from 'rxjs/operators';
 
-import { PaymentSectionService } from './payment-section.service';
-import { PaymentInstitutionRealmService } from './services/payment-institution-realm/payment-institution-realm.service';
-import { RealmShopsService } from './services/realm-shops/realm-shops.service';
 import { PaymentInstitution } from '@dsh/api-codegen/capi';
 
+import { PaymentInstitutionRealmService } from './services/payment-institution-realm/payment-institution-realm.service';
+import { RealmShopsService } from './services/realm-shops/realm-shops.service';
 
 @Component({
     templateUrl: 'payment-section.component.html',
     styleUrls: ['../main-sections.scss', 'payment-section.scss'],
-    providers: [PaymentSectionService, PaymentInstitutionRealmService, RealmShopsService],
+    providers: [PaymentInstitutionRealmService, RealmShopsService],
 })
 export class PaymentSectionComponent {
     isTestRealm$ = this.paymentSectionService.realm$.pipe(map((realm) => realm === PaymentInstitution.RealmEnum.Test));
