445804ee22c5a475c65d668ccfba4e0f6b31a289
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2021-07-23 19:25:06 +0300

    rem unused

--- src/utils/random-string.spec.ts
+++
@@ -1,9 +0,0 @@
-import { randomString } from './random-string';
-
-describe('randomString', () => {
-    it('should create a string of length 10', () => {
-        const length = 1000;
-        const str = randomString(length);
-        expect(str.length).toBe(length);
-    });
-});
