3575c6e8fba226693681eb47d9aff80d4833f008
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2021-07-27 18:32:56 +0300

    add company search for onboarding/claim/claimID

--- src/app/sections/onboarding/company-search/company-search.component.ts
+++ src/app/sections/onboarding/company-search/company-search.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { Observable, of } from 'rxjs';
 import { catchError, map, shareReplay, switchMap, take } from 'rxjs/operators';
 
@@ -14,6 +15,7 @@ import {
     konturFocusDataToQuestionaryData,
 } from './to-questionary-data';
 
+@UntilDestroy()
 @Component({
     templateUrl: 'company-search.component.html',
     styleUrls: ['company-search.component.scss'],
@@ -25,16 +27,15 @@ export class CompanySearchComponent {
     manualContractorSelector = false;
     isKnownOrgType: boolean;
     content: PartyContent;
-
     data$: Observable<QuestionaryData>;
 
     constructor(private companySearchService: CompanySearchService) {}
 
-    leaveOnboarding() {
+    leaveOnboarding(): void {
         this.companySearchService.leaveOnboarding();
     }
 
-    next() {
+    next(): void {
         this.data$
             .pipe(
                 switchMap((data) => this.companySearchService.createInitialClaim(data)),
@@ -43,7 +44,7 @@ export class CompanySearchComponent {
             .subscribe(({ claimID, documentID }) => this.companySearchService.goToOnboardingFlow(claimID, documentID));
     }
 
-    updateSuggestion(content: PartyContent) {
+    updateSuggestion(content: PartyContent): void {
         this.isKnownOrgType = this.companySearchService.isKnownOrgType(content);
         if (this.isKnownOrgType) {
             this.manualContractorSelector = false;
@@ -54,23 +55,23 @@ export class CompanySearchComponent {
         }
     }
 
-    searchSuggestionError() {
+    searchSuggestionError(): void {
         this.manualContractorSelector = true;
         this.cleanData();
     }
 
-    suggestionNotFound() {
+    suggestionNotFound(): void {
         this.manualContractorSelector = true;
         this.cleanData();
     }
 
-    manualContractorTypeSelected(t: Contractor.ContractorTypeEnum) {
+    manualContractorTypeSelected(t: Contractor.ContractorTypeEnum): void {
         this.setDataByManualContractorType(t);
     }
 
     private cleanData() {
         this.content = null;
-        this.data$ = of(null);
+        this.data$ = of<QuestionaryData>(null);
     }
 
     private setDataByManualContractorType(t: Contractor.ContractorTypeEnum) {
@@ -86,6 +87,7 @@ export class CompanySearchComponent {
                 return of(null);
             }),
             map((data) => (data ? konturFocusDataToQuestionaryData(data) : dadataDataToQuestionaryData(content))),
+            untilDestroyed(this),
             shareReplay(1)
         );
     }
