6c30c8decc5c639930d127bfb393fa5d7c70a7a9
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2021-08-02 17:06:04 +0300

    refactor eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,74 +1,49 @@
+const rules = require('@rbkmoney/eslint-plugin/lib/rules');
+
+const baseTsRules = {
+    parserOptions: {
+        project: ['tsconfig.json', 'e2e/tsconfig.json'],
+        createDefaultProgram: true,
+    },
+    extends: [
+        'plugin:@rbkmoney/typescript',
+        'plugin:@rbkmoney/angular',
+        'plugin:@rbkmoney/lodash',
+        'plugin:@rbkmoney/prettier',
+    ],
+    rules: {
+        ...rules.createImportOrderRule({ internalPathsPattern: '@dsh/**' }),
+        // TODO: pretenders for error
+        '@typescript-eslint/no-unsafe-call': 'warn',
+        '@typescript-eslint/no-unsafe-member-access': 'warn',
+        '@typescript-eslint/no-unsafe-assignment': 'warn',
+        '@typescript-eslint/no-unsafe-return': 'warn',
+        '@typescript-eslint/no-misused-promises': 'warn',
+        '@typescript-eslint/unbound-method': 'warn',
+        '@typescript-eslint/restrict-plus-operands': 'warn',
+        '@typescript-eslint/restrict-template-expressions': 'warn',
+    },
+};
+
 module.exports = {
     root: true,
     parser: '@typescript-eslint/parser',
     ignorePatterns: ['**/openapi-codegen/**/*.ts', '**/swagger-codegen/**/*.ts'],
     overrides: [
         {
+            ...baseTsRules,
             files: ['*.ts'],
-            parserOptions: {
-                project: ['tsconfig.json', 'e2e/tsconfig.json'],
-                createDefaultProgram: true,
-            },
-            extends: [
-                'plugin:@rbkmoney/typescript',
-                'plugin:@rbkmoney/angular',
-                'plugin:@rbkmoney/lodash',
-                'plugin:@rbkmoney/prettier',
-            ],
             rules: {
-                '@angular-eslint/directive-selector': [
-                    'error',
-                    {
-                        type: 'attribute',
-                        prefix: 'dsh',
-                        style: 'camelCase',
-                    },
-                ],
-                '@angular-eslint/component-selector': [
-                    'error',
-                    {
-                        type: 'element',
-                        prefix: 'dsh',
-                        style: 'kebab-case',
-                    },
-                ],
-
+                ...baseTsRules.rules,
+                ...rules.createAngularSelectorRules({ prefix: 'dsh' }),
                 // TODO: pretenders for error
                 '@typescript-eslint/no-floating-promises': 'warn',
-                '@typescript-eslint/no-unsafe-call': 'warn',
-                '@typescript-eslint/no-unsafe-member-access': 'warn',
-                '@typescript-eslint/no-unsafe-assignment': 'warn',
-                '@typescript-eslint/no-unsafe-return': 'warn',
-                '@typescript-eslint/no-misused-promises': 'warn',
-                '@typescript-eslint/unbound-method': 'warn',
-                '@typescript-eslint/restrict-plus-operands': 'warn',
-                '@typescript-eslint/restrict-template-expressions': 'warn',
             },
         },
         {
+            ...baseTsRules,
             files: ['*.spec.ts'],
-            parserOptions: {
-                project: ['tsconfig.json', 'e2e/tsconfig.json'],
-                createDefaultProgram: true,
-            },
-            extends: [
-                'plugin:@rbkmoney/typescript',
-                'plugin:@rbkmoney/angular',
-                'plugin:@rbkmoney/jasmine',
-                'plugin:@rbkmoney/lodash',
-                'plugin:@rbkmoney/prettier',
-            ],
-            rules: {
-                // TODO: pretenders for error
-                '@typescript-eslint/no-unsafe-call': 'warn',
-                '@typescript-eslint/no-unsafe-member-access': 'warn',
-                '@typescript-eslint/no-unsafe-assignment': 'warn',
-                '@typescript-eslint/no-unsafe-return': 'warn',
-                '@typescript-eslint/no-misused-promises': 'warn',
-                '@typescript-eslint/unbound-method': 'warn',
-                '@typescript-eslint/restrict-plus-operands': 'warn',
-                '@typescript-eslint/restrict-template-expressions': 'warn',
-            },
+            extends: [...baseTsRules.extends, 'plugin:@rbkmoney/jasmine'],
         },
         {
             files: ['*.html'],
