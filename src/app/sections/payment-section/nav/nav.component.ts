6cb5655c171e6af6643d0844b1287abefc74ed4e
Ildar Galeev <KeinAsylum@gmail.com>
2021-08-11 14:33:53 +0300

    FR-709 Rude draft

--- src/app/sections/payment-section/nav/nav.component.ts
+++ src/app/sections/payment-section/nav/nav.component.ts
@@ -1,7 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { NavbarDirection, IconSize } from '@dsh/components/navigation';
+
+interface NavbarConfig {
+    navDirection: NavbarDirection;
+    hideNavItemContent: boolean;
+    iconSize: IconSize;
+}
+
+type NavMode = 'desktop' | 'mobile';
 
 @Component({
     selector: 'dsh-nav',
     templateUrl: 'nav.component.html',
+    styleUrls: ['nav.component.scss'],
 })
-export class NavComponent {}
+export class NavComponent {
+    @Input() mode: NavMode = 'desktop';
+
+    get navbarConfig(): NavbarConfig {
+        switch (this.mode) {
+            case 'desktop':
+                return {
+                    navDirection: 'column',
+                    hideNavItemContent: false,
+                    iconSize: 'md',
+                };
+            case 'mobile':
+                return {
+                    navDirection: 'row',
+                    hideNavItemContent: true,
+                    iconSize: 'lg',
+                };
+        }
+    }
+}
