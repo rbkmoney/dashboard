2994822e78e6bea11279f16ff539729f94adb614
Denis Ezhov <ezhoden@mail.ru>
2021-09-14 10:54:18 +0300

    FR-746: Wallet main info (#567)
    * wallets list refactoring
    
    * wallet main info
    
    * wallet account
    
    * remove delay
    
    * fixes
    
    * Update src/app/sections/wallet-section/wallets/wallets-list/wallet-account-info/services/fetch-wallet-account/fetch-wallet-account.service.ts
    
    Co-authored-by: Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
    
    * Update src/app/sections/wallet-section/wallets/wallets-list/wallet-account-info/services/fetch-wallet-account/fetch-wallet-account.service.ts
    
    Co-authored-by: Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
    
    * isLoading fix
    
    * remove test delay
    
    Co-authored-by: Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>

--- src/app/sections/wallet-section/wallets/wallets-list/wallets-list.module.ts
+++ src/app/sections/wallet-section/wallets/wallets-list/wallets-list.module.ts
@@ -1,12 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FlexModule } from '@angular/flex-layout';
+import { MatDividerModule } from '@angular/material/divider';
 import { TranslocoModule } from '@ngneat/transloco';
 
 import { LastUpdatedModule } from '@dsh/components/indicators/last-updated/last-updated.module';
 import { AccordionModule, CardModule, ExpandPanelModule, RowModule } from '@dsh/components/layout';
 
-import { WalletRowComponent, WalletRowHeaderComponent } from './components';
+import { WalletRowComponent, WalletRowHeaderComponent, WalletDetailsComponent } from './components';
+import { WalletsAccountInfoModule } from './wallet-account-info';
+import { WalletsMainInfoModule } from './wallet-main-info';
 import { WalletsListComponent } from './wallets-list.component';
 
 @NgModule({
@@ -14,13 +17,16 @@ import { WalletsListComponent } from './wallets-list.component';
         TranslocoModule,
         CommonModule,
         FlexModule,
+        MatDividerModule,
         LastUpdatedModule,
         AccordionModule,
         ExpandPanelModule,
         CardModule,
         RowModule,
+        WalletsMainInfoModule,
+        WalletsAccountInfoModule,
     ],
-    declarations: [WalletsListComponent, WalletRowHeaderComponent, WalletRowComponent],
+    declarations: [WalletsListComponent, WalletRowHeaderComponent, WalletRowComponent, WalletDetailsComponent],
     exports: [WalletsListComponent],
 })
 export class WalletsListModule {}
