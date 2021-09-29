1a360af57999cd31267ae126b73d6c72a16186db
hedgehog <ezhoden@mail.ru>
2021-09-29 13:31:00 +0300

    not hide filters on mobile, remove unused file

--- src/app/sections/payment-section/analytics/analytics-search-filters/get-default-daterange.ts
+++
@@ -1,8 +0,0 @@
-import moment from 'moment';
-
-import { Daterange } from '@dsh/pipes/daterange';
-
-export const getDefaultDaterange = (): Daterange => ({
-    begin: moment().startOf('M'),
-    end: moment().endOf('M'),
-});
