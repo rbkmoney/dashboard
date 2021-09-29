9c855cd93783c98ab9dc37c6103674b5a5c8f759
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2021-09-29 12:36:52 +0300

    add todos

--- src/app/api/capi/models/invoice-template-type.ts
+++ src/app/api/capi/models/invoice-template-type.ts
@@ -1,3 +1,4 @@
+// TODO: need to fix swag for generate enum
 export enum InvoiceTemplateType {
     InvoiceTemplateSingleLine = 'InvoiceTemplateSingleLine',
     InvoiceTemplateMultiLine = 'InvoiceTemplateMultiLine',
