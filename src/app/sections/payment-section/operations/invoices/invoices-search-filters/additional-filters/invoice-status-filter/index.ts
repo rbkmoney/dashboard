288a2d1778f9fde4c42b3275e913227a58eebcef
Denis Ezhov <ezhoden@mail.ru>
2021-10-05 17:35:58 +0300

    FR-715: Invoices mobile filters (#575)
    * secondary filters to additional for mobile
    
    * test fix
    
    * not hide filters on mobile, remove unused file
    
    * clean up

---
+++ src/app/sections/payment-section/operations/invoices/invoices-search-filters/additional-filters/invoice-status-filter/index.ts
@@ -0,0 +1,2 @@
+export * from './invoice-status-filter.module';
+export * from './invoice-status-filter.component';
---
+++ src/app/sections/payment-section/operations/invoices/invoices-search-filters/additional-filters/invoices-filter/index.ts
@@ -0,0 +1,3 @@
+export * from './invoices-filter.module';
+export * from './invoices-filter.component';
+export * from './types';
