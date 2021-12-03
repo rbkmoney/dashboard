a733edb74e2d34120a5b7d2a45f69b8f03fbaab8
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2021-12-03 16:30:22 +0300

    add icon

--- src/app/home/actionbar/components/user-dropdown/user-dropdown.component.ts
+++ src/app/home/actionbar/components/user-dropdown/user-dropdown.component.ts
@@ -5,15 +5,18 @@ import { ContextService } from '@dsh/app/shared/services/context';
 import { coerceBoolean } from '@dsh/utils';
 
 import { KeycloakService } from '../../../../auth';
+import { ROTATE } from './utils/rotate-animation';
 
 @Component({
     selector: 'dsh-user-dropdown',
     templateUrl: 'user-dropdown.component.html',
     styleUrls: ['user-dropdown.component.scss'],
+    animations: [ROTATE],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UserDropdownComponent {
     @Input() @coerceBoolean inverted = false;
+    @Input() @coerceBoolean expanded = false;
 
     username = this.keycloakService.getUsername();
     orgName$ = this.contextService.organization$.pipe(pluck('name'));
